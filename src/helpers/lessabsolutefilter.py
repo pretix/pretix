8ccbaa6caf4b08b2da77846bcda2fc4383708bfd
Raphael Michel <mail@raphaelmichel.de>
2014-09-11 20:49:05 +0200

    Include FontAwesome

---
+++ src/helpers/lessabsolutefilter.py
@@ -0,0 +1,10 @@
+from compressor.filters.base import CompilerFilter
+from compressor.filters.css_default import CssAbsoluteFilter
+
+class LessFilter(CompilerFilter):
+    def __init__(self, content, attrs, **kwargs):
+        super(LessFilter, self).__init__(content, command='lessc {infile} {outfile}', **kwargs)
+
+    def input(self, **kwargs):
+        content = super(LessFilter, self).input(**kwargs)
+        return CssAbsoluteFilter(content).input(**kwargs)
