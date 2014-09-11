5365893b3154b17bafe8213a65b713a63ffb33d5
Raphael Michel <mail@raphaelmichel.de>
2014-09-11 20:49:22 +0200

    Implement Logout

--- src/tixlcontrol/urls.py
+++ src/tixlcontrol/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import patterns, url
 
 urlpatterns = patterns('',
     url(r'^$', 'tixlcontrol.views.main.index', name='index'),
+    url(r'^logout$', 'tixlcontrol.views.auth.logout', name='auth.logout'),
     url(r'^login$', 'tixlcontrol.views.auth.login', name='auth.login'),
 )
