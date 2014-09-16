a64f69ad24ed7cf1c2153589e7f0f8c38852f35c
Raphael Michel <mail@raphaelmichel.de>
2014-09-17 00:23:38 +0200

    Simplified tixlcontrol.urls

--- src/tixlcontrol/urls.py
+++ src/tixlcontrol/urls.py
@@ -1,11 +1,24 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import patterns, url, include
 from tixlcontrol.views import main, event
 
-urlpatterns = patterns('',
-    url(r'^$', 'tixlcontrol.views.main.index', name='index'),
-    url(r'^event/(?P<organizer>[^/]+)/(?P<event>[^/]+)/$', 'tixlcontrol.views.event.index', name='event.index'),
-    url(r'^event/(?P<organizer>[^/]+)/(?P<event>[^/]+)/settings$', event.EventUpdate.as_view(), name='event.settings'),
+urlpatterns = patterns('',)
+urlpatterns += patterns(
+    'tixlcontrol.views.auth',
+    url(r'^logout$', 'logout', name='auth.logout'),
+    url(r'^login$', 'login', name='auth.login'),
+)
+urlpatterns += patterns(
+    'tixlcontrol.views.main',
+    url(r'^$', 'index', name='index'),
     url(r'^events/$', main.EventList.as_view(), name='events'),
-    url(r'^logout$', 'tixlcontrol.views.auth.logout', name='auth.logout'),
-    url(r'^login$', 'tixlcontrol.views.auth.login', name='auth.login'),
+)
+urlpatterns += patterns(
+    'tixlcontrol.views.event',
+    url(r'^event/(?P<organizer>[^/]+)/(?P<event>[^/]+)/', include(
+        patterns(
+            'tixlcontrol.views.event',
+            url(r'^$', 'index', name='event.index'),
+            url(r'^settings$', event.EventUpdate.as_view(), name='event.settings'),
+        )
+        ))
 )
