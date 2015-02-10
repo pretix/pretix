0d552e8c079a8bd96ab211980745123870e3ddff
Raphael Michel <mail@raphaelmichel.de>
2015-02-10 22:48:03 +0100

    Static file and template setup for pretixpresale

--- src/pretixpresale/views/event.py
+++ src/pretixpresale/views/event.py
@@ -1,10 +1,16 @@
 from django.views.generic import TemplateView
 
 
-class EventIndex(TemplateView):
+class EventViewMixin:
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['event'] = self.request.event
+        return context
+
+
+class EventIndex(EventViewMixin, TemplateView):
     template_name = "pretixpresale/event/index.html"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['event'] = self.request.event
         return context
