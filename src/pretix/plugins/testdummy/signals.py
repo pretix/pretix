dade162154bd561d2d9d957a013ed07eda3f0319
Raphael Michel <mail@raphaelmichel.de>
2015-02-21 17:02:57 +0100

    Fix failing plugins test

--- src/pretix/plugins/testdummy/signals.py
+++ src/pretix/plugins/testdummy/signals.py
@@ -6,8 +6,8 @@ from pretix.base.signals import determine_availability
 @receiver(determine_availability)
 def availability_handler(sender, **kwargs):
     kwargs['sender'] = sender
-    variations = kwargs['variations']
     if sender.settings.testdummy_available is not None:
+        variations = kwargs['variations']
         variations = [d.copy() for d in variations]
         for v in variations:
             v['available'] = (sender.settings.testdummy_available == 'yes')
