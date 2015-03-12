7666d0458085fe0af4e3376d14f7c9997cd36a64
Raphael Michel <mail@raphaelmichel.de>
2015-03-12 23:06:30 +0100

    django-debug-toolbar now supports Django 1.8. Yay!

--- src/pretix/urls.py
+++ src/pretix/urls.py
@@ -14,11 +14,10 @@ urlpatterns = patterns('',
 )
 
 if settings.DEBUG:
-    pass
-    # import debug_toolbar
-    # urlpatterns += patterns('',
-    #     url(r'^__debug__/', include(debug_toolbar.urls)),
-    # )
+    import debug_toolbar
+    urlpatterns += patterns('',
+        url(r'^__debug__/', include(debug_toolbar.urls)),
+    )
 
 urlpatterns += patterns('',
     url(r'', include(pretix.presale.urls, namespace='presale'))
