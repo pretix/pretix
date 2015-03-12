97f5e4a2f1899f9238ef20ce5d261961563b870e
Raphael Michel <mail@raphaelmichel.de>
2015-03-13 00:03:27 +0100

    Migrate to Django 1.8-style URLconfigs
    See
    https://docs.djangoproject.com/en/dev/releases/1.8/#django-conf-urls-patterns
    and #14

--- src/pretix/presale/urls.py
+++ src/pretix/presale/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import patterns, url, include
+from django.conf.urls import url, include
 
 import pretix.presale.views.event
 import pretix.presale.views.cart
@@ -6,24 +6,20 @@ import pretix.presale.views.checkout
 import pretix.presale.views.order
 
 
-urlpatterns = patterns(
-    '',
-    url(r'^(?P<organizer>[^/]+)/(?P<event>[^/]+)/', include(
-        patterns(
-            'pretix.presale.views.event',
-            url(r'^$', pretix.presale.views.event.EventIndex.as_view(), name='event.index'),
-            url(r'^cart/add$', pretix.presale.views.cart.CartAdd.as_view(), name='event.cart.add'),
-            url(r'^cart/remove$', pretix.presale.views.cart.CartRemove.as_view(), name='event.cart.remove'),
-            url(r'^checkout$', pretix.presale.views.checkout.CheckoutStart.as_view(), name='event.checkout.start'),
-            url(r'^checkout/payment$', pretix.presale.views.checkout.PaymentDetails.as_view(),
-                name='event.checkout.payment'),
-            url(r'^checkout/confirm$', pretix.presale.views.checkout.OrderConfirm.as_view(),
-                name='event.checkout.confirm'),
-            url(r'^order/(?P<order>[^/]+)/$', pretix.presale.views.order.OrderDetails.as_view(),
-                name='event.order'),
-            url(r'^order/(?P<order>[^/]+)/cancel$', pretix.presale.views.order.OrderCancel.as_view(),
-                name='event.order.cancel'),
-            url(r'^login$', pretix.presale.views.event.EventLogin.as_view(), name='event.checkout.login'),
-        )
-    )),
-)
+urlpatterns = [
+    url(r'^(?P<organizer>[^/]+)/(?P<event>[^/]+)/', include([
+        url(r'^$', pretix.presale.views.event.EventIndex.as_view(), name='event.index'),
+        url(r'^cart/add$', pretix.presale.views.cart.CartAdd.as_view(), name='event.cart.add'),
+        url(r'^cart/remove$', pretix.presale.views.cart.CartRemove.as_view(), name='event.cart.remove'),
+        url(r'^checkout$', pretix.presale.views.checkout.CheckoutStart.as_view(), name='event.checkout.start'),
+        url(r'^checkout/payment$', pretix.presale.views.checkout.PaymentDetails.as_view(),
+            name='event.checkout.payment'),
+        url(r'^checkout/confirm$', pretix.presale.views.checkout.OrderConfirm.as_view(),
+            name='event.checkout.confirm'),
+        url(r'^order/(?P<order>[^/]+)/$', pretix.presale.views.order.OrderDetails.as_view(),
+            name='event.order'),
+        url(r'^order/(?P<order>[^/]+)/cancel$', pretix.presale.views.order.OrderCancel.as_view(),
+            name='event.order.cancel'),
+        url(r'^login$', pretix.presale.views.event.EventLogin.as_view(), name='event.checkout.login'),
+    ])),
+]
--- src/pretix/urls.py
+++ src/pretix/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import include, url
 from django.contrib import admin
 from django.conf import settings
 
@@ -6,19 +6,19 @@ import pretix.control.urls
 import pretix.presale.urls
 
 
-urlpatterns = patterns('',
+urlpatterns = [
     url(r'^control/', include(pretix.control.urls, namespace='control')),
     url(r'^admin/', include(admin.site.urls)),
     # The pretixpresale namespace is configured at the bottom of this file, because it
     # contains a wildcard-style URL which has to be configured _after_ debug settings.
-)
+]
 
 if settings.DEBUG:
     import debug_toolbar
-    urlpatterns += patterns('',
+    urlpatterns.append(
         url(r'^__debug__/', include(debug_toolbar.urls)),
     )
 
-urlpatterns += patterns('',
+urlpatterns.append(
     url(r'', include(pretix.presale.urls, namespace='presale'))
 )
