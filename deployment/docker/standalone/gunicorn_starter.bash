73a469485b8984775708c33ec846f85d1b8afc61
Raphael Michel <mail@raphaelmichel.de>
2015-05-18 16:28:55 +0200

    Docker standalone: migrate before start

--- deployment/docker/standalone/gunicorn_starter.bash
+++ deployment/docker/standalone/gunicorn_starter.bash
@@ -2,6 +2,7 @@
 cd /pretix/src
 export DJANGO_SETTINGS_MODULE=pretix.settings
 export MEDIA_ROOT=/data/
+python3 manage.py migrate
 gunicorn \
 	-b '0.0.0.0:80' \
 	-w 3 --max-requests 1000 --max-requests-jitter 50 \
