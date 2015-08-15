617de765e08cfbb2c65b847051a01bcf1db301b0
Raphael Michel <mail@raphaelmichel.de>
2015-08-15 23:51:40 +0200

    Fixed download button links in pretix control

--- src/pretix/control/urls.py
+++ src/pretix/control/urls.py
@@ -64,6 +64,8 @@ urlpatterns = [
         url(r'^orders/(?P<code>[0-9A-Z]+)/extend$', orders.OrderExtend.as_view(),
             name='event.order.extend'),
         url(r'^orders/(?P<code>[0-9A-Z]+)/$', orders.OrderDetail.as_view(), name='event.order'),
+        url(r'^orders/(?P<code>[0-9A-Z]+)/download/(?P<output>[^/]+)$', orders.OrderDownload.as_view(),
+            name='event.order.download'),
         url(r'^orders/overview/$', orders.OverView.as_view(), name='event.orders.overview'),
         url(r'^orders/export/$', orders.ExportView.as_view(), name='event.orders.export'),
         url(r'^orders/go$', orders.OrderGo.as_view(), name='event.orders.go'),
