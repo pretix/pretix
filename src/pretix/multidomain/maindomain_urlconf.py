d33e015919c4c57bff0138f4daa8c3467e4da7b5
Raphael Michel <mail@raphaelmichel.de>
2015-11-09 21:32:37 +0100

    Added an global index page

--- src/pretix/multidomain/maindomain_urlconf.py
+++ src/pretix/multidomain/maindomain_urlconf.py
@@ -1,4 +1,5 @@
 from django.conf.urls import include, url
+from django.views.generic import TemplateView
 
 from pretix.presale.urls import (
     event_patterns, locale_patterns, organizer_patterns,
@@ -8,7 +9,8 @@ from pretix.urls import common_patterns
 presale_patterns_main = [
     url(r'', include(locale_patterns + [
         url(r'^(?P<organizer>[^/]+)/(?P<event>[^/]+)/', include(event_patterns)),
-        url(r'^(?P<organizer>[^/]+)/', include(organizer_patterns))
+        url(r'^(?P<organizer>[^/]+)/', include(organizer_patterns)),
+        url(r'^$', TemplateView.as_view(template_name='pretixpresale/index.html'))
     ], namespace='presale'))
 ]
 
