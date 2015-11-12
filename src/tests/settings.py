778ca10676d63861498c49974ee90e0c0341f874
Raphael Michel <mail@raphaelmichel.de>
2015-11-12 18:58:11 +0100

    Fixed test settings

--- src/tests/settings.py
+++ src/tests/settings.py
@@ -4,9 +4,7 @@ TEST_DIR = os.path.dirname(__file__)
 
 TEMPLATES[0]['DIRS'].append(os.path.join(TEST_DIR, 'templates'))
 
-INSTALLED_APPS = INSTALLED_APPS + (
-    'tests.testdummy',
-)
+INSTALLED_APPS.append('tests.testdummy')
 
 MEDIA_ROOT = os.path.join(TEST_DIR, 'media')
 
