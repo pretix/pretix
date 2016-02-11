640fc5f230d92c1c3a33407c275dfbd54279764e
Raphael Michel <mail@raphaelmichel.de>
2016-02-11 18:19:20 +0100

    Item.tax_rate should not be nullable

--- src/pretix/base/models/items.py
+++ src/pretix/base/models/items.py
@@ -1,5 +1,6 @@
 import sys
 from datetime import datetime
+from decimal import Decimal
 
 from django.db import models
 from django.db.models import Q, Case, Count, Sum, When
@@ -132,9 +133,9 @@ class Item(LoggedModel):
         max_digits=7, decimal_places=2, null=True
     )
     tax_rate = models.DecimalField(
-        null=True, blank=True,
         verbose_name=_("Taxes included in percent"),
-        max_digits=7, decimal_places=2
+        max_digits=7, decimal_places=2,
+        default=Decimal('0.00')
     )
     admission = models.BooleanField(
         verbose_name=_("Is an admission ticket"),
