472b3a2f2b95a760f7be03d94ab1ec3849264658
Raphael Michel <mail@raphaelmichel.de>
2016-03-19 16:37:39 +0100

    Fixed problems with less imports

--- src/pretix/helpers/lessabsolutefilter.py
+++ src/pretix/helpers/lessabsolutefilter.py
@@ -1,10 +1,13 @@
 from compressor.filters.base import CompilerFilter
 from compressor.filters.css_default import CssAbsoluteFilter
 
+from pretix.settings import STATIC_ROOT, STATICFILES_DIRS
+
 
 class LessFilter(CompilerFilter):
     def __init__(self, content, attrs, **kwargs):
-        super(LessFilter, self).__init__(content, command='lessc {infile} {outfile}', **kwargs)
+        cmd = 'lessc --include-path=%s {infile} {outfile}' % ":".join(STATICFILES_DIRS + [STATIC_ROOT])
+        super(LessFilter, self).__init__(content, command=cmd, **kwargs)
 
     def input(self, **kwargs):
         content = super(LessFilter, self).input(**kwargs)
