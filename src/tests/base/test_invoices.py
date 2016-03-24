d96da427e12a0eb2e9ee2066f6e6868bce0a32c3
Raphael Michel <mail@raphaelmichel.de>
2016-03-24 14:38:10 +0100

    Fixed invoice test

--- src/tests/base/test_invoices.py
+++ src/tests/base/test_invoices.py
@@ -1,11 +1,12 @@
 from datetime import timedelta
+from decimal import Decimal
 
 import pytest
 from django.utils.timezone import now
-from decimal import Decimal
 
-from pretix.base.models import Event, Organizer, Order, Item, OrderPosition, InvoiceAddress
-from pretix.base.payment import FreeOrderProvider
+from pretix.base.models import (
+    Event, InvoiceAddress, Item, Order, OrderPosition, Organizer,
+)
 from pretix.base.services.invoices import generate_invoice
 
 
@@ -14,7 +15,7 @@ def env():
     o = Organizer.objects.create(name='Dummy', slug='dummy')
     event = Event.objects.create(
         organizer=o, name='Dummy', slug='dummy',
-        date_from=now(), plugins='banktransfer'
+        date_from=now(), plugins='pretix.plugins.banktransfer'
     )
     o = Order.objects.create(
         code='FOO', event=event, email='dummy@dummy.test',
@@ -22,7 +23,7 @@ def env():
         datetime=now(), expires=now() + timedelta(days=10),
         total=0, payment_provider='banktransfer',
         payment_fee=Decimal('0.25'), payment_fee_tax_rate=0,
-        payment_fee_tax_value=0
+        payment_fee_tax_value=0, locale='en'
     )
     ticket = Item.objects.create(event=event, name='Early-bird ticket',
                                  category=None, default_price=23,
@@ -31,7 +32,7 @@ def env():
         order=o,
         item=ticket,
         variation=None,
-        price=Decimal("14"),
+        price=Decimal("23.00"),
     )
     return event, o
 
@@ -89,4 +90,4 @@ def test_positions(env):
     assert last.gross_value == order.payment_fee
     assert last.tax_rate == order.payment_fee_tax_rate
     assert last.tax_value == order.payment_fee_tax_value
-    assert inv.invoice_to == "Acme Company\n\n221B Baker Street\n12345 London\nUK"
+    assert inv.invoice_to == ""
