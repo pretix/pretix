d3e4941a58fae42f683c370a56f536966d7678c9
Raphael Michel <mail@raphaelmichel.de>
2016-06-07 11:55:00 +0200

    Fixed infinite reload loop

--- src/static/pretixbase/js/reloadpending.js
+++ src/static/pretixbase/js/reloadpending.js
@@ -1,6 +1,7 @@
-window.setInterval(function () {
+var intId = window.setInterval(function () {
     $.get(location.href + '?ajax=1', function (data, status) {
         if (data === "1") {
+            window.clearInterval(intId);
             location.reload();
         }
     });
