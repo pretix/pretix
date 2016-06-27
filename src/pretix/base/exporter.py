f68ada1f413c9c713510b6f94ffe83b47340be7c
Raphael Michel <mail@raphaelmichel.de>
2016-06-27 19:23:43 +0200

    Added filters for the MailExporter

--- src/pretix/base/exporter.py
+++ src/pretix/base/exporter.py
@@ -1,9 +1,13 @@
 import json
+from collections import OrderedDict
 
+from django import forms
 from django.core.serializers.json import DjangoJSONEncoder
 from django.dispatch import receiver
+from django.utils.translation import ugettext as _
 from typing import Tuple
 
+from pretix.base.models import Order
 from pretix.base.signals import register_data_exporters
 
 
@@ -164,12 +168,28 @@ class JSONExporter(BaseExporter):
 
 class MailExporter(BaseExporter):
     identifier = 'mailaddrs'
-    verbose_name = 'Email addresses (text file)'
+    verbose_name = _('Email addresses (text file)')
 
     def render(self, form_data: dict):
-        addrs = self.event.orders.values('email')
+        qs = self.event.orders.filter(status__in=form_data['status'])
+        addrs = qs.values('email')
         data = "\r\n".join(set(a['email'] for a in addrs))
         return 'pretixemails.txt', 'text/plain', data.encode("utf-8")
+
+    @property
+    def export_form_fields(self):
+        return OrderedDict(
+            [
+                ('status',
+                 forms.MultipleChoiceField(
+                     label=_('Filter by status'),
+                     initial=[Order.STATUS_PENDING, Order.STATUS_PAID],
+                     choices=Order.STATUS_CHOICE,
+                     widget=forms.CheckboxSelectMultiple,
+                     required=False
+                 )),
+            ]
+        )
 
 
 @receiver(register_data_exporters, dispatch_uid="exporter_json")
