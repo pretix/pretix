pretix/pretix

commit 4fa631ab97ae663b2687e7bd8c68223d2fb7b49a

Assert that the item count is a positive integer (#221)
Thanks to David Gullasch for pointing this one out.

src/pretix/presale/views/cart.py
@@ -56,13 +56,23 @@ class CartActionMixin:
                 parts = parts[:-1]
             else:
                 voucher = None
+
+            try:
+                amount = int(value)
+            except ValueError:
+                messages.error(self.request, _('Please enter numbers only.'))
+                return []
+            if amount <= 0:
+                messages.error(self.request, _('Please enter positive numbers only.'))
+                return []
+
             price = self.request.POST.get('price_' + "_".join(parts[1:]), "")
             if key.startswith('item_'):
                 try:
                     items.append({
                         'item': int(parts[1]),
                         'variation': None,
-                        'count': int(value),
+                        'count': amount,
                         'price': price,
                         'voucher': voucher
                     })
@@ -74,7 +84,7 @@ class CartActionMixin:
                     items.append({
                         'item': int(parts[1]),
                         'variation': int(parts[2]),
-                        'count': int(value),
+                        'count': amount,
                         'price': price,
                         'voucher': voucher
                     })

commit c913e448032a5130bd5805556a84911c9b56f4bc

Fixed a problem with custom prices introduced in 4fa631a

src/pretix/presale/views/cart.py
@@ -57,6 +57,9 @@ class CartActionMixin:
             else:
                 voucher = None
 
+            if not key.startswith('item_') and not key.startswith('variation_'):
+                continue
+
             try:
                 amount = int(value)
             except ValueError: