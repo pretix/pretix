090d25aba05c5957c26416b809ac277b603c75d0
Raphael Michel <mail@raphaelmichel.de>
2016-09-01 11:10:48 +0200

    Added examples to i18n documentation, run doctests

---
+++ .travis.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+set -e
+set -x
+
+echo "Executing job $1"
+
+if [ "$1" == "style" ]; then
+	XDG_CACHE_HOME=/cache pip3 install -Ur src/requirements.txt -r src/requirements/dev.txt -r src/requirements/py34.txt
+	cd src
+    flake8 --ignore=E123,F403,F401,N802,C901,W503,E402 .
+    isort -c -rc .
+fi
+if [ "$1" == "doctests" ]; then
+	XDG_CACHE_HOME=/cache pip3 install -Ur doc/requirements.txt -r src/requirements/py34.txt
+	cd doc
+	make doctest
+fi
+if [ "$1" == "tests" ]; then
+	pip3 install -r src/requirements.txt -Ur src/requirements/dev.txt -r src/requirements/py34.txt
+	cd src
+	python manage.py check
+	make all compress
+	coverage run -m py.test tests && coverage report
+fi
+if [ "$1" == "tests-cov" ]; then
+	pip3 install -r src/requirements.txt -Ur src/requirements/dev.txt -r src/requirements/py34.txt
+	cd src
+	python manage.py check
+	make all compress
+	coverage run -m py.test tests && coveralls
+fi
