pretix/pretix

commit 833c31022fc0ec39359f9ace39691f4393315750

Banktransfer: Save imported date into CharField, date format autodetection never works

src/pretix/plugins/banktransfer/tasks.py
@@ -3,7 +3,6 @@ import logging
 import re
 from decimal import Decimal
 
-import dateutil.parser
 from django.conf import settings
 from django.db import transaction
 from django.utils.translation import ugettext_noop
@@ -73,12 +72,11 @@ def _get_unknown_transactions(event: Event, job: BankImportJob, data: list):
             logger.exception('Could not parse amount of transaction: {}'.format(amount))
             amount = Decimal("0.00")
 
-        date = dateutil.parser.parse(row['date'])
         trans = BankTransaction(event=event, import_job=job,
                                 payer=row['payer'],
                                 reference=row['reference'],
                                 amount=amount,
-                                date=date)
+                                date=row['date'])
         trans.checksum = trans.calculate_checksum()
         if trans.checksum not in known_checksums:
             trans.state = BankTransaction.STATE_UNCHECKED

src/pretix/plugins/banktransfer/migrations/0002_auto_20160908_2020.py
@@ -1,0 +1,31 @@
+# -*- coding: utf-8 -*-
+# Generated by Django 1.9.4 on 2016-09-08 20:20
+from __future__ import unicode_literals
+
+import django.db.models.deletion
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('banktransfer', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='bankimportjob',
+            name='state',
+            field=models.CharField(choices=[('pending', 'pending'), ('running', 'running'), ('error', 'error'), ('completed', 'completed')], default='pending', max_length=32),
+        ),
+        migrations.AlterField(
+            model_name='banktransaction',
+            name='date',
+            field=models.CharField(max_length=50),
+        ),
+        migrations.AlterField(
+            model_name='banktransaction',
+            name='import_job',
+            field=models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='transactions', to='banktransfer.BankImportJob'),
+        ),
+    ]

src/pretix/plugins/banktransfer/models.py
@@ -48,7 +48,7 @@ class BankTransaction(models.Model):
     payer = models.TextField(blank=True)
     reference = models.TextField(blank=True)
     amount = models.DecimalField(max_digits=10, decimal_places=2)
-    date = models.DateField()
+    date = models.CharField(max_length=50)
     order = models.ForeignKey('pretixbase.Order', null=True, blank=True)
 
     def calculate_checksum(self):
@@ -57,7 +57,7 @@ class BankTransaction(models.Model):
         hasher.update(clean.sub('', self.payer.lower()).encode('utf-8'))
         hasher.update(clean.sub('', self.reference.lower()).encode('utf-8'))
         hasher.update(clean.sub('', str(self.amount).lower()).encode('utf-8'))
-        hasher.update(clean.sub('', str(self.date).lower()).encode('utf-8'))
+        hasher.update(clean.sub('', self.date.lower()).encode('utf-8'))
         return str(hasher.hexdigest())
 
     def shred_private_data(self):

src/pretix/plugins/banktransfer/static/pretixplugins/banktransfer/ui.css
@@ -1,10 +1,3 @@
 table.transaction-list td.actions {
-    max-width: 100px;
-}
-table.transaction-list td.actions .input-group {
-    display: inline-block;
-    width: 110px;
-}
-table.transaction-list td.actions .input-group button {
-    line-height: 1.43;
+    width: 100px;
 }

src/pretix/plugins/banktransfer/templates/pretixplugins/banktransfer/transaction_list.html
@@ -53,7 +53,7 @@
                         </div>
                     {% endif %}
                 </td>
-                <td>{{ trans.date|date:"SHORT_DATE_FORMAT" }}</td>
+                <td>{{ trans.date }}</td>
                 <td>{{ trans.payer }}<br/>{{ trans.reference }}</td>
                 <td>{{ trans.amount|floatformat:2 }}</td>
                 <td>

commit bb1cd6bcec9e1ad94f78fa2bac8a88078b4354f4

Banktransfer: Fixed issue introduced in 833c310

src/pretix/plugins/banktransfer/tasks.py
@@ -44,7 +44,7 @@ def _handle_transaction(event: Event, trans: BankTransaction, code: str):
         try:
             mark_order_paid(trans.order, provider='banktransfer', info=json.dumps({
                 'reference': trans.reference,
-                'date': trans.date.isoformat(),
+                'date': trans.date,
                 'payer': trans.payer,
                 'trans_id': trans.pk
             }))