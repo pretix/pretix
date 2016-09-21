0e4a232d5d717df4d509c5f85e55a30d2ba94cc2
Raphael Michel <mail@raphaelmichel.de>
2016-09-21 19:07:26 +0200

    Always ignore redis in development

--- src/tests/settings.py
+++ src/tests/settings.py
@@ -16,4 +16,16 @@ COMPRESS_ENABLED = COMPRESS_OFFLINE = False
 
 PASSWORD_HASHERS = ['django.contrib.auth.hashers.MD5PasswordHasher']
 
+# Disable celery
 CELERY_ALWAYS_EAGER = True
+HAS_CELERY = False
+
+# Don't use redis
+SESSION_ENGINE = "django.contrib.sessions.backends.db"
+HAS_REDIS = False
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
+        'LOCATION': 'unique-snowflake',
+    }
+}
