c736690227d486794afa28ff826b84b5b04df23e
Jonas Große Sundrup <cherti@letopolis.de>
2016-10-16 00:52:25 +0200

    implement HTTP basic auth in front of metrics-endpoint

--- src/pretix/base/views/metrics.py
+++ src/pretix/base/views/metrics.py
@@ -1,8 +1,28 @@
+from django.conf import settings
 from http import HttpResponse
 from .. import metrics
 
+unauthedResponse = HttpResponse("<html><title>Forbidden</title><body>You are not authorized to view this page.</body></html>", mimetype="text/html")
+unauthedResponse["WWW-Authenticate"] = 'Basic realm="metrics"'
+unauthedResponse.status_code = 401
 
 def serve_metrics(request):
+    # first check if the user is properly authorized:
+    if not request.META.has_key("HTTP_AUTHORIZATION"):
+        return unauthedResponse
+
+    method, credentials = request.META["HTTP_AUTHORIZATION"].split(" ", 1)
+    if method.lower() != "basic":
+        return unauthedResponse
+
+    user, passphrase = credentials.strip().decode("base64").split(":", 1)
+
+    if user != settings.METRICS_USER:
+        return unauthedResponse
+    if passphrase != settings.METRICS_PASSPHRASE:
+        return unauthedResponse
+
+    # ok, the request passed the authentication-barrier, let's hand out the metrics:
     m = metrics.metric_values()
 
     output = []
