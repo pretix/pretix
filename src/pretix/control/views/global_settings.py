acab1af0e9bebeea011de1be472f298ddedd862b
Raphael Michel <mail@raphaelmichel.de>
2016-11-01 17:05:29 +0100

    Add feedback to global settings

--- src/pretix/control/views/global_settings.py
+++ src/pretix/control/views/global_settings.py
@@ -1,4 +1,6 @@
+from django.contrib import messages
 from django.shortcuts import reverse
+from django.utils.translation import ugettext_lazy as _
 from django.views.generic import FormView
 
 from pretix.control.forms.global_settings import GlobalSettingsForm
@@ -11,7 +13,12 @@ class GlobalSettingsView(AdministratorPermissionRequiredMixin, FormView):
 
     def form_valid(self, form):
         form.save()
+        messages.success(self.request, _('Your changes have been saved.'))
         return super().form_valid(form)
+
+    def form_invalid(self, form):
+        messages.error(self.request, _('Your changes have not been saved, see below for errors.'))
+        return super().form_invalid(form)
 
     def get_success_url(self):
         return reverse('control:global-settings')
