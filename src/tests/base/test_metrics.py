db176995f1837b1194fb67ee0aee83b7c2938345
Jonas Große Sundrup <cherti@letopolis.de>
2016-11-20 12:21:06 +0100

    implement upstream suggestions, round 3

--- src/tests/base/test_metrics.py
+++ src/tests/base/test_metrics.py
@@ -3,9 +3,10 @@
 from django.test import override_settings
 
 from pretix.base import metrics
+from pretix.base.views import metrics as metricsview
 
 
-class fake_redis(object):
+class fakeRedis(object):
 
     def __init__(self):
         self.storage = {}
@@ -27,15 +28,19 @@ class fake_redis(object):
 @override_settings(HAS_REDIS=True)
 def test_counter(monkeypatch):
 
-    local_fake_redis = fake_redis()
+    fake_redis = fakeRedis()
 
-    monkeypatch.setattr(metrics, "redis", local_fake_redis, raising=False)
+    monkeypatch.setattr(metrics, "redis", fake_redis, raising=False)
 
     # now test
-    fullname = metrics.http_requests_total._construct_metric_identifier('http_requests_total', {"code": "200", "handler": "/foo", "method": "GET"})
+    fullname_GET  = metrics.http_requests_total._construct_metric_identifier('http_requests_total', {"code": "200", "handler": "/foo", "method": "GET"})
+    fullname_POST = metrics.http_requests_total._construct_metric_identifier('http_requests_total', {"code": "200", "handler": "/foo", "method": "POST"})
     metrics.http_requests_total.inc(code="200", handler="/foo", method="GET")
-    assert local_fake_redis.storage[metrics.REDIS_KEY_PREFIX + fullname] == 1
+    assert local_fake_redis.storage[metrics.REDIS_KEY_PREFIX + fullname_GET] == 1
     metrics.http_requests_total.inc(code="200", handler="/foo", method="GET")
-    assert local_fake_redis.storage[metrics.REDIS_KEY_PREFIX + fullname] == 2
+    assert local_fake_redis.storage[metrics.REDIS_KEY_PREFIX + fullname_GET] == 2
     metrics.http_requests_total.inc(7, code="200", handler="/foo", method="GET")
-    assert local_fake_redis.storage[metrics.REDIS_KEY_PREFIX + fullname] == 9
+    assert local_fake_redis.storage[metrics.REDIS_KEY_PREFIX + fullname_GET] == 9
+    metrics.http_requests_total.inc(7, code="200", handler="/foo", method="POST")
+    assert local_fake_redis.storage[metrics.REDIS_KEY_PREFIX + fullname_GET] == 9
+    assert local_fake_redis.storage[metrics.REDIS_KEY_PREFIX + fullname_POST] == 7
