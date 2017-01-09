ffa35a9b9b2e40446c8bc7022a34348e2e0db5aa
Raphael Michel <mail@raphaelmichel.de>
2017-01-09 20:20:02 +0100

    Fix Django version in PyPI classifier

--- src/setup.py
+++ src/setup.py
@@ -56,7 +56,7 @@ setup(
         'License :: OSI Approved :: Apache Software License',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
-        'Framework :: Django :: 1.8'
+        'Framework :: Django :: 1.10'
     ],
 
     keywords='tickets web shop ecommerce',
