3dd2492926a2841718b7f21a6ccefbc524cb5a5f
Raphael Michel <mail@raphaelmichel.de>
2017-04-17 17:13:18 +0200

    Fix a broken import

--- src/pretix/presale/management/commands/updatestyles.py
+++ src/pretix/presale/management/commands/updatestyles.py
@@ -1,6 +1,6 @@
 from django.core.management.base import BaseCommand
 
-from pretix.base.models import EventSetting
+from pretix.base.models import Event_SettingsStore
 
 from ...style import regenerate_css
 
@@ -9,5 +9,5 @@ class Command(BaseCommand):
     help = "Re-generate all custom stylesheets"
 
     def handle(self, *args, **options):
-        for es in EventSetting.objects.filter(key="presale_css_file"):
+        for es in Event_SettingsStore.objects.filter(key="presale_css_file"):
             regenerate_css.apply_async(args=(es.object_id,))
