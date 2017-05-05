be5fc78f8d697049eb57c6951f905fd3a80d7dc7
aph3rson <aph3rson@gmail.com>
2017-05-04 17:58:08 -0700

    Added non-admission setting to event
    `ticket_download_nonadm` now setting in storage. Still need logic for
    order page/PDF generation.
    Works on pretix/pretix#456.

--- src/pretix/base/settings.py
+++ src/pretix/base/settings.py
@@ -163,6 +163,10 @@ DEFAULTS = {
         'default': 'False',
         'type': bool
     },
+    'ticket_download_nonadm': {
+        'default': 'True',
+        'type': bool
+    },
     'last_order_modification_date': {
         'default': None,
         'type': datetime
