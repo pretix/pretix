d27a0c0deeb49f7b8e112891c9164c9662593719
Raphael Michel <mail@raphaelmichel.de>
2017-06-06 09:24:29 +0200

    Fix waiting list widget

--- src/pretix/control/views/dashboards.py
+++ src/pretix/control/views/dashboards.py
@@ -98,9 +98,9 @@ def waitinglist_widgets(sender, **kwargs):
         for wle in wles:
             if (wle.item, wle.variation) not in itemvar_cache:
                 itemvar_cache[(wle.item, wle.variation)] = (
-                    wle.variation.check_quotas(count_waitinglist=False, _cache=quota_cache)
+                    wle.variation.check_quotas(subevent=wle.subevent, count_waitinglist=False, _cache=quota_cache)
                     if wle.variation
-                    else wle.item.check_quotas(count_waitinglist=False, _cache=quota_cache)
+                    else wle.item.check_quotas(subevent=wle.subevent, count_waitinglist=False, _cache=quota_cache)
                 )
             row = itemvar_cache.get((wle.item, wle.variation))
             if row[1] > 0:
