02786f4801b3a39e775b5dc917cc0e5a38142ab7
Daniel <daniellee468@gmail.com>
2017-07-17 18:29:47 +0800

    Fix #552 -- Add icon to expansible panels (#560)
    * Ref #552 -- Add icon to expansible panels
    
    Offer informative feedback to user by showing icons to indicate
    expansible panels.
    
    * Fix wrongly removed JavaScript line

--- src/pretix/static/pretixpresale/js/ui/main.js
+++ src/pretix/static/pretixpresale/js/ui/main.js
@@ -26,7 +26,7 @@ $(function () {
         $(this).parent().parent().parent().find(".variations").slideToggle();
         e.preventDefault();
     });
-    $(".collapsed").removeClass("collapsed").addClass("collapse");
+     $("div.collapsed").removeClass("collapsed").addClass("collapse");
 
     $("#voucher-box").hide();
     $("#voucher-toggle").show();
