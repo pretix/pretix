64b67e5396d3fe6d151a98a513f441dcb25a4095
Raphael Michel <mail@raphaelmichel.de>
2017-10-17 18:06:56 +0200

    Reduce number of calls to domain cache

--- src/pretix/multidomain/urlreverse.py
+++ src/pretix/multidomain/urlreverse.py
@@ -7,14 +7,17 @@ from pretix.base.models import Event, Organizer
 
 
 def get_domain(organizer):
-    c = organizer.get_cache()
-    domain = c.get('domain')
+    domain = getattr(organizer, '_cached_domain', None) or organizer.cache.get('domain')
     if domain is None:
         domains = organizer.domains.all()
         domain = domains[0].domainname if domains else None
-        c.set('domain', domain or 'none')
+        organizer.cache.set('domain', domain or 'none')
+        organizer._cached_domain = domain or 'none'
     elif domain == 'none':
+        organizer._cached_domain = 'none'
         return None
+    else:
+        organizer._cached_domain = domain
     return domain
 
 
