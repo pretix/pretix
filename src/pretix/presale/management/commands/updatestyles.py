fe61e4f3e2ccb8e2c04b7211abaca53ca8db5b20
Raphael Michel <mail@raphaelmichel.de>
2017-10-26 23:39:45 +0200

    updatestyles should also update organizer styles

--- src/pretix/presale/management/commands/updatestyles.py
+++ src/pretix/presale/management/commands/updatestyles.py
@@ -1,8 +1,8 @@
 from django.core.management.base import BaseCommand
 
-from pretix.base.models import Event_SettingsStore
+from pretix.base.models import Event_SettingsStore, Organizer_SettingsStore
 
-from ...style import regenerate_css
+from ...style import regenerate_css, regenerate_organizer_css
 
 
 class Command(BaseCommand):
@@ -11,3 +11,5 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         for es in Event_SettingsStore.objects.filter(key="presale_css_file"):
             regenerate_css.apply_async(args=(es.object_id,))
+        for es in Organizer_SettingsStore.objects.filter(key="presale_css_file"):
+            regenerate_organizer_css.apply_async(args=(es.object_id,))
