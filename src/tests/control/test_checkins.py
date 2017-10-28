dde1bd2e05a62cbff2cb29849a064d91dfff13d3
Raphael Michel <mail@raphaelmichel.de>
2017-10-28 23:13:59 +0200

    Improve unit test to check the result of the action

--- src/tests/control/test_checkins.py
+++ src/tests/control/test_checkins.py
@@ -5,8 +5,8 @@ import pytest
 from django.utils.timezone import now
 
 from pretix.base.models import (
-    Checkin, Event, Item, ItemAddOn, ItemCategory, Order, OrderPosition,
-    Organizer, Team, User,
+    Checkin, Event, Item, ItemAddOn, ItemCategory, LogEntry, Order,
+    OrderPosition, Organizer, Team, User,
 )
 from pretix.control.views.dashboards import checkin_widget
 
@@ -270,20 +270,16 @@ def test_checkins_list_mixed(client, checkin_list_env, query, expected):
 
 
 @pytest.mark.django_db
-@pytest.mark.parametrize("query, expected", [
-    ('status=&item=&user=', ['A1Ticket', 'A1Mascot', 'A2Ticket', 'A3Ticket']),
-    ('status=1&item=&user=', ['A1Ticket', 'A2Ticket', 'A3Ticket']),
-])
-def test_manual_checkins(client, checkin_list_env, query, expected):
+def test_manual_checkins(client, checkin_list_env):
     client.login(email='dummy@dummy.dummy', password='dummy')
-    response = client.post('/control/event/dummy/dummy/checkins/', {
+    assert not checkin_list_env[5][3].checkins.exists()
+    client.post('/control/event/dummy/dummy/checkins/', {
         'checkin': [checkin_list_env[5][3].pk]
     })
-    response = client.get('/control/event/dummy/dummy/checkins/?' + query)
-    qs = response.context['entries']
-    item_keys = [q.order.code + str(q.item.name) for q in qs]
-    print(item_keys)
-    assert item_keys == expected
+    assert checkin_list_env[5][3].checkins.exists()
+    assert LogEntry.objects.filter(
+        action_type='pretix.control.views.checkin', object_id=checkin_list_env[5][3].order.pk
+    ).exists()
 
 
 @pytest.fixture
