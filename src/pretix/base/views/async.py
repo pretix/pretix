831e31ea9d1871ac968090e322aa161ada3644eb
Jakob Schnell <github@ezelo.de>
2017-11-03 11:46:35 +0100

    occured -> occurred (#660)
    * Fix duplicate source string
    
    * occured -> occurred
    
    All resources I could find listed this as misspelled, but I wasn't too
    sure…
    Also, it should be checked if all changes to the .po-files are respected
    in the corresponding src-files.

--- src/pretix/base/views/async.py
+++ src/pretix/base/views/async.py
@@ -133,7 +133,7 @@ class AsyncAction:
             return str(exception)
         else:
             logger.error('Unexpected exception: %r' % exception)
-            return _('An unexpected error has occured.')
+            return _('An unexpected error has occurred.')
 
     def get_success_message(self, value):
         return _('The task has been completed.')
