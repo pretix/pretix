e4167380b92e98e3d00c344dca700fa6da47ad8f
Ture Gjørup <ture@ignatz.dk>
2017-11-16 18:39:10 +0100

    API: Write methods for quotas (#657)
    * MKBDIGI-183: Added quotas API write methods
    
    * MKBDIGI-183: Fixed code formatting
    
    * MKBDIGI-183: Added test for permission requirements
    
    * MKBDIGI-183: Documentation corrections
    
    * MKBDIGI-183: Removed redundant create/update locks
    
    * MKBDIGI-183: Added quota validation to check that items and variations corresponds to each other
    
    * MKBDIGI-183: Added quota validation to check that item belong to the same event as the endpoint
    
    * MKBDIGI-183: Added subevent validation to check that subevent belong to the same event as the endpoint
    
    * MKBDIGI-183: Added subevent validation to check that subevent is null for non-series events
    
    * MKBDIGI-183: Changed validation error text
    
    * MKBDIGI-183: Added logging for subevents
    
    * MKBDIGI-183: Fixed code formatting
    
    * MKBDIGI-183: Fixed validation error in API test
    
    * MKBDIGI-183: Fixed documentation errors
    
    * MKBDIGI-183: Fixed typos in validation messages
    
    * MKBDIGI-183: Refactored validation loop vars check
    
    * MKBDIGI-183: Updated error strings in test assersions
    
    * MKBDIGI-183: Fixed logging for API quota update to account changing subevents

--- src/tests/api/conftest.py
+++ src/tests/api/conftest.py
@@ -34,6 +34,28 @@ def event(organizer, meta_prop):
 
 
 @pytest.fixture
+def event2(organizer, meta_prop):
+    e = Event.objects.create(
+        organizer=organizer, name='Dummy2', slug='dummy2',
+        date_from=datetime(2017, 12, 27, 10, 0, 0, tzinfo=UTC),
+        plugins='pretix.plugins.banktransfer,pretix.plugins.ticketoutputpdf'
+    )
+    e.meta_values.create(property=meta_prop, value="Conference")
+    return e
+
+
+@pytest.fixture
+def event3(organizer, meta_prop):
+    e = Event.objects.create(
+        organizer=organizer, name='Dummy3', slug='dummy3',
+        date_from=datetime(2017, 12, 27, 10, 0, 0, tzinfo=UTC),
+        plugins='pretix.plugins.banktransfer,pretix.plugins.ticketoutputpdf'
+    )
+    e.meta_values.create(property=meta_prop, value="Conference")
+    return e
+
+
+@pytest.fixture
 def team(organizer):
     return Team.objects.create(
         organizer=organizer,
@@ -65,8 +87,17 @@ def token_client(client, team):
 def subevent(event, meta_prop):
     event.has_subevents = True
     event.save()
-    se = event.subevents.create(name="Foobar",
-                                date_from=datetime(2017, 12, 27, 10, 0, 0, tzinfo=UTC))
+    se = event.subevents.create(name="Foobar", date_from=datetime(2017, 12, 27, 10, 0, 0, tzinfo=UTC))
+
+    se.meta_values.create(property=meta_prop, value="Workshop")
+    return se
+
+
+@pytest.fixture
+def subevent2(event2, meta_prop):
+    event2.has_subevents = True
+    event2.save()
+    se = event2.subevents.create(name="Foobar", date_from=datetime(2017, 12, 27, 10, 0, 0, tzinfo=UTC))
 
     se.meta_values.create(property=meta_prop, value="Workshop")
     return se
