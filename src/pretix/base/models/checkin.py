3d74c1c1f7cf8a1a18934bf7237a29e52cd62da6
Raphael Michel <mail@raphaelmichel.de>
2017-11-24 18:25:27 +0100

    Data model and migration

--- src/pretix/base/models/checkin.py
+++ src/pretix/base/models/checkin.py
@@ -1,5 +1,15 @@
 from django.db import models
 from django.utils.timezone import now
+from django.utils.translation import pgettext_lazy, ugettext_lazy as _
+
+
+class CheckinList(models.Model):
+    event = models.ForeignKey('Event', related_name='checkin_lists')
+    name = models.CharField(max_length=190)
+    all_products = models.BooleanField(default=True, verbose_name=_("All products (including newly created ones)"))
+    limit_products = models.ManyToManyField('Item', verbose_name=_("Limit to products"), blank=True)
+    subevent = models.ForeignKey('SubEvent', null=True, blank=True,
+                                 verbose_name=pgettext_lazy('subevent', 'Date'))
 
 
 class Checkin(models.Model):
@@ -9,3 +19,6 @@ class Checkin(models.Model):
     position = models.ForeignKey('pretixbase.OrderPosition', related_name='checkins')
     datetime = models.DateTimeField(default=now)
     nonce = models.CharField(max_length=190, null=True, blank=True)
+    list = models.ForeignKey(
+        'pretixbase.CheckinList', related_name='checkins', on_delete=models.PROTECT,
+    )
