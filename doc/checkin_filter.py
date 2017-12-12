462f79dc7c68059d92530e76d4ceffb17756b71a
koebi <koebi@ezelo.de>
2017-12-12 15:40:37 +0100

    split on occurrence of "checkin_*"
    Instead of skipping when encountering "checkin_*", we now split and only
    pass the second part to the spell-checker. This fixes the aforementioned
    problem.

--- doc/checkin_filter.py
+++ doc/checkin_filter.py
@@ -1,9 +1,11 @@
-from enchant.tokenize import Filter
+from enchant.tokenize import get_tokenizer, Filter, unit_tokenize
 
 class CheckinFilter(Filter):
     """ If a word looks like checkin_count, it refers to a so-called variable in
     the code, and is treated as being spelled right."""
 
-    def _skip(self, word):
+    def _split(self, word):
         if word[:8] == "checkin_":
-            return True
+            return unit_tokenize(word[8:])
+
+        return unit_tokenize(word)
