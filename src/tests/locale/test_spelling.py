04a5803bac8614abcc0274f843f0d1acb8a2e51b
koebi <koebi@ezelo.de>
2017-12-21 23:11:39 +0100

    extend checks to all files

--- src/tests/locale/test_spelling.py
+++ src/tests/locale/test_spelling.py
@@ -12,35 +12,46 @@ class PythonFormatFilter(Filter):
             return True
         return False
 
-locales = os.path.dirname(__file__) + '../../pretix/locale'
+# The languages dict maps the language of the po-file to the the dictionary language used by pyenchant
+languages = {
+        "de": "de_DE",
+        "en": "en_US",
+}
+
+# locales is the directory containing the translations
+# ignores is the directory containing the ignore-files
+# These wordlists only need to contain uncapitalized words, the corresponding capitalized words are automatically recognized.
+# TODO: make this more python-y
+locales = '../../pretix/locale'
+ignores = os.listdir(".")
+
+class Check:
+    def __init__(self, path):
+        self.popath = path
+        self.po = polib.pofile(path)
+        lang = self.po.metadata["Language"]
+        checklang = languages[lang]
+        self.get_ignorefile(lang)
+        check_dict = ec.DictWithPWL(checklang, pwl=self.ignore)
+        self.checker = SpellChecker(check_dict, chunkers=[HTMLChunker], filters=[PythonFormatFilter])
+        self.output = open('_build/' + lang + '_output.txt', 'w')
+
+    def get_ignorefile(self, lang):
+        for f in ignores:
+            if "ignore" in f and lang in f:
+                self.ignore = f
+
+# checks contains tuples of po-files and corresponding checkers
+checks = []
+
 for root, dirs, files in os.walk(locales):
     for f in files:
         if f.endswith(".po"):
-            print(os.path.join(root, f))
+            checks.append(Check(os.path.join(root, f)))
 
-quit()
-# TODO: expand to all files
-# idee: dict mit mapping von po.metadata["Language"] -> Language Code, den wir
-# an ec.DictWithPWL geben können, und mit dem die entsprechende ignore-file
-# bezeichnet wird
-# Dann können wir alle .po-files in "pretix/locale" finden, hauen die in eine Datenstruktur, die jeder po-file einen checker zuordnet, und lassen einen check laufen
-
-
-popath = '/home/koebi/github/pretix/src/pretix/locale/de/LC_MESSAGES/django.po'
-ignore_en = '/home/koebi/github/pretix/src/tests/locale/ignore_en.txt'
-ignore_de = '/home/koebi/github/pretix/src/tests/locale/ignore_de.txt'
-po = polib.pofile(popath)
-
-# TODO: recognize language automatically
-lang = po.metadata["Language"]
-
-# The wordlists only need to contain uncapitalized words, the corresponding
-# capitalized words are automatically recognized.
-en_dict = ec.DictWithPWL("en_US", pwl=ignore_en)
-de_dict = ec.DictWithPWL("de", pwl = ignore_de)
-
+en_dict = ec.DictWithPWL("en_US", pwl='./ignore_en.txt')
 en_ckr = SpellChecker(en_dict, chunkers=[HTMLChunker], filters=[PythonFormatFilter])
-de_ckr = SpellChecker(de_dict, chunkers=[HTMLChunker], filters=[PythonFormatFilter])
+output = open('_build/en_output.txt', 'w')
 
 try:
     print('Creating build directory…')
@@ -48,31 +59,26 @@ try:
 except FileExistsError:
     print('Build directory already exists.')
 
-of_en = open('_build/en_output.txt', 'w')
-of_de = open('_build/de_output.txt', 'w')
+for c in checks:
+    for entry in c.po:
+        if entry.obsolete:
+            continue
 
-flags = []
-
-for entry in po:
-    if entry.obsolete:
-        continue
-
-    en_ckr.set_text(entry.msgid)
-    for err in en_ckr:
-        print("ERROR:", popath, ":", entry.linenum, ":", err.word)
-        of_en.write("ERROR:" + popath + ":" + str(entry.linenum) + ":" + err.word + "\n")
+        en_ckr.set_text(entry.msgid)
+        for err in en_ckr:
+            print("ERROR:", c.popath, ":", entry.linenum, ":", err.word)
+            output.write("ERROR:" + c.popath + ":" + str(entry.linenum) + ":" + err.word + "\n")
 
 
-    de_ckr.set_text(entry.msgstr)
-    for err in de_ckr:
-        print("ERROR:", popath, ":", entry.linenum, ":", err.word)
-        of_de.write("ERROR:" + popath + ":" + str(entry.linenum) + ":" + err.word + "\n")
+        c.checker.set_text(entry.msgstr)
+        for err in c.checker:
+            print("ERROR:", c.popath, ":", entry.linenum, ":", err.word)
+            c.output.write("ERROR:" + c.popath + ":" + str(entry.linenum) + ":" + err.word + "\n")
 
-print("Spell-checking done. You can find the outputs in", of_en.name, "and", of_de.name, ".")
+print("Spell-checking done. You can find the outputs in \"_build/<lang>_output\".")
 # TODO: extend english wordlist
 # TODO: extend german wordlist
 # TODO: use python test library to make good tests
 
 # For reference: How sphinxcontrib-spelling reports errors:
 # INFO 2017-12-13 10:26:55,977 sphinx.application logging api/resources/orders.rst:112:checkins:["chickens", "checking", "check ins", "check-ins", "kitchens", "checkin"]
-
