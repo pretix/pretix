cf1f33c61c487d1f9d710fe505698367c76ba8e8
koebi <koebi@ezelo.de>
2017-12-23 13:46:03 +0100

    refactored in review, changed overall structure\n\nThe script now has all the maybe-configurable variables on top, they may be moved to a config-file in the process of including this properly as a test (which is what I intend to do and why it lives in the tests-dir ;). Then the two used classes follow, then the rest. I am more or less happy with the structure, I'd still like to know if this is the way to go, particularly in the pretix-test-context

--- src/tests/locale/test_spelling.py
+++ src/tests/locale/test_spelling.py
@@ -5,7 +5,33 @@ from enchant.checker import SpellChecker
 from enchant.tokenize import Filter, HTMLChunker
 from shutil import rmtree
 
+
+# The languages dict maps the language of the po-file to the the dictionary
+# language used by pyenchant
+LANGUAGES = {
+        "de": "de_DE",
+        "en": "en_US",
+}
+
+# LOCALES_DIR is the directory that contains the po-files per language.
+# It follows the structure <lang>/LC_MESSAGES/django{js}.po where <lang> is the
+# language that is translated into.
+LOCALES_DIR = os.path.abspath('../../pretix/locale')
+
+# IGNORES_DIR is the directory containing the lists with words to be ignored by
+# the spellchecker, since they are spelled correctly.  The wordlists only need
+# to contain uncapitalized words, the corresponding capitalized words are
+# automatically recognized.
+IGNORES_DIR = os.getcwd()
+
+# BUILD_DIR is the directory that the output-files should be written to.
+BUILD_DIR = os.path.abspath('./_build')
+
 class PythonFormatFilter(Filter):
+    """
+    We need to filter the python-format-words such as %(redeemed)s so that they
+    don't get spell-checked.
+    """
     def _skip(self, word):
         if word[:2] == "%(":
             return True
@@ -13,39 +39,31 @@ class PythonFormatFilter(Filter):
             return True
         return False
 
-# The languages dict maps the language of the po-file to the the dictionary language used by pyenchant
-languages = {
-        "de": "de_DE",
-        "en": "en_US",
-}
-
-# These wordlists in the IGNORES_DIR only need to contain uncapitalized words,
-# the corresponding capitalized words are automatically recognized.
-LOCALES_DIR = os.path.abspath('../../pretix/locale')
-IGNORES_DIR = os.getcwd()
-BUILD_DIR = os.path.abspath('./_build')
-
-try:
-    print('Creating build directory at' + BUILD_DIR + '…')
-    os.mkdir(BUILD_DIR)
-except FileExistsError:
-    print("File or directory" + BUILD_DIR + "already exists, deleting…")
-    rmtree(BUILD_DIR)
-    print('Recreating build directory')
-    os.mkdir(BUILD_DIR)
-    print('Build directory done')
 
 class Check:
+    """
+    A Check represents a po-file and a corresponding spellchecker for the
+    po-files language. It also handles the output-file and some metadata.
+
+    :param popath: The path to the po-file
+    :type popath: path
+    :param po: The po-file itself
+    :type po: pofile
+    :param checker: The spellchecker corresponding to the po-file's language
+    :type checker: SpellChecker
+    :param output_file: The file to write the checks output to
+    :type output_file: file
+    """
     def __init__(self, path):
         self.popath = path
         self.po = polib.pofile(path)
         lang = self.po.metadata["Language"]
-        checklang = languages[lang]
-        self.get_ignorefile(lang)
-        check_dict = ec.DictWithPWL(checklang, pwl=self.ignore)
+        checklang = LANGUAGES[lang]
+        ignore = self.get_ignorefile(lang)
+        check_dict = ec.DictWithPWL(checklang, pwl=ignore)
         self.checker = SpellChecker(check_dict, chunkers=[HTMLChunker], filters=[PythonFormatFilter])
         self.set_output(lang)
-        
+
     def set_output(self, lang):
         name = (lang + '_output.txt')
         files = os.listdir(BUILD_DIR)
@@ -57,11 +75,20 @@ class Check:
     def get_ignorefile(self, lang):
         for f in os.listdir(IGNORES_DIR):
             if lang in f:
-                self.ignore = f
-                return
+                return f# as there should be only one language file
 
 
-# checks contains tuples of po-files and corresponding checkers
+try:
+    print('Creating build directory at', BUILD_DIR)
+    os.mkdir(BUILD_DIR)
+except FileExistsError:
+    print("File or directory", BUILD_DIR ,"already exists, deleting")
+    rmtree(BUILD_DIR)
+    print('Recreating build directory')
+    os.mkdir(BUILD_DIR)
+    print('Build directory done')
+
+# checks contains one Check-Object for every po-file
 checks = []
 
 for root, dirs, files in os.walk(LOCALES_DIR):
@@ -71,7 +98,7 @@ for root, dirs, files in os.walk(LOCALES_DIR):
 
 en_dict = ec.DictWithPWL("en_US", pwl='./ignore_en.txt')
 en_ckr = SpellChecker(en_dict, chunkers=[HTMLChunker], filters=[PythonFormatFilter])
-output_file = open('_build/en_output.txt', 'w')
+output_file = open(BUILD_DIR + '/en_output.txt', 'w')
 
 for c in checks:
     for entry in c.po:
@@ -89,7 +116,7 @@ for c in checks:
             print("ERROR: {}:{}: {}".format(path, entry.linenum, err.word))
             c.output_file.write("ERROR: {}:{}: {}\n".format(path, entry.linenum, err.word))
 
-print("Spell-checking done. You can find the outputs in '_build/<lang>_output'.")
+print("Spell-checking done. You can find the outputs in", BUILD_DIR + "/<lang>_output")
 # TODO: extend english wordlist
 # TODO: extend german wordlist
 # TODO: use python test library to make good tests
