dca530f2f2bc4e5162cca53837c786354cbe2d80
Raphael Michel <mail@raphaelmichel.de>
2018-04-12 10:30:04 +0200

    Fix #860 -- Workaround for SQLite 3.23.0

--- src/pretix/presale/views/event.py
+++ src/pretix/presale/views/event.py
@@ -52,7 +52,7 @@ def get_grouped_items(event, subevent=None, voucher=None):
         Q(active=True)
         & Q(Q(available_from__isnull=True) | Q(available_from__lte=now()))
         & Q(Q(available_until__isnull=True) | Q(available_until__gte=now()))
-        & ~Q(category__is_addon=True)
+        & Q(Q(category__isnull=True) | Q(category__is_addon=False))
     )
 
     vouchq = Q(hide_without_voucher=False)
