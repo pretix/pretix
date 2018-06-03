14d203055bb65ee51f8dbc02c9fa775ff681adcd
Raphael Michel <mail@raphaelmichel.de>
2018-06-03 21:59:30 +0200

    ChunkBasedFileResponse: Support Content-Length

--- src/pretix/helpers/http.py
+++ src/pretix/helpers/http.py
@@ -5,5 +5,7 @@ class ChunkBasedFileResponse(StreamingHttpResponse):
     block_size = 4096
 
     def __init__(self, streaming_content=(), *args, **kwargs):
+        filelike = streaming_content
         streaming_content = streaming_content.chunks(self.block_size)
         super().__init__(streaming_content, *args, **kwargs)
+        self['Content-Length'] = filelike.size
