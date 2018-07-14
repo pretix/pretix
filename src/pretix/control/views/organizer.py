59245c4ec398086261895e0251baa843ea5b319a
Raphael Michel <mail@raphaelmichel.de>
2018-07-14 14:23:12 +0200

    New navi mechanism

--- src/pretix/control/views/organizer.py
+++ src/pretix/control/views/organizer.py
@@ -23,7 +23,6 @@ from pretix.control.forms.organizer import (
     OrganizerSettingsForm, OrganizerUpdateForm, TeamForm,
 )
 from pretix.control.permissions import OrganizerPermissionRequiredMixin
-from pretix.control.signals import nav_organizer
 from pretix.control.views import PaginationMixin
 from pretix.helpers.urls import build_absolute_uri
 from pretix.presale.style import regenerate_organizer_css
@@ -64,13 +63,7 @@ class TokenForm(forms.Form):
 class OrganizerDetailViewMixin:
     def get_context_data(self, **kwargs):
         ctx = super().get_context_data(**kwargs)
-        ctx['nav_organizer'] = []
         ctx['organizer'] = self.request.organizer
-
-        for recv, retv in nav_organizer.send(sender=self.request.organizer, request=self.request,
-                                             organizer=self.request.organizer):
-            ctx['nav_organizer'] += retv
-        ctx['nav_organizer'].sort(key=lambda n: n['label'])
         return ctx
 
     def get_object(self, queryset=None) -> Organizer:
