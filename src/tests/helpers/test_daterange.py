74c5fe512fbafd02eecd08bc172439bf94bf3b71
Alvaro Enrique Ruano <alvaro.ruano90@gmail.com>
2018-12-15 19:23:07 -0600

    Support for daterange in spanish

--- src/tests/helpers/test_daterange.py
+++ src/tests/helpers/test_daterange.py
@@ -17,6 +17,12 @@ def test_same_day_english():
         assert daterange(df, df) == "Feb. 1st, 2003"
 
 
+def test_same_day_spanish():
+    with translation.override('es'):
+        df = date(2003, 2, 1)
+        assert daterange(df, df) == "1 de Febrero de 2003"
+
+
 def test_same_month_german():
     with translation.override('de'):
         df = date(2003, 2, 1)
@@ -29,6 +35,13 @@ def test_same_month_english():
         df = date(2003, 2, 1)
         dt = date(2003, 2, 3)
         assert daterange(df, dt) == "Feb. 1st – 3rd, 2003"
+
+
+def test_same_month_spanish():
+    with translation.override('es'):
+        df = date(2003, 2, 1)
+        dt = date(2003, 2, 3)
+        assert daterange(df, dt) == "1 - 3 de Febrero de 2003"
 
 
 def test_same_year_german():
@@ -45,6 +58,13 @@ def test_same_year_english():
         assert daterange(df, dt) == "Feb. 1st – April 3rd, 2003"
 
 
+def test_same_year_spanish():
+    with translation.override('es'):
+        df = date(2003, 2, 1)
+        dt = date(2003, 4, 3)
+        assert daterange(df, dt) == "1 de Febrero - 3 de Abril de 2003"
+
+
 def test_different_dates_german():
     with translation.override('de'):
         df = date(2003, 2, 1)
@@ -57,3 +77,10 @@ def test_different_dates_english():
         df = date(2003, 2, 1)
         dt = date(2005, 4, 3)
         assert daterange(df, dt) == "Feb. 1, 2003 – April 3, 2005"
+
+
+def test_different_dates_spanish():
+    with translation.override('es'):
+        df = date(2003, 2, 1)
+        dt = date(2005, 4, 3)
+        assert daterange(df, dt) == "1 de Febrero de 2003 – 3 de Abril de 2005"
