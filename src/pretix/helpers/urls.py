dc433f6420a98d8fada568711475f2a6b00b730d
Raphael Michel <mail@raphaelmichel.de>
2019-01-02 09:23:58 +0100

    Reverse on global URL config in build_absolute_uri

--- src/pretix/helpers/urls.py
+++ src/pretix/helpers/urls.py
@@ -5,4 +5,6 @@ from django.urls import reverse
 
 
 def build_absolute_uri(urlname, args=None, kwargs=None):
-    return urljoin(settings.SITE_URL, reverse(urlname, args=args, kwargs=kwargs))
+    from pretix.multidomain import maindomain_urlconf
+
+    return urljoin(settings.SITE_URL, reverse(urlname, args=args, kwargs=kwargs, urlconf=maindomain_urlconf))
