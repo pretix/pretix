4019b588be49ee3c3b5769b2e7277e3596b96ef0
Martin Gross <gross@rami.io>
2019-02-20 11:23:48 +0100

    Filter sensitive keys from log-messages

--- src/pretix/base/models/base.py
+++ src/pretix/base/models/base.py
@@ -77,6 +77,13 @@ class LoggingMixin:
 
         logentry = LogEntry(content_object=self, user=user, action_type=action, event=event, **kwargs)
         if isinstance(data, dict):
+            sensitivekeys = ['password', 'smtp_password', 'payment_stripe_secret_key', 'payment_paypal_secret',
+                             'payment_mollie_api_key', 'payment_sofort_api_key', 'payment_wirecard_secret']
+
+            for k, v in data.items():
+                if k in sensitivekeys:
+                    data[k] = "********"
+
             logentry.data = json.dumps(data, cls=CustomJSONEncoder)
         elif data:
             raise TypeError("You should only supply dictionaries as log data.")
