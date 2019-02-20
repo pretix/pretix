90c8e0c17290429d6cccbbf4ec5d129de8818a66
Raphael Michel <mail@raphaelmichel.de>
2019-02-20 15:09:36 +0100

    Ensure attendee name in email renderer test

--- src/pretix/control/views/event.py
+++ src/pretix/control/views/event.py
@@ -690,7 +690,8 @@ class MailSettingsRendererPreview(MailSettingsPreview):
                                                     expires=now(), code="PREVIEW", total=119)
                 item = request.event.items.create(name=ugettext("Sample product"), default_price=42.23,
                                                   description=ugettext("Sample product description"))
-                order.positions.create(item=item, attendee_name_parts={'full_name': ugettext("John Doe")}, price=item.default_price)
+                order.positions.create(item=item, attendee_name_parts={'_legacy': ugettext("John Doe")},
+                                       price=item.default_price)
                 v = renderers[request.GET.get('renderer')].render(
                     v,
                     str(request.event.settings.mail_text_signature),
