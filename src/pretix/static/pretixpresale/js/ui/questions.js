17029320c9cdbd6b2afa790a7f4051b6da532425
Raphael Michel <mail@raphaelmichel.de>
2019-03-13 16:47:23 +0100

    JavaScript: Correctly walk up the dependency tree recursively

--- src/pretix/static/pretixpresale/js/ui/questions.js
+++ src/pretix/static/pretixpresale/js/ui/questions.js
@@ -1,36 +1,46 @@
 /*global $ */
 
 function questions_toggle_dependent(ev) {
-    $("[data-question-dependency]").each(function () {
-        var $dependent = $(this).closest(".form-group");
-        var dependency_name = $(this).attr("name").split("_")[0] + "_" + $(this).attr("data-question-dependency");
-        var dependency_value = $(this).attr("data-question-dependency-value");
-        var should_be_shown = false;
-        var is_shown = !$dependent.hasClass("dependency-hidden");
+    function q_should_be_shown($el) {
+        if (!$el.attr('data-question-dependency')) {
+            return true;
+        }
+
+        var dependency_name = $el.attr("name").split("_")[0] + "_" + $el.attr("data-question-dependency");
+        var dependency_value = $el.attr("data-question-dependency-value");
+        var $dependency_el;
+
         if ($("select[name=" + dependency_name + "]").length) {
             // dependency is type C
-            var $dependency_el = $("select[name=" + dependency_name + "]");
+            $dependency_el = $("select[name=" + dependency_name + "]");
             if (!$dependency_el.closest(".form-group").hasClass("dependency-hidden")) {  // do not show things that depend on hidden things
-                should_be_shown = $dependency_el.val() === dependency_value;
+                return q_should_be_shown($dependency_el) && $dependency_el.val() === dependency_value;
             }
         } else if ($("input[type=checkbox][name=" + dependency_name + "]").length) {
             // dependency type is B or M
             if (dependency_value === "True" || dependency_value === "False") {
-                var $dependency_el = $("input[name=" + dependency_name + "]");
+                $dependency_el = $("input[name=" + dependency_name + "]");
                 if (!$dependency_el.closest(".form-group").hasClass("dependency-hidden")) {  // do not show things that depend on hidden things
                     if (dependency_value === "True") {
-                        should_be_shown = $dependency_el.prop('checked');
+                        return q_should_be_shown($dependency_el) && $dependency_el.prop('checked');
                     } else {
-                        should_be_shown = !$dependency_el.prop('checked');
+                        return q_should_be_shown($dependency_el) && !$dependency_el.prop('checked');
                     }
                 }
             } else {
-                var $dependency_el = $("input[value=" + dependency_value + "][name=" + dependency_name + "]");
+                $dependency_el = $("input[value=" + dependency_value + "][name=" + dependency_name + "]");
                 if (!$dependency_el.closest(".form-group").hasClass("dependency-hidden")) {  // do not show things that depend on hidden things
-                    should_be_shown = $dependency_el.prop('checked');
+                    return q_should_be_shown($dependency_el) && $dependency_el.prop('checked');
                 }
             }
         }
+    }
+
+    $("[data-question-dependency]").each(function () {
+        var $dependent = $(this).closest(".form-group");
+        var is_shown = !$dependent.hasClass("dependency-hidden");
+        var should_be_shown = q_should_be_shown($(this));
+
         if (should_be_shown && !is_shown) {
             $dependent.stop().removeClass("dependency-hidden");
             if (!ev) {
@@ -42,7 +52,7 @@ function questions_toggle_dependent(ev) {
                 $(this).prop("required", true).removeClass("required-hidden");
             });
         } else if (!should_be_shown && is_shown) {
-            if ($dependent.hasClass("has-error") || $dependent.find(".has-error")) {
+            if ($dependent.hasClass("has-error") || $dependent.find(".has-error").length) {
                 // Do not hide things with invalid validation
                 return;
             }
