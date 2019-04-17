21ef6c7950edd5cd52103ab789bc4619259f66c9
Raphael Michel <mail@raphaelmichel.de>
2019-04-17 10:07:02 +0200

    Update framework classifier

--- src/setup.py
+++ src/setup.py
@@ -82,7 +82,7 @@ setup(
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
-        'Framework :: Django :: 2.1'
+        'Framework :: Django :: 2.2'
     ],
 
     keywords='tickets web shop ecommerce',
