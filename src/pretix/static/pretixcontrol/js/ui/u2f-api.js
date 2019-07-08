42af8b1602d69d776186d5c603cd9552c2fd8368
Martin Gross <gross@rami.io>
2019-07-08 13:39:12 +0200

    Remove excessive chars in U2F_GET_API_VERSION_RESPONSE

--- src/pretix/static/pretixcontrol/js/ui/u2f-api.js
+++ src/pretix/static/pretixcontrol/js/ui/u2f-api.js
@@ -48,7 +48,7 @@ var build_u2f_object = function () {
         'U2F_SIGN_REQUEST': 'u2f_sign_request',
         'U2F_SIGN_RESPONSE': 'u2f_sign_response',
         'U2F_GET_API_VERSION_REQUEST': 'u2f_get_api_version_request',
-        'ndowsU2F_GET_API_VERSION_RESPONSE': 'u2f_get_api_version_response'
+        'U2F_GET_API_VERSION_RESPONSE': 'u2f_get_api_version_response'
     };
 
 
