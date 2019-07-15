4bb8c3991eb2ac26b4b1f19dc96ae0a2a59d942f
Raphael Michel <mail@raphaelmichel.de>
2019-07-15 10:46:27 +0200

    Fix badge-creation task
    PRETIXEU-150

--- src/pretix/plugins/badges/tasks.py
+++ src/pretix/plugins/badges/tasks.py
@@ -16,9 +16,8 @@ logger = logging.getLogger(__name__)
 
 
 @app.task(base=EventTask, throws=(OrderError,))
-def badges_create_pdf(event: int, fileid: int, positions: List[int]) -> int:
+def badges_create_pdf(event: Event, fileid: int, positions: List[int]) -> int:
     file = CachedFile.objects.get(id=fileid)
-    event = Event.objects.get(id=event)
 
     pdfcontent = render_pdf(event, OrderPosition.objects.filter(id__in=positions))
     file.file.save(cachedfile_name(file, file.filename), ContentFile(pdfcontent.read()))
