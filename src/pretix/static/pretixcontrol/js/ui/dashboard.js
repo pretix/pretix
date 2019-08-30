be1a1f79953cb001ef2c4889de77db6bae25d4c5
Martin Gross <gross@rami.io>
2019-08-30 10:41:08 +0200

    Fix widget-URL for subevents

--- src/pretix/static/pretixcontrol/js/ui/dashboard.js
+++ src/pretix/static/pretixcontrol/js/ui/dashboard.js
@@ -4,7 +4,7 @@ $(function () {
     if ($("div[data-lazy-id]").length == 0) {
         return;
     }
-    $.getJSON("widgets.json", function (data) {
+    $.getJSON("widgets.json" + ($("select[name='subevent']").val() ? "?subevent=" + $("select[name='subevent']").val() : ""), function (data) {
         $.each(data.widgets, function (k, v) {
             $("[data-lazy-id=" + v.lazy + "]").removeClass("widget-lazy-loading");
             $("[data-lazy-id=" + v.lazy + "] .widget").html(v.content);
