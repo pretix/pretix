4a3bb9a7aaed76e9333ccc606a1d5fc1a75ea26a
Martin Gross <gross@rami.io>
2019-09-18 20:04:19 +0200

    Also fix CSV/nameparts-test

--- src/tests/plugins/test_checkinlist.py
+++ src/tests/plugins/test_checkinlist.py
@@ -90,10 +90,11 @@ def test_csv_order_by_name_parts(event):  # noqa
     })
     assert clean(content.decode()) == clean(""""Order code","Attendee name","Attendee name: Title",
 "Attendee name: First name","Attendee name: Middle name","Attendee name: Family name","Product","Price",
-"Checked in","Secret","E-mail","Company","Voucher code","Order date","Requires special attention","Comment"
-"FOO","Mrs Andrea J Zulu","Mrs","Andrea","J","Zulu","Ticket","13.00","","ggsngqtnmhx74jswjngw3fk8pfwz2a7k",
+"Checked in","Automatically checked in","Secret","E-mail","Company","Voucher code","Order date","Requires special
+ attention","Comment"
+"FOO","Mrs Andrea J Zulu","Mrs","Andrea","J","Zulu","Ticket","13.00","","","ggsngqtnmhx74jswjngw3fk8pfwz2a7k",
 "dummy@dummy.test","","","2019-02-22","No",""
-"FOO","Mr Peter A Jones","Mr","Peter","A","Jones","Ticket","23.00","","hutjztuxhkbtwnesv2suqv26k6ttytxx",
+"FOO","Mr Peter A Jones","Mr","Peter","A","Jones","Ticket","23.00","","","hutjztuxhkbtwnesv2suqv26k6ttytxx",
 "dummy@dummy.test","","","2019-02-22","No",""
 """)
     c = CSVCheckinList(event)
@@ -106,9 +107,10 @@ def test_csv_order_by_name_parts(event):  # noqa
     })
     assert clean(content.decode()) == clean(""""Order code","Attendee name","Attendee name: Title",
 "Attendee name: First name","Attendee name: Middle name","Attendee name: Family name","Product","Price",
-"Checked in","Secret","E-mail","Company","Voucher code","Order date","Requires special attention","Comment"
-"FOO","Mr Peter A Jones","Mr","Peter","A","Jones","Ticket","23.00","","hutjztuxhkbtwnesv2suqv26k6ttytxx",
+"Checked in","Automatically checked in","Secret","E-mail","Company","Voucher code","Order date","Requires special
+ attention","Comment"
+"FOO","Mr Peter A Jones","Mr","Peter","A","Jones","Ticket","23.00","","","hutjztuxhkbtwnesv2suqv26k6ttytxx",
 "dummy@dummy.test","","","2019-02-22","No",""
-"FOO","Mrs Andrea J Zulu","Mrs","Andrea","J","Zulu","Ticket","13.00","","ggsngqtnmhx74jswjngw3fk8pfwz2a7k",
+"FOO","Mrs Andrea J Zulu","Mrs","Andrea","J","Zulu","Ticket","13.00","","","ggsngqtnmhx74jswjngw3fk8pfwz2a7k",
 "dummy@dummy.test","","","2019-02-22","No",""
 """)
