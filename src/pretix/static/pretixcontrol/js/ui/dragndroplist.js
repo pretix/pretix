d01c8a96b40c46c859ced7927693fa44bd16a532
sohalt <sohalt@sohalt.net>
2019-10-08 20:12:42 +0200

    Show first up arrow and last down arrow

--- src/pretix/static/pretixcontrol/js/ui/dragndroplist.js
+++ src/pretix/static/pretixcontrol/js/ui/dragndroplist.js
@@ -3,15 +3,30 @@ $(function () {
     $("[data-dnd-url]").each(function(){
         var container = $(this),
             url = container.data("dnd-url"),
-            handle = $("<span>", {class: "btn btn-default btn-sm dnd-sort-handle"});
-        handle.append('<i class="fa fa-arrows"></i>');
-        container.find("a:has(.fa-arrow-up)").remove();
-        container.find("a:has(.fa-arrow-down)").replaceWith(handle);
+            up = container.find("a:has(.fa-arrow-up)"),
+            handle = $('<span class="btn btn-default btn-sm dnd-sort-handle"><i class="fa fa-arrows"></i></span>');
+
+        function hideArrows(container){
+            var up = container.find("a:has(.fa-arrow-up)"),
+                firstUp = up.first(),
+                down = container.find("a:has(.fa-arrow-down)"),
+                lastDown = down.last();
+            up.not(firstUp).css("display","none");
+            down.not(lastDown).css("display","none");
+            firstUp.css("display","inline-block");
+            lastDown.css("display","inline-block");
+        }
+        up.after(handle);
+        hideArrows(container);
 
         Sortable.create(container.get(0), {
             handle: ".dnd-sort-handle",
             onSort: function(evt){
-                var ids = $(evt.to).find("[data-dnd-id]").toArray().map(e => e.dataset.dndId);
+                var container = $(evt.to),
+                    ids = container.find("[data-dnd-id]").toArray().map(e => e.dataset.dndId);
+
+                hideArrows(container);
+
                 $.ajax(
                     {
                         'type': 'POST',
