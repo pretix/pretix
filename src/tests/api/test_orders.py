f3cd092b4119a40d52ebccdfece58ce08a5197b3
Martin Gross <gross@rami.io>
2019-10-18 16:38:50 +0200

    Tests

--- src/tests/api/test_orders.py
+++ src/tests/api/test_orders.py
@@ -6,6 +6,7 @@ from unittest import mock
 
 import pytest
 from django.core import mail as djmail
+from django.dispatch import receiver
 from django.utils.timezone import now
 from django_countries.fields import Country
 from django_scopes import scopes_disabled
@@ -13,6 +14,7 @@ from pytz import UTC
 from stripe.error import APIConnectionError
 from tests.plugins.stripe.test_provider import MockedCharge
 
+from pretix.base.channels import SalesChannel
 from pretix.base.models import (
     InvoiceAddress, Order, OrderPosition, Question, SeatingPlan,
 )
@@ -22,6 +24,21 @@ from pretix.base.models.orders import (
 from pretix.base.services.invoices import (
     generate_cancellation, generate_invoice,
 )
+from pretix.base.signals import register_sales_channels
+
+
+class FoobarSalesChannel(SalesChannel):
+    identifier = "bar"
+    verbose_name = "Foobar"
+    icon = "home"
+    testmode_supported = False
+
+
+@receiver(register_sales_channels, dispatch_uid="test_orders_register_sales_channels")
+def base_sales_channels(sender, **kwargs):
+    return (
+        FoobarSalesChannel(),
+    )
 
 
 @pytest.fixture
@@ -1534,6 +1551,22 @@ def test_order_create_in_test_mode(token_client, organizer, event, item, quota, 
     with scopes_disabled():
         o = Order.objects.get(code=resp.data['code'])
     assert o.testmode
+
+
+@pytest.mark.django_db
+def test_order_create_in_test_mode_saleschannel_limited(token_client, organizer, event, item, quota, question):
+    res = copy.deepcopy(ORDER_CREATE_PAYLOAD)
+    res['positions'][0]['item'] = item.pk
+    res['positions'][0]['answers'][0]['question'] = question.pk
+    res['testmode'] = True
+    res['sales_channel'] = 'bar'
+    resp = token_client.post(
+        '/api/v1/organizers/{}/events/{}/orders/'.format(
+            organizer.slug, event.slug
+        ), format='json', data=res
+    )
+    assert resp.status_code == 400
+    assert resp.data == {'testmode': ['This sales channel does not provide support for testmode.']}
 
 
 @pytest.mark.django_db
