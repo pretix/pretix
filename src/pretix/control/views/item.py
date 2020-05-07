pretix/pretix

commit e117545b3f0a57f9b87297767f6ea2309ea8c8f4

Refactor quota calculation (#1668)

src/pretix/control/views/item.py
@@ -31,6 +31,7 @@ from pretix.base.models import (
 )
 from pretix.base.models.event import SubEvent
 from pretix.base.models.items import ItemAddOn, ItemBundle, ItemMetaValue
+from pretix.base.services.quotas import QuotaAvailability
 from pretix.base.services.tickets import invalidate_cache
 from pretix.base.signals import quota_availability
 from pretix.control.forms.item import (
@@ -643,9 +644,7 @@ class QuotaList(PaginationMixin, ListView):
     template_name = 'pretixcontrol/items/quotas.html'
 
     def get_queryset(self):
-        qs = Quota.objects.filter(
-            event=self.request.event
-        ).prefetch_related(
+        qs = self.request.event.quotas.prefetch_related(
             Prefetch(
                 "items",
                 queryset=Item.objects.annotate(
@@ -654,12 +653,27 @@ class QuotaList(PaginationMixin, ListView):
                 to_attr="cached_items"
             ),
             "variations",
-            "variations__item"
+            "variations__item",
+            Prefetch(
+                "subevent",
+                queryset=self.request.event.subevents.all()
+            )
         )
         if self.request.GET.get("subevent", "") != "":
             s = self.request.GET.get("subevent", "")
             qs = qs.filter(subevent_id=s)
         return qs
+
+    def get_context_data(self, **kwargs):
+        ctx = super().get_context_data()
+
+        qa = QuotaAvailability()
+        qa.queue(*ctx['quotas'])
+        qa.compute()
+        for quota in ctx['quotas']:
+            qa.cached_avail = qa.results[quota]
+
+        return ctx
 
 
 class QuotaCreate(EventPermissionRequiredMixin, CreateView):
@@ -719,28 +733,30 @@ class QuotaView(ChartContainingView, DetailView):
     def get_context_data(self, *args, **kwargs):
         ctx = super().get_context_data()
 
-        avail = self.object.availability()
-        ctx['avail'] = avail
+        qa = QuotaAvailability(full_results=True)
+        qa.queue(self.object)
+        qa.compute()
+        ctx['avail'] = qa.results[self.object]
 
         data = [
             {
                 'label': gettext('Paid orders'),
-                'value': self.object.count_paid_orders(),
+                'value': qa.count_paid_orders[self.object],
                 'sum': True,
             },
             {
                 'label': gettext('Pending orders'),
-                'value': self.object.count_pending_orders(),
+                'value': qa.count_pending_orders[self.object],
                 'sum': True,
             },
             {
                 'label': gettext('Vouchers and waiting list reservations'),
-                'value': self.object.count_blocking_vouchers(),
+                'value': qa.count_vouchers[self.object],
                 'sum': True,
             },
             {
                 'label': gettext('Current user\'s carts'),
-                'value': self.object.count_in_cart(),
+                'value': qa.count_cart[self.object],
                 'sum': True,
             },
         ]
@@ -756,14 +772,14 @@ class QuotaView(ChartContainingView, DetailView):
         })
         data.append({
             'label': gettext('Waiting list (pending)'),
-            'value': self.object.count_waiting_list_pending(),
+            'value': qa.count_waitinglist[self.object],
             'sum': False,
         })
 
         if self.object.size is not None:
             data.append({
                 'label': gettext('Currently for sale'),
-                'value': avail[1],
+                'value': ctx['avail'][1],
                 'sum': False,
                 'strong': True
             })
@@ -786,7 +802,17 @@ class QuotaView(ChartContainingView, DetailView):
         ctx['has_ignore_vouchers'] = Voucher.objects.filter(
             Q(allow_ignore_quota=True) &
             Q(Q(valid_until__isnull=True) | Q(valid_until__gte=now())) &
-            Q(Q(self.object._position_lookup) | Q(quota=self.object)) &
+            Q(
+                (
+                    (  # Orders for items which do not have any variations
+                        Q(variation__isnull=True) &
+                        Q(item_id__in=Quota.items.through.objects.filter(quota_id=self.object.pk).values_list('item_id', flat=True))
+                    ) | (  # Orders for items which do have any variations
+                        Q(variation__in=Quota.variations.through.objects.filter(quota_id=self.object.pk).values_list(
+                            'itemvariation_id', flat=True))
+                    )
+                ) | Q(quota=self.object)
+            ) &
             Q(redeemed__lt=F('max_usages'))
         ).exists()
         if self.object.closed:

src/pretix/api/views/item.py
@@ -20,6 +20,7 @@ from pretix.base.models import (
     CartPosition, Item, ItemAddOn, ItemBundle, ItemCategory, ItemVariation,
     Question, QuestionOption, Quota,
 )
+from pretix.base.services.quotas import QuotaAvailability
 from pretix.helpers.dicts import merge_dicts
 
 with scopes_disabled():
@@ -533,14 +534,17 @@ class QuotaViewSet(ConditionalListView, viewsets.ModelViewSet):
     def availability(self, request, *args, **kwargs):
         quota = self.get_object()
 
-        avail = quota.availability()
+        qa = QuotaAvailability()
+        qa.queue(quota)
+        qa.compute()
+        avail = qa.results[quota]
 
         data = {
-            'paid_orders': quota.count_paid_orders(),
-            'pending_orders': quota.count_pending_orders(),
-            'blocking_vouchers': quota.count_blocking_vouchers(),
-            'cart_positions': quota.count_in_cart(),
-            'waiting_list': quota.count_waiting_list_pending(),
+            'paid_orders': qa.count_paid_orders[quota],
+            'pending_orders': qa.count_pending_orders[quota],
+            'blocking_vouchers': qa.count_vouchers[quota],
+            'cart_positions': qa.count_cart[quota],
+            'waiting_list': qa.count_pending_orders[quota],
             'available_number': avail[1],
             'available': avail[0] == Quota.AVAILABILITY_OK,
             'total_size': quota.size,

src/pretix/base/exporters/orderlist.py
@@ -14,6 +14,7 @@ from pretix.base.models import (
     GiftCard, InvoiceAddress, InvoiceLine, Order, OrderPosition, Question,
 )
 from pretix.base.models.orders import OrderFee, OrderPayment, OrderRefund
+from pretix.base.services.quotas import QuotaAvailability
 from pretix.base.settings import PERSON_NAME_SCHEMES
 
 from ..exporter import ListExporter, MultiSheetListExporter
@@ -516,16 +517,21 @@ class QuotaListExporter(ListExporter):
         ]
         yield headers
 
-        for quota in self.event.quotas.all():
-            avail = quota.availability()
+        quotas = list(self.event.quotas.all())
+        qa = QuotaAvailability(full_results=True)
+        qa.queue(*quotas)
+        qa.compute()
+
+        for quota in quotas:
+            avail = qa.results[quota]
             row = [
                 quota.name,
                 _('Infinite') if quota.size is None else quota.size,
-                quota.count_paid_orders(),
-                quota.count_pending_orders(),
-                quota.count_blocking_vouchers(),
-                quota.count_in_cart(),
-                quota.count_waiting_list_pending(),
+                qa.count_paid_orders[quota],
+                qa.count_pending_orders[quota],
+                qa.count_vouchers[quota],
+                qa.count_cart[quota],
+                qa.count_waitinglist[quota],
                 _('Infinite') if avail[1] is None else avail[1]
             ]
             yield row

src/pretix/base/models/event.py
@@ -214,8 +214,10 @@ class EventMixin:
         vars_reserved = set()
         items_gone = set()
         vars_gone = set()
+
+        r = getattr(self, '_quota_cache', {})
         for q in self.active_quotas:
-            res = q.availability(allow_cache=True)
+            res = r[q] if q in r else q.availability(allow_cache=True)
 
             if res[0] == Quota.AVAILABILITY_OK:
                 if q.active_items:

src/pretix/base/models/items.py
@@ -7,11 +7,10 @@ from typing import Tuple
 
 import dateutil.parser
 import pytz
-from django.conf import settings
 from django.core.exceptions import ValidationError
 from django.core.validators import RegexValidator
 from django.db import models
-from django.db.models import F, Func, Q, Sum
+from django.db.models import Q
 from django.utils import formats
 from django.utils.crypto import get_random_string
 from django.utils.functional import cached_property
@@ -25,7 +24,6 @@ from pretix.base.models import fields
 from pretix.base.models.base import LoggedModel
 from pretix.base.models.fields import MultiStringField
 from pretix.base.models.tax import TaxedPrice
-from pretix.base.signals import quota_availability
 
 from .event import Event, SubEvent
 
@@ -1350,6 +1348,7 @@ class Quota(LoggedModel):
             self.event.cache.clear()
 
     def save(self, *args, **kwargs):
+        # This is *not* called when the db-level cache is upated, since we use bulk_update there
         clear_cache = kwargs.pop('clear_cache', True)
         super().save(*args, **kwargs)
         if self.event and clear_cache:
@@ -1384,6 +1383,8 @@ class Quota(LoggedModel):
         :returns: a tuple where the first entry is one of the ``Quota.AVAILABILITY_`` constants
                   and the second is the number of available tickets.
         """
+        from ..services.quotas import QuotaAvailability
+
         if allow_cache and self.cache_is_hot() and count_waitinglist:
             return self.cached_availability_state, self.cached_availability_number
 
@@ -1392,140 +1393,15 @@ class Quota(LoggedModel):
 
         if _cache is not None and self.pk in _cache:
             return _cache[self.pk]
-        now_dt = now_dt or now()
-        res = self._availability(now_dt, count_waitinglist)
-        for recv, resp in quota_availability.send(sender=self.event, quota=self, result=res,
-                                                  count_waitinglist=count_waitinglist):
-            res = resp
-
-        if res[0] <= Quota.AVAILABILITY_ORDERED and self.close_when_sold_out and not self.closed:
-            self.closed = True
-            self.save(update_fields=['closed'])
-            self.log_action('pretix.event.quota.closed')
-
-        self.event.cache.delete('item_quota_cache')
-        rewrite_cache = count_waitinglist and (
-            not self.cache_is_hot(now_dt) or res[0] > self.cached_availability_state
-        )
-        if rewrite_cache:
-            self.cached_availability_state = res[0]
-            self.cached_availability_number = res[1]
-            self.cached_availability_time = now_dt
-            if self.size is None:
-                self.cached_availability_paid_orders = self.count_paid_orders()
-            self.save(
-                update_fields=[
-                    'cached_availability_state', 'cached_availability_number', 'cached_availability_time',
-                    'cached_availability_paid_orders'
-                ],
-                clear_cache=False,
-                using='default'
-            )
+        qa = QuotaAvailability(count_waitinglist=count_waitinglist, early_out=False)
+        qa.queue(self)
+        qa.compute(now_dt=now_dt)
+        res = qa.results[self]
 
         if _cache is not None:
             _cache[self.pk] = res
             _cache['_count_waitinglist'] = count_waitinglist
         return res
-
-    def _availability(self, now_dt: datetime=None, count_waitinglist=True, ignore_closed=False):
-        now_dt = now_dt or now()
-        if self.closed and not ignore_closed:
-            return Quota.AVAILABILITY_ORDERED, 0
-
-        size_left = self.size
-        if size_left is None:
-            return Quota.AVAILABILITY_OK, None
-
-        paid_orders = self.count_paid_orders()
-        self.cached_availability_paid_orders = paid_orders
-        size_left -= paid_orders
-        if size_left <= 0:
-            return Quota.AVAILABILITY_GONE, 0
-
-        size_left -= self.count_pending_orders()
-        if size_left <= 0:
-            return Quota.AVAILABILITY_ORDERED, 0
-
-        size_left -= self.count_blocking_vouchers(now_dt)
-        if size_left <= 0:
-            return Quota.AVAILABILITY_ORDERED, 0
-
-        if count_waitinglist:
-            size_left -= self.count_waiting_list_pending()
-            if size_left <= 0:
-                return Quota.AVAILABILITY_ORDERED, 0
-
-        size_left -= self.count_in_cart(now_dt)
-        if size_left <= 0:
-            return Quota.AVAILABILITY_RESERVED, 0
-
-        return Quota.AVAILABILITY_OK, size_left
-
-    def count_blocking_vouchers(self, now_dt: datetime=None) -> int:
-        from pretix.base.models import Voucher
-
-        now_dt = now_dt or now()
-        if 'sqlite3' in settings.DATABASES['default']['ENGINE']:
-            func = 'MAX'
-        else:  # NOQA
-            func = 'GREATEST'
-
-        return Voucher.objects.filter(
-            Q(event=self.event) & Q(subevent=self.subevent) &
-            Q(block_quota=True) &
-            Q(Q(valid_until__isnull=True) | Q(valid_until__gte=now_dt)) &
-            Q(Q(self._position_lookup) | Q(quota=self))
-        ).values('id').aggregate(
-            free=Sum(Func(F('max_usages') - F('redeemed'), 0, function=func))
-        )['free'] or 0
-
-    def count_waiting_list_pending(self) -> int:
-        from pretix.base.models import WaitingListEntry
-        return WaitingListEntry.objects.filter(
-            Q(voucher__isnull=True) & Q(subevent=self.subevent) &
-            self._position_lookup
-        ).distinct().count()
-
-    def count_in_cart(self, now_dt: datetime=None) -> int:
-        from pretix.base.models import CartPosition
-
-        now_dt = now_dt or now()
-        return CartPosition.objects.filter(
-            Q(event=self.event) & Q(subevent=self.subevent) &
-            Q(expires__gte=now_dt) &
-            Q(
-                Q(voucher__isnull=True)
-                | Q(voucher__block_quota=False)
-                | Q(voucher__valid_until__lt=now_dt)
-            ) &
-            self._position_lookup
-        ).count()
-
-    def count_pending_orders(self) -> dict:
-        from pretix.base.models import Order, OrderPosition
-
-        # This query has beeen benchmarked against a Count('id', distinct=True) aggregate and won by a small margin.
-        return OrderPosition.objects.filter(
-            self._position_lookup, order__status=Order.STATUS_PENDING, order__event=self.event, subevent=self.subevent
-        ).count()
-
-    def count_paid_orders(self):
-        from pretix.base.models import Order, OrderPosition
-
-        return OrderPosition.objects.filter(
-            self._position_lookup, order__status=Order.STATUS_PAID, order__event=self.event, subevent=self.subevent
-        ).count()
-
-    @cached_property
-    def _position_lookup(self) -> Q:
-        return (
-            (  # Orders for items which do not have any variations
-               Q(variation__isnull=True) &
-               Q(item_id__in=Quota.items.through.objects.filter(quota_id=self.pk).values_list('item_id', flat=True))
-            ) | (  # Orders for items which do have any variations
-                   Q(variation__in=Quota.variations.through.objects.filter(quota_id=self.pk).values_list('itemvariation_id', flat=True))
-            )
-        )
 
     class QuotaExceededException(Exception):
         pass
@@ -1535,7 +1411,6 @@ class Quota(LoggedModel):
         for variation in (variations or []):
             if variation.item not in items:
                 raise ValidationError(_('All variations must belong to an item contained in the items list.'))
-                break
 
     @staticmethod
     def clean_items(event, items, variations):

src/pretix/base/services/cart.py
@@ -25,6 +25,7 @@ from pretix.base.reldate import RelativeDateWrapper
 from pretix.base.services.checkin import _save_answers
 from pretix.base.services.locking import LockTimeoutException, NoLockManager
 from pretix.base.services.pricing import get_price
+from pretix.base.services.quotas import QuotaAvailability
 from pretix.base.services.tasks import ProfiledEventTask
 from pretix.base.settings import PERSON_NAME_SCHEMES
 from pretix.base.signals import validate_cart_addons
@@ -728,10 +729,14 @@ class CartManager:
 
     def _get_quota_availability(self):
         quotas_ok = defaultdict(int)
+        qa = QuotaAvailability()
+        qa.queue(*[k for k, v in self._quota_diff.items() if v > 0])
+        qa.compute(now_dt=self.now_dt)
         for quota, count in self._quota_diff.items():
             if count <= 0:
                 quotas_ok[quota] = 0
-            avail = quota.availability(self.now_dt)
+                break
+            avail = qa.results[quota]
             if avail[1] is not None and avail[1] < count:
                 quotas_ok[quota] = min(count, avail[1])
             else:

src/pretix/base/services/orders.py
@@ -44,6 +44,7 @@ from pretix.base.services.invoices import (
 from pretix.base.services.locking import LockTimeoutException, NoLockManager
 from pretix.base.services.mail import SendMailException
 from pretix.base.services.pricing import get_price
+from pretix.base.services.quotas import QuotaAvailability
 from pretix.base.services.tasks import ProfiledEventTask, ProfiledTask
 from pretix.base.signals import (
     allow_ticket_download, order_approved, order_canceled, order_changed,
@@ -1391,10 +1392,13 @@ class OrderChangeManager:
                     raise OrderError(self.error_messages['seat_subevent_mismatch'].format(seat=v['seat'].name))
 
     def _check_quotas(self):
+        qa = QuotaAvailability()
+        qa.queue(*[k for k, v in self._quotadiff.items() if v > 0])
+        qa.compute()
         for quota, diff in self._quotadiff.items():
             if diff <= 0:
                 continue
-            avail = quota.availability()
+            avail = qa.results[quota]
             if avail[0] != Quota.AVAILABILITY_OK or (avail[1] is not None and avail[1] < diff):
                 raise OrderError(self.error_messages['quota'].format(name=quota.name))
 

commit 6a4c81ff3cd916311054cef15f143e5e1ea94067

Fix two bugs introduced in e117545b3

src/pretix/control/views/item.py
@@ -671,7 +671,7 @@ class QuotaList(PaginationMixin, ListView):
         qa.queue(*ctx['quotas'])
         qa.compute()
         for quota in ctx['quotas']:
-            qa.cached_avail = qa.results[quota]
+            quota.cached_avail = qa.results[quota]
 
         return ctx
 
@@ -816,7 +816,10 @@ class QuotaView(ChartContainingView, DetailView):
             Q(redeemed__lt=F('max_usages'))
         ).exists()
         if self.object.closed:
-            ctx['closed_and_sold_out'] = self.object._availability(ignore_closed=True)[0] <= Quota.AVAILABILITY_ORDERED
+            qa = QuotaAvailability(ignore_closed=True)
+            qa.queue(self.object)
+            qa.compute()
+            ctx['closed_and_sold_out'] = qa.results[self.object][0] <= Quota.AVAILABILITY_ORDERED
 
         return ctx
 