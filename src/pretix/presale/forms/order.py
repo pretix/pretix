98dfdd8b013c7fd87d7c8ae1baedcae2b9593fe1
Felix Rindt <felix@rindt.me>
2020-07-24 18:44:35 +0200

    no ugettext

--- src/pretix/presale/forms/order.py
+++ src/pretix/presale/forms/order.py
@@ -1,7 +1,7 @@
 from decimal import Decimal
 
 from django import forms
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 
 from pretix.base.models import Quota
 from pretix.base.models.tax import TaxedPrice
