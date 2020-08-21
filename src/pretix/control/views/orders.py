e9a4c3845ad7bf34ca8a9e4c625d18552199fa41
Raphael Michel <mail@raphaelmichel.de>
2020-08-21 16:06:52 +0200

    Fix crash when processing refund for empty order

--- src/pretix/control/views/orders.py
+++ src/pretix/control/views/orders.py
@@ -565,7 +565,7 @@ class OrderRefundProcess(OrderView):
         if self.refund.state == OrderRefund.REFUND_STATE_EXTERNAL:
             self.refund.done(user=self.request.user)
 
-            if self.request.POST.get("action") == "r" and self.order.status != Order.STATUS_CANCELED:
+            if self.request.POST.get("action") == "r" and (self.order.status != Order.STATUS_CANCELED and self.order.positions.exists()):
                 mark_order_refunded(self.order, user=self.request.user)
             elif not (self.order.status == Order.STATUS_PAID and self.order.pending_sum <= 0):
                 self.order.status = Order.STATUS_PENDING
