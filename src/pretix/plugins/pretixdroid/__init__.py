af2b4ebb4bd9e0278aa3a4589004d73db1da7680
Raphael Michel <mail@raphaelmichel.de>
2020-09-18 15:59:58 +0200

    Properly deprecate pretixdroid

--- src/pretix/plugins/pretixdroid/__init__.py
+++ src/pretix/plugins/pretixdroid/__init__.py
@@ -6,13 +6,14 @@ from pretix import __version__ as version
 
 class PretixdroidApp(AppConfig):
     name = 'pretix.plugins.pretixdroid'
-    verbose_name = _("Check-in device API")
+    verbose_name = _("Old check-in device API")
 
     class PretixPluginMeta:
-        name = _("Check-in device API")
+        name = _("Old check-in device API")
         author = _("the pretix team")
         version = version
-        visible = True
+        visible = False
+        restricted = True
         category = 'INTEGRATION'
         description = _("This plugin allows you to use the pretixdroid and pretixdesk apps for your event.")
 
