da149682aadbf6da97ed953284fd6cf1814ea8a8
Raphael Michel <mail@raphaelmichel.de>
2020-10-31 16:33:45 +0100

    Improve load behavior of ajaxpending.js

--- src/pretix/static/pretixbase/js/ajaxpending.js
+++ src/pretix/static/pretixbase/js/ajaxpending.js
@@ -1,7 +1,10 @@
-window.setInterval(function () {
-    $.get(location.href + '?ajax=1', function (data, status) {
-        if (data === "1") {
-            location.reload();
+var check = function () {
+    $.getJSON(location.href + '&ajax=1', function (data, status) {
+        if (data.redirect) {
+            location.href = data.redirect;
+        } else {
+            window.setTimeout(check, 500);
         }
     });
-}, 500);
+}
+window.setTimeout(check, 500);
