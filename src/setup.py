bc64109e11b1da0072243244e9642bb4d2a1ebdd
Raphael Michel <mail@raphaelmichel.de>
2021-03-14 12:26:41 +0100

    Fix Pillow version

--- src/setup.py
+++ src/setup.py
@@ -101,7 +101,7 @@ setup(
         'django-filter==2.4.*',
         'django-scopes==1.2.*',
         'reportlab>=3.5.65',
-        'Pillow==7.*',
+        'Pillow==8.*',
         'PyPDF2==1.26.*',
         'django-libsass==0.8',
         'libsass==0.20.*',
