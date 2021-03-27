fa97c9b5b540d1e555ee9ebb0063e296b2c04766
Raphael Michel <mail@raphaelmichel.de>
2021-03-27 13:21:09 +0100

    Revert "Reduce search to attendee names"
    This reverts commit cedd0c877e11e7046d32d5049a7311d2b4d2d9a3.

--- src/pretix/api/views/order.py
+++ src/pretix/api/views/order.py
@@ -736,7 +736,14 @@ with scopes_disabled():
 
         def search_qs(self, queryset, name, value):
             return queryset.filter(
-                Q(attendee_name_cached__icontains=value)
+                Q(secret__istartswith=value)
+                | Q(attendee_name_cached__icontains=value)
+                | Q(addon_to__attendee_name_cached__icontains=value)
+                | Q(attendee_email__icontains=value)
+                | Q(addon_to__attendee_email__icontains=value)
+                | Q(order__code__istartswith=value)
+                | Q(order__invoice_address__name_cached__icontains=value)
+                | Q(order__email__icontains=value)
             )
 
         def has_checkin_qs(self, queryset, name, value):
