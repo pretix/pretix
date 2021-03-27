1a330206e90c1517b18ee00529b34dcaf2fae0da
Raphael Michel <mail@raphaelmichel.de>
2021-03-27 14:06:46 +0100

    Fix npm install

--- src/pretix/helpers/compressor.py
+++ src/pretix/helpers/compressor.py
@@ -13,13 +13,15 @@ class VueCompiler(CompilerFilter):
 
     def __init__(self, content, attrs, **kwargs):
         config_dir = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'static', 'npm_dir')
-        node_path = os.path.join(settings.STATIC_ROOT, 'node_prefix', 'node_modules', 'pretix', 'node_modules')
-        rollup_bin = os.path.join(node_path, 'rollup', 'dist', 'bin', 'rollup')
+        node_path = os.path.join(settings.STATIC_ROOT, 'node_prefix', 'node_modules')
+        self.rollup_bin = os.path.join(node_path, 'rollup', 'dist', 'bin', 'rollup')
         rollup_config = os.path.join(config_dir, 'rollup.config.js')
+        if not os.path.exists(self.rollup_bin) and not settings.DEBUG:
+            raise FilterError("Rollup not installed or pretix not built properly, please run 'make npminstall' in source root.")
         command = (
             ' '.join((
                 'NODE_PATH=' + shlex.quote(node_path),
-                shlex.quote(rollup_bin),
+                shlex.quote(self.rollup_bin),
                 '-c',
                 shlex.quote(rollup_config))
             ) +
@@ -30,6 +32,8 @@ class VueCompiler(CompilerFilter):
     def input(self, **kwargs):
         if self.filename is None:
             raise FilterError('VueCompiler can only compile files, not inline code.')
+        if not os.path.exists(self.rollup_bin):
+            raise FilterError("Rollup not installed, please run 'make npminstall' in source root.")
         self.options += (('export_name', re.sub(
             r'^([a-z])|[^a-z0-9A-Z]+([a-zA-Z0-9])?',
             lambda s: s.group(0)[-1].upper(),
