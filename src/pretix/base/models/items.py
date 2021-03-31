a6d4e26a3b4a3ba2eeccecd61b309b5984ffdb8b
Raphael Michel <mail@raphaelmichel.de>
2021-03-31 10:38:39 +0200

    Fix import order

--- src/pretix/base/models/items.py
+++ src/pretix/base/models/items.py
@@ -7,9 +7,9 @@ from typing import Tuple
 
 import dateutil.parser
 import pytz
+from django.conf import settings
 from django.core.exceptions import ValidationError
 from django.core.validators import RegexValidator
-from django.conf import settings
 from django.db import models
 from django.db.models import Q
 from django.utils import formats
