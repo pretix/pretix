3dee6c42e535765310438d4c98808c6dc035dee0
Raphael Michel <mail@raphaelmichel.de>
2021-04-08 11:39:12 +0200

    ..

--- src/pretix/base/models/memberships.py
+++ src/pretix/base/models/memberships.py
@@ -1,6 +1,7 @@
 from django.db import models
 from jsonfallback.fields import FallbackJSONField
 
+from django.utils.translation import gettext_lazy as _
 from pretix.base.models import Customer
 from pretix.base.models.base import LoggedModel
 from pretix.base.models.organizer import Organizer
@@ -9,8 +10,16 @@ from pretix.base.models.organizer import Organizer
 class MembershipType(LoggedModel):
     id = models.BigAutoField(primary_key=True)
     organizer = models.ForeignKey(Organizer, related_name='membership_types', on_delete=models.CASCADE)
-    name = models.CharField(max_length=255)
-    transferable = models.BooleanField(default=False)
+    name = models.CharField(
+        verbose_name=_('Name'),
+        max_length=255
+    )
+    transferable = models.BooleanField(
+        verbose_name=_('Membership is transferable'),
+        help_text=_('If this is not selected, the membership can only be used to purchase tickets with the same '
+                    'attendee name as the original membership purchase.'),
+        default=False
+    )
 
     def __str__(self):
         return self.name
