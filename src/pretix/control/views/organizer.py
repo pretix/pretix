ff4f56392dd90ddef78a67c2cf9fe9524e169ae0
Raphael Michel <michel@rami.io>
2021-06-25 11:34:44 +0200

    Fix isort issue

--- src/pretix/control/views/organizer.py
+++ src/pretix/control/views/organizer.py
@@ -102,8 +102,8 @@ from pretix.control.permissions import (
 from pretix.control.signals import nav_organizer
 from pretix.control.views import PaginationMixin
 from pretix.helpers.dicts import merge_dicts
-from pretix.multidomain.urlreverse import build_absolute_uri
 from pretix.helpers.urls import build_absolute_uri as build_global_uri
+from pretix.multidomain.urlreverse import build_absolute_uri
 from pretix.presale.forms.customer import TokenGenerator
 from pretix.presale.style import regenerate_organizer_css
 
