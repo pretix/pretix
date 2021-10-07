42a96312ab39e94a49c1a0e7a225f3b45fc10311
Richard Schreiber <wiffbi@gmail.com>
2021-10-07 16:01:36 +0200

    add light grey background to dragged element

--- src/pretix/static/pretixcontrol/js/ui/dragndroplist.js
+++ src/pretix/static/pretixcontrol/js/ui/dragndroplist.js
@@ -10,6 +10,7 @@ $(function () {
 
         Sortable.create(container.get(0), {
             handle: ".dnd-sort-handle",
+            ghostClass: "sortable-ghost",
             onSort: function (evt){
                 var container = $(evt.to),
                     ids = container.find("[data-dnd-id]").toArray().map(function (e) { return e.dataset.dndId; });
