eeb6d9712bd92f3f5861e168c8e8f277699560de
Raphael Michel <michel@rami.io>
2021-11-03 11:21:37 +0100

    Fix tests

--- src/tests/api/test_events.py
+++ src/tests/api/test_events.py
@@ -106,7 +106,7 @@ TEST_EVENT_RES = {
     "is_public": True,
     "presale_start": None,
     "presale_end": None,
-    "location": {"en": "Heidelberg"},
+    "location": None,
     "geo_lat": None,
     "geo_lon": None,
     "slug": "dummy",
