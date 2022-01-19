c2bd3dde4467bd37728c917cc9a6effdfe7adecd
Raphael Michel <michel@rami.io>
2022-01-19 17:11:51 +0100

    GitHub actions: Do not run on 3.10 yet (too many warnings)

--- src/setup.py
+++ src/setup.py
@@ -153,7 +153,6 @@ setup(
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3.10',
         'Framework :: Django :: 3.2'
     ],
 
