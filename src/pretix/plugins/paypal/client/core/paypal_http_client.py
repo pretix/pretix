3a186d5676b9798cfedea08ad6cd23915fd0133b
Martin Gross <gross@rami.io>
2022-03-04 12:12:17 +0100

    Blindly add Paypal-Auth-Assertation-Header. To be revisited

--- src/pretix/plugins/paypal/client/core/paypal_http_client.py
+++ src/pretix/plugins/paypal/client/core/paypal_http_client.py
@@ -1,15 +1,11 @@
-from paypalcheckoutsdk.core import (
-    AccessTokenRequest, PayPalHttpClient as VendorPayPalHttpClient,
-    RefreshTokenRequest,
-)
+from paypalcheckoutsdk.core import PayPalHttpClient as VendorPayPalHttpClient
 
 
 class PayPalHttpClient(VendorPayPalHttpClient):
     def __call__(self, request):
         super().__call__(request)
-        if "Authorization" not in request.headers and not isinstance(request, AccessTokenRequest) and not isinstance(request, RefreshTokenRequest):
-            if self.environment.merchant_id:
-                request.headers["PayPal-Auth-Assertion"] = self.environment.authorization_assertation()
+        if self.environment.merchant_id:
+            request.headers["PayPal-Auth-Assertion"] = self.environment.authorization_assertation()
 
         if self.environment.partner_id:
             request.headers["PayPal-Partner-Attribution-Id"] = self.environment.partner_id
