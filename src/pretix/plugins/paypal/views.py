0554cd26873f5191267bba8f25da1d04da2c3cbc
Raphael Michel <michel@rami.io>
2022-03-17 16:44:49 +0100

    Raise a 400 instead

--- src/pretix/plugins/paypal/views.py
+++ src/pretix/plugins/paypal/views.py
@@ -176,7 +176,9 @@ def webhook(request, *args, **kwargs):
     event_json = json.loads(event_body)
 
     # We do not check the signature, we just use it as a trigger to look the charge up.
-    if not ('resource_type' in event_json) or (event_json['resource_type'] not in ('sale', 'refund')):
+    if 'resource_type' not in event_json:
+        return HttpResponse("Invalid body, no resource_type given", status=400)
+    if event_json['resource_type'] not in ('sale', 'refund'):
         return HttpResponse("Not interested in this resource type", status=200)
 
     if event_json['resource_type'] == 'sale':
