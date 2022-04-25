38c7f4630e06c83c2ce92c26f0a0347c9bab8d21
Martin Gross <gross@rami.io>
2022-04-25 10:47:30 +0200

    Update src/pretix/plugins/paypal/views.py
    Co-authored-by: Raphael Michel <michel@rami.io>

--- src/pretix/plugins/paypal/views.py
+++ src/pretix/plugins/paypal/views.py
@@ -221,7 +221,7 @@ def isu_return(request, *args, **kwargs):
                 if request.GET.get("isEmailConfirmed") == "false":  # Yes - literal!
                     messages.warning(
                         request,
-                        _('The E-Mail address on your PayPal account has not yet been confirmed. You will need to do '
+                        _('The e-mail address on your PayPal account has not yet been confirmed. You will need to do '
                           'this before you can start accepting payments.')
                     )
                 messages.success(
