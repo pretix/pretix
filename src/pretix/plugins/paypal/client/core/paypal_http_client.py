9ba304d3d9f6c87cf8ed91a4a96afbbdbcb43abe
Martin Gross <gross@rami.io>
2022-04-25 10:47:09 +0200

    Better Cache-Handling of Access-Tokens

--- src/pretix/plugins/paypal/client/core/paypal_http_client.py
+++ src/pretix/plugins/paypal/client/core/paypal_http_client.py
@@ -1,3 +1,5 @@
+import hashlib
+
 from paypalcheckoutsdk.core import PayPalHttpClient as VendorPayPalHttpClient, AccessToken
 
 from django.core.cache import cache
@@ -5,36 +7,38 @@ from django.core.cache import cache
 
 class PayPalHttpClient(VendorPayPalHttpClient):
     def __call__(self, request):
-        # First we set an optional existing refresh and access token
-        self._refresh_token = cache.get("pretix_paypal_refresh_token", None)
+        # First we get all the items that make up the current credentials and create a hash to detect changes
 
-        access_token = cache.get("pretix_paypal_access_token", None)
+        checksum = hashlib.sha256(''.join([
+            self.environment.base_url, self.environment.client_id, self.environment.client_secret
+        ]).encode()).hexdigest()
+        cache_key_hash = f'pretix_paypal_token_hash_{checksum}'
+        token_hash = cache.get(cache_key_hash)
 
-        if access_token:
+        if token_hash:
+            # First we set an optional access token
             self._access_token = AccessToken(
-                access_token=cache.get("pretix_paypal_access_token"),
-                expires_in=cache.get("pretix_paypal_access_token_expires_in"),
-                token_type=cache.get("pretix_paypal_access_token_type"),
+                access_token=token_hash['access_token'],
+                expires_in=token_hash['expires_in'],
+                token_type=token_hash['token_type'],
             )
             # This is not part of the constructor - so we need to set it after the fact.
-            self._access_token.created_at = cache.get("pretix_paypal_access_token_created_at")
+            self._access_token.created_at = token_hash['created_at']
 
         # Only then we'll call the original __call__() method, as it will verify the validity of the tokens
         # and request new ones if required.
         super().__call__(request)
 
-        # At this point - if there were any changes in refresh or access-token, we should have them
-        # and can cache them again
-        if self._refresh_token:
-            cache.set("pretix_paypal_refresh_token", self._refresh_token)
-
-        if self._access_token and (not access_token or access_token != self._access_token.access_token):
+        # At this point - if there were any changes in access-token, we should have them and can cache them again
+        if self._access_token and (not token_hash or token_hash['access_token'] != self._access_token.access_token):
             expiration = self._access_token.expires_in - 60  # For good measure, we expire 60 seconds earlier
 
-            cache.set("pretix_paypal_access_token", self._access_token.access_token, expiration)
-            cache.set("pretix_paypal_access_token_expires_in", self._access_token.expires_in, expiration)
-            cache.set("pretix_paypal_access_token_type", self._access_token.token_type, expiration)
-            cache.set("pretix_paypal_access_token_created_at", self._access_token.created_at, expiration)
+            cache.set(cache_key_hash, {
+                'access_token': self._access_token.access_token,
+                'expires_in': self._access_token.expires_in,
+                'token_type': self._access_token.token_type,
+                'created_at': self._access_token.created_at
+            }, expiration)
 
         # And now for some housekeeping.
         if self.environment.merchant_id:
