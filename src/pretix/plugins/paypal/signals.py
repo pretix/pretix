3ee1420a2b891990cfc6776597d0d7deea2d71ca
Martin Gross <gross@rami.io>
2022-05-25 13:52:48 +0200

    Unify logdisplay

--- src/pretix/plugins/paypal/signals.py
+++ src/pretix/plugins/paypal/signals.py
@@ -22,10 +22,17 @@
 
 from django.dispatch import receiver
 
-from pretix.base.signals import register_payment_providers
+from pretix.base.signals import register_payment_providers, logentry_display
 
 
 @receiver(register_payment_providers, dispatch_uid="payment_paypal")
 def register_payment_provider(sender, **kwargs):
     from .payment import Paypal
     return Paypal
+
+
+@receiver(signal=logentry_display, dispatch_uid="paypal_logentry_display")
+def pretixcontrol_logentry_display(sender, logentry, **kwargs):
+    from pretix.plugins.paypal2.signals import pretixcontrol_logentry_display
+
+    return pretixcontrol_logentry_display(sender, logentry, **kwargs)
