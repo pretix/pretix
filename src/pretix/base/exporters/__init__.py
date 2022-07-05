db1c480905a2dfacea4d4cb8a597648178bf5c68
Raphael Michel <michel@rami.io>
2022-07-05 12:22:24 +0200

    Add exporter for list of products

--- src/pretix/base/exporters/__init__.py
+++ src/pretix/base/exporters/__init__.py
@@ -23,6 +23,7 @@ from .answers import *  # noqa
 from .dekodi import *  # noqa
 from .events import *  # noqa
 from .invoices import *  # noqa
+from .items import *  # noqa
 from .json import *  # noqa
 from .mail import *  # noqa
 from .orderlist import *  # noqa
