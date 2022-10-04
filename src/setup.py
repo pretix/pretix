14fdd7cfcad26c1b0747119e70529b0660a627e6
Raphael Michel <michel@rami.io>
2022-10-04 16:39:49 +0200

    Bump django-compressor to 4.1.*

--- src/setup.py
+++ src/setup.py
@@ -170,7 +170,7 @@ setup(
         'dj-static',
         'Django==3.2.*',
         'django-bootstrap3==22.1.*',
-        'django-compressor==3.1.*',
+        'django-compressor==4.1.*',
         'django-countries==7.3.*',
         'django-filter==21.1',
         'django-formset-js-improved==0.5.0.2',
