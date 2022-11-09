pretix/pretix

commit 0b95f89882f88768e5df770730663d93b0528c24

Fix paypal disabling continue button (Z#23110784) (#2875)

src/pretix/plugins/paypal2/static/pretixplugins/paypal2/pretix-paypal.js
@@ -72,8 +72,10 @@ var pretixpaypal = {
             pretixpaypal.currency = $("body").attr("data-currency");
             pretixpaypal.locale = this.guessLocale();
         }
-
-        pretixpaypal.continue_button.prop("disabled", true);
+        // if no payment option is selected, disable the continue button
+        if (!pretixpaypal.continue_button[0].form.elements['payment'].value) {
+            pretixpaypal.continue_button.prop("disabled", true);
+        }
 
         // We are setting the cogwheel already here, as the renderAPM() method might take some time to get loaded.
         let apmtextselector = $("label[for=input_payment_paypal_apm]");
@@ -310,7 +312,6 @@ var pretixpaypal = {
             'tr_TR',
         ]
         let lang = $("body").attr("data-locale").split('-')[0];
-        console.log(allowed_locales.find(element => element.startsWith(lang)));
         return allowed_locales.find(element => element.startsWith(lang));
     }
 };

commit 7d2dd722bd211340b32ca85f5113fb49a7b48166

PayPal: Fix loading of Smart Payment Buttons on APM payment page (regression introduced in #2875)

src/pretix/plugins/paypal2/static/pretixplugins/paypal2/pretix-paypal.js
@@ -72,9 +72,13 @@ var pretixpaypal = {
             pretixpaypal.currency = $("body").attr("data-currency");
             pretixpaypal.locale = this.guessLocale();
         }
-        // if no payment option is selected, disable the continue button
-        if (!pretixpaypal.continue_button[0].form.elements['payment'].value) {
-            pretixpaypal.continue_button.prop("disabled", true);
+
+        // If no payment option is selected, and we're not on the paypage (which doesn't have a continue button),
+        // disable the continue button
+        if (!pretixpaypal.paypage) {
+            if (!pretixpaypal.continue_button[0].form.elements['payment'].value) {
+                pretixpaypal.continue_button.prop("disabled", true);
+            }
         }
 
         // We are setting the cogwheel already here, as the renderAPM() method might take some time to get loaded.