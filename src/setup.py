fbf52a52199fd94488793cc6f0e40777c818f37b
Raphael Michel <michel@rami.io>
2022-11-10 09:13:15 +0100

    Bump Pillow to 9.3.*

--- src/setup.py
+++ src/setup.py
@@ -206,7 +206,7 @@ setup(
         'paypal-checkout-serversdk==1.0.*',
         'PyJWT==2.6.*',
         'phonenumberslite==8.12.*',
-        'Pillow==9.2.*',
+        'Pillow==9.3.*',
         'protobuf==4.21.*',
         'psycopg2-binary',
         'pycountry',
