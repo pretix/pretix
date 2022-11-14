4fcdaa644f4289afdef2583e1f943c9d45592d0f
Richard Schreiber <schreiber@rami.io>
2022-11-14 13:15:28 +0100

    use select2 for subevents selector

--- src/pretix/control/forms/waitinglist.py
+++ src/pretix/control/forms/waitinglist.py
@@ -1,16 +1,29 @@
+from django.urls import reverse
 from django_scopes.forms import SafeModelChoiceField
 
 from pretix.base.forms import I18nModelForm
 from pretix.base.models import WaitingListEntry
+from pretix.control.forms.widgets import Select2
 
 
 class WaitingListEntryEditForm(I18nModelForm):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.fields['subevent'].required = True
-        self.fields['subevent'].empty_label = None
-        self.fields['subevent'].queryset = self.event.subevents.filter(active=True)
+
+        if self.event.has_subevents:
+            self.fields['subevent'].required = True
+            self.fields['subevent'].queryset = self.event.subevents.filter(active=True)
+            self.fields['subevent'].widget = Select2(
+                attrs={
+                    'data-model-select2': 'event',
+                    'data-select2-url': reverse('control:event.subevents.select2', kwargs={
+                        'event': self.event.slug,
+                        'organizer': self.event.organizer.slug,
+                    }),
+                }
+            )
+            self.fields['subevent'].widget.choices = self.fields['subevent'].choices
 
     class Meta:
         model = WaitingListEntry
