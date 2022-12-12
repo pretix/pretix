47dabc1fe76ad3757fdfb0b300f8004b4856aca3
Raphael Michel <michel@rami.io>
2022-12-12 10:53:21 +0100

    Bump pytest-xdist to 3.1.*

--- src/setup.py
+++ src/setup.py
@@ -252,7 +252,7 @@ setup(
             'pytest-mock==3.10.*',
             'pytest-rerunfailures==10.*',
             'pytest-sugar',
-            'pytest-xdist==3.0.*',
+            'pytest-xdist==3.1.*',
             'pytest==7.2.*',
             'responses',
         ],
