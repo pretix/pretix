34cc07429de519294b65ebf4f46d757b8ca1f4a4
Raphael Michel <michel@rami.io>
2023-01-16 15:46:49 +0100

    Actually run it

--- src/pretix/base/services/export.py
+++ src/pretix/base/services/export.py
@@ -19,28 +19,44 @@
 # You should have received a copy of the GNU Affero General Public License along with this program.  If not, see
 # <https://www.gnu.org/licenses/>.
 #
+import logging
+from datetime import timedelta
 from typing import Any, Dict
 
+from celery.exceptions import MaxRetriesExceededError
 from django.conf import settings
 from django.core.files.base import ContentFile
-from django.utils.timezone import override
+from django.dispatch import receiver
+from django.utils.timezone import override, now
 from django.utils.translation import gettext
+from django_scopes import scopes_disabled
+from i18nfield.strings import LazyI18nString
 
+from pretix.base.email import get_email_context
 from pretix.base.exporter import OrganizerLevelExportMixin
 from pretix.base.i18n import LazyLocaleException, language
 from pretix.base.models import (
-    CachedFile, Device, Event, Organizer, TeamAPIToken, User, cachedfile_name,
+    CachedFile, Device, Event, Organizer, TeamAPIToken, User, cachedfile_name, ScheduledEventExport,
 )
 from pretix.base.services.tasks import (
     ProfiledEventTask, ProfiledOrganizerUserTask,
 )
+from pretix.base.services.mail import mail
 from pretix.base.signals import (
-    register_data_exporters, register_multievent_data_exporters,
+    register_data_exporters, register_multievent_data_exporters, periodic_task,
 )
 from pretix.celery_app import app
+from pretix.helpers.urls import build_absolute_uri
+
+
+logger = logging.getLogger(__name__)
 
 
 class ExportError(LazyLocaleException):
+    pass
+
+
+class ExportEmptyError(ExportError):
     pass
 
 
@@ -56,7 +72,7 @@ def export(self, event: Event, fileid: str, provider: str, form_data: Dict[str, 
     file = CachedFile.objects.get(id=fileid)
     with language(event.settings.locale, event.settings.region), override(event.settings.timezone):
         responses = register_data_exporters.send(event)
-        for receiver, response in responses:
+        for recv, response in responses:
             if not response:
                 continue
             ex = response(event, event.organizer, set_progress)
@@ -115,7 +131,7 @@ def multiexport(self, organizer: Organizer, user: User, device: int, token: int,
             events = allowed_events
         responses = register_multievent_data_exporters.send(organizer)
 
-        for receiver, response in responses:
+        for recv, response in responses:
             if not response:
                 continue
             ex = response(events, organizer, set_progress)
@@ -138,3 +154,102 @@ def multiexport(self, organizer: Organizer, user: User, device: int, token: int,
                 f = ContentFile(data)
                 file.file.save(cachedfile_name(file, file.filename), f)
     return file.pk
+
+
+@app.task(base=ProfiledEventTask, bind=True, max_retries=5, default_retry_delay=120)
+def scheduled_export(self, event: Event, schedule: int) -> None:
+    schedule = event.scheduled_exports.get(pk=schedule)
+
+    with language(schedule.locale, event.settings.region), override(event.settings.timezone):
+        file = CachedFile(web_download=False)
+        file.date = now()
+        file.expires = now() + timedelta(hours=24)
+        file.save()
+
+        def _handle_error(msg, soft=False):
+            mail(
+                email=schedule.owner.email,
+                subject=gettext('Export failed'),
+                template='pretixbase/email/export_failed.txt',
+                context={
+                    'configuration_url': build_absolute_uri(
+                        'control:event.orders.export',
+                        kwargs={
+                            'event': event.slug,
+                            'organizer': event.organizer.slug,
+                        }
+                    ) + f'?identifier={schedule.export_identifier}&scheduled={schedule.pk}',
+                    'reason': msg,
+                    'soft': soft,
+                },
+                event=event,
+                locale=schedule.locale,
+            )
+            if not soft:
+                schedule.error_counter += 1
+                schedule.error_last_message = msg
+                schedule.save(update_fields=['error_counter', 'error_last_message'])
+
+        if not schedule.owner.has_event_permission(event.organizer, event, 'can_view_orders'):
+            _handle_error(gettext('Permission denied.'))
+
+        try:
+            responses = register_data_exporters.send(event)
+            for recv, response in responses:
+                if not response:
+                    continue
+                ex = response(event, event.organizer)
+                if ex.identifier == schedule.export_identifier:
+                    d = ex.render(schedule.export_form_data)
+                    if d is None:
+                        raise ExportEmptyError(
+                            gettext('Your export did not contain any data.')
+                        )
+                    file.filename, file.type, data = d
+                    if len(data) > 20 * 1024 * 1024:  # 20 MB
+                        raise ExportError(
+                            gettext('Your exported data exceeded the size limit for scheduled exports.')
+                        )
+                    f = ContentFile(data)
+                    file.file.save(cachedfile_name(file, file.filename), f)
+                    break
+            else:
+                raise ExportError("Export type not found.")
+        except ExportEmptyError as e:
+            _handle_error(e, soft=True)
+        except ExportError as e:
+            _handle_error(e, soft=False)
+        except Exception:
+            logger.exception("Scheduled export failed.")
+            try:
+                self.retry()
+            except MaxRetriesExceededError:
+                _handle_error('Internal Error')
+        else:
+            mail(
+                email=[schedule.owner.email] + [r for r in schedule.mail_additional_recipients.split(",") if r],
+                cc=[r for r in schedule.mail_additional_recipients_cc.split(",") if r],
+                bcc=[r for r in schedule.mail_additional_recipients_bcc.split(",") if r],
+                subject=schedule.mail_subject,
+                template=LazyI18nString(schedule.mail_template),
+                context=get_email_context(event=event),
+                event=event,
+                locale=schedule.locale,
+                attach_cached_files=[file],
+            )
+            return True
+
+
+@receiver(signal=periodic_task)
+@scopes_disabled()
+def run_scheduled_exports(sender, **kwargs):
+    qs = ScheduledEventExport.objects.filter(
+        schedule_next_run__lt=now(),
+    ).select_related('event')
+    for s in qs:
+        scheduled_export.apply_async(kwargs={
+            'event': s.event.pk,
+            'schedule': s.pk,
+        })
+        s.compute_next_run()
+        s.save(update_fields=['schedule_next_run'])
