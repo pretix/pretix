9696fad482105367c7c1d6becb372747e79d8050
Raphael Michel <mail@raphaelmichel.de>
2023-04-16 22:08:02 +0200

    Bump Pillow to 9.5.*

--- src/setup.py
+++ src/setup.py
@@ -206,7 +206,7 @@ setup(
         'paypal-checkout-serversdk==1.0.*',
         'PyJWT==2.6.*',
         'phonenumberslite==8.13.*',
-        'Pillow==9.4.*',
+        'Pillow==9.5.*',
         'protobuf==4.22.*',
         'psycopg2-binary',
         'pycountry',
