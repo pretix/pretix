39855b676e141cd6da46325b10422dfdb2d53f7a
Richard Schreiber <schreiber@rami.io>
2023-04-25 16:27:04 +0200

    fix details return-key

--- src/pretix/static/pretixbase/js/details.js
+++ src/pretix/static/pretixbase/js/details.js
@@ -31,6 +31,10 @@ setup_collapsible_details = function (el) {
         e.preventDefault();
         return false;
     }).keyup(function (event) {
+        if ($details.hasClass('sneak-peek')) {
+            // if sneak-peek is active, needs to be handled differently
+            return true;
+        }
         if (32 == event.keyCode || (13 == event.keyCode && !isOpera)) {
             // Space or Enter is pressed — trigger the `click` event on the `summary` element
             // Opera already seems to trigger the `click` event when Enter is pressed
