928120a0245b055c1b9c82a62cd864b1ecbf8b11
Raphael Michel <michel@rami.io>
2023-05-22 10:52:46 +0200

    Invoices: Support font choice and Arabic text

--- src/pretix/base/pdf.py
+++ src/pretix/base/pdf.py
@@ -48,7 +48,6 @@ from functools import partial
 from io import BytesIO
 
 import jsonschema
-from arabic_reshaper import ArabicReshaper
 from bidi.algorithm import get_display
 from django.conf import settings
 from django.contrib.staticfiles import finders
@@ -57,7 +56,6 @@ from django.db.models import Max, Min
 from django.dispatch import receiver
 from django.utils.deconstruct import deconstructible
 from django.utils.formats import date_format
-from django.utils.functional import SimpleLazyObject
 from django.utils.html import conditional_escape
 from django.utils.timezone import now
 from django.utils.translation import gettext_lazy as _, pgettext
@@ -78,12 +76,12 @@ from reportlab.pdfgen.canvas import Canvas
 from reportlab.platypus import Paragraph
 
 from pretix.base.i18n import language
-from pretix.base.invoice import ThumbnailingImageReader
 from pretix.base.models import Order, OrderPosition, Question
 from pretix.base.settings import PERSON_NAME_SCHEMES
 from pretix.base.signals import layout_image_variables, layout_text_variables
 from pretix.base.templatetags.money import money_filter
 from pretix.base.templatetags.phone_format import phone_format
+from pretix.helpers.reportlab import ThumbnailingImageReader, reshaper
 from pretix.presale.style import get_fonts
 
 logger = logging.getLogger(__name__)
@@ -697,12 +695,6 @@ def get_seat(op: OrderPosition):
     if op.addon_to_id:
         return op.addon_to.seat
     return None
-
-
-reshaper = SimpleLazyObject(lambda: ArabicReshaper(configuration={
-    'delete_harakat': True,
-    'support_ligatures': False,
-}))
 
 
 class Renderer:
