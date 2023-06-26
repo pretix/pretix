341ac6eda2935a88f6b7e3c45fa078ae10d7dd35
Phin Wolkwitz <wolkwitz@rami.io>
2023-06-26 12:40:05 +0200

    Improve migrations

--- src/pretix/plugins/sendmail/migrations/0004_rule_restrict_to_status.py
+++ src/pretix/plugins/sendmail/migrations/0004_rule_restrict_to_status.py
@@ -3,16 +3,31 @@ from django.db import migrations
 import pretix.base.models.fields
 
 
+def migrate_status_rules(apps, schema_editor):
+    rule_model = apps.get_model("sendmail", "Rule")
+    for r in rule_model.objects.all():
+        r.restrict_to_status = ['p', 'valid_if_pending']
+        if r.include_pending:
+            r.restrict_to_status.append('na')
+        r.save()
+
+
 class Migration(migrations.Migration):
 
     dependencies = [
         ('sendmail', '0003_rule_attach_ical'),
+        ('pretixbase', '0241_itemmetaproperties_required_values'),
     ]
 
     operations = [
         migrations.AddField(
             model_name='rule',
             name='restrict_to_status',
-            field=pretix.base.models.fields.MultiStringField(default=['p', 'na', 'valid_if_pending']),
+            field=pretix.base.models.fields.MultiStringField(default=['p', 'valid_if_pending']),
+        ),
+        migrations.RunPython(migrate_status_rules),
+        migrations.RemoveField(
+            model_name='rule',
+            name='include_pending',
         ),
     ]
