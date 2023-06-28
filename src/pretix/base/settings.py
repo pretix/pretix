f6486acf050851ffc671454acef688651c6bc8e1
Raphael Michel <michel@rami.io>
2023-06-28 13:30:26 +0200

    Update src/pretix/base/settings.py
    Co-authored-by: Richard Schreiber <schreiber@rami.io>

--- src/pretix/base/settings.py
+++ src/pretix/base/settings.py
@@ -903,7 +903,7 @@ DEFAULTS = {
             help_text=_("The order will only actually expire this many days after the expiration date communicated "
                         "to the customer. However, this will not delay beyond the \"last date of payments\" "
                         "configured above, which is always enforced. The delay may also end on a weekend regardless "
-                        "of the other serttings above."),
+                        "of the other settings above."),
             # Every order in between the official expiry date and the delayed expiry date has a performance penalty
             # for the cron job, so we limit this feature to 30 days to prevent arbitrary numbers of orders needing
             # to be checked.
