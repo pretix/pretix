4ee4fb67a828d3ddfec9c54fb3954211f0bc18e7
Ronan LE MEILLAT <eltorio@users.noreply.github.com>
2023-08-05 13:36:10 +0200

    isort code quality

--- src/pretix/helpers/thumb.py
+++ src/pretix/helpers/thumb.py
@@ -22,8 +22,8 @@
 import hashlib
 import math
 import os
-
 from io import BytesIO
+
 from cairosvg import svg2png
 from django.core.files.base import ContentFile
 from django.core.files.storage import default_storage
