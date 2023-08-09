6250ab216558f705a692afdfcd9d9c90d704295f
Mira <weller@rami.io>
2023-08-09 18:23:45 +0200

    Bank transfer: Allow customer to send latest invoice via email (Z#207218) (#3511)
    Co-authored-by: Raphael Michel <michel@rami.io>

--- src/pretix/base/models/orders.py
+++ src/pretix/base/models/orders.py
@@ -1676,7 +1676,7 @@ class OrderPayment(models.Model):
         """
         Marks the order as failed and sets info to ``info``, but only if the order is in ``created`` or ``pending``
         state. This is equivalent to setting ``state`` to ``OrderPayment.PAYMENT_STATE_FAILED`` and logging a failure,
-        but it adds strong database logging since we do not want to report a failure for an order that has just
+        but it adds strong database locking since we do not want to report a failure for an order that has just
         been marked as paid.
         :param send_mail: Whether an email should be sent to the user about this event (default: ``True``).
         """
