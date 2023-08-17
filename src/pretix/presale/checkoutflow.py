d081154d638261242711d5ba1d8c346100936e32
Mira Weller <weller@rami.io>
2023-08-17 19:20:31 +0200

    Remove print statements

--- src/pretix/presale/checkoutflow.py
+++ src/pretix/presale/checkoutflow.py
@@ -707,7 +707,6 @@ def business_heuristic(organizer):
     if cached_result is None:
         result = InvoiceAddress.objects.filter(order__event__organizer=organizer).aggregate(
             total=Count('*'), business=Sum(Cast('is_business', output_field=models.IntegerField())))
-        print("business_heuristic", organizer.slug, result)
         is_business = result['business'] / result['total'] >= 0.6
         organizer.cache.set('checkout_heuristic_is_business', str(is_business), timeout=15)
         return is_business
