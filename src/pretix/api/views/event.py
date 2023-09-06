231e05f967376d9233070d0e89556ad7c900370d
robbi5 <richt@rami.io>
2023-09-06 10:10:04 +0200

    API: Add parent event slug to subevent list search (#3567)
    Co-authored-by: Raphael Michel <michel@rami.io>

--- src/pretix/api/views/event.py
+++ src/pretix/api/views/event.py
@@ -381,15 +381,28 @@ with scopes_disabled():
                 | Q(location__icontains=i18ncomp(value))
             )
 
+    class OrganizerSubEventFilter(SubEventFilter):
+        def search_qs(self, queryset, name, value):
+            return queryset.filter(
+                Q(name__icontains=i18ncomp(value))
+                | Q(event__slug__icontains=value)
+                | Q(location__icontains=i18ncomp(value))
+            )
+
 
 class SubEventViewSet(ConditionalListView, viewsets.ModelViewSet):
     serializer_class = SubEventSerializer
     queryset = SubEvent.objects.none()
     write_permission = 'can_change_event_settings'
     filter_backends = (DjangoFilterBackend, TotalOrderingFilter)
-    filterset_class = SubEventFilter
     ordering = ('date_from',)
     ordering_fields = ('id', 'date_from', 'last_modified')
+
+    @property
+    def filterset_class(self):
+        if getattr(self.request, 'event', None):
+            return SubEventFilter
+        return OrganizerSubEventFilter
 
     def get_queryset(self):
         if getattr(self.request, 'event', None):
