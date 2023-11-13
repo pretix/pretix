db9049130c7843c2c6c7586354aa138e7ba65144
Raphael Michel <michel@rami.io>
2023-11-13 12:42:58 +0100

    Do not send password-reset for non-native users

--- src/pretix/control/views/auth.py
+++ src/pretix/control/views/auth.py
@@ -266,7 +266,7 @@ class Forgot(TemplateView):
             has_redis = settings.HAS_REDIS
 
             try:
-                user = User.objects.get(is_active=True, email__iexact=email)
+                user = User.objects.get(is_active=True, auth_backend='native', email__iexact=email)
 
                 if has_redis:
                     from django_redis import get_redis_connection
