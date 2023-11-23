8c80200fc0c24798d897e28753da48615bd52e0e
Julian Baumann <jeriox@users.noreply.github.com>
2023-11-23 09:48:28 +0100

    Orders: Add bulk action to refund overpaid amount (#3721)
    * add bulk action to refund overpaid amount
    
    * display number of successful actions, use existing annotate method
    
    * add tests, address review comments
    
    * lint

--- src/pretix/control/views/orders.py
+++ src/pretix/control/views/orders.py
@@ -213,10 +213,14 @@ class BaseOrderBulkActionView(OrderSearchMixin, EventPermissionRequiredMixin, As
     def execute_bulk(self, queryset: QuerySet, form: forms.Form):
         qs = self.allowed_for(self.allowed_for(self.get_queryset()))
         total = qs.count()
+        orders_with_successful_action = 0
         for i, o in enumerate(qs):
-            self.execute_single(o, form)
+            res = self.execute_single(o, form)
+            if res:
+                orders_with_successful_action += 1
             if i % 100 == 0:
                 self.async_set_progress(i / total * 100)
+        return orders_with_successful_action, total
 
     def get_error_url(self):
         return self.get_success_url(None)
@@ -231,6 +235,9 @@ class BaseOrderBulkActionView(OrderSearchMixin, EventPermissionRequiredMixin, As
             'event': self.request.event.slug,
             'organizer': self.request.event.organizer.slug,
         })
+
+    def get_success_message(self, value):
+        return _("Succesfully executed the action \"{label}\" on {success} of {total} orders.").format(success=value[0], label=self.label, total=value[1])
 
     def get_context_data(self, **kwargs):
         ctx = super().get_context_data(**kwargs)
@@ -271,7 +278,7 @@ class BaseOrderBulkActionView(OrderSearchMixin, EventPermissionRequiredMixin, As
 
     @transaction.atomic()
     def async_form_valid(self, task, form):
-        self.execute_bulk(self.allowed_for(self.get_queryset()), form)
+        return self.execute_bulk(self.allowed_for(self.get_queryset()), form)
 
 
 class OrderApproveBulkActionView(BaseOrderBulkActionView):
@@ -285,6 +292,7 @@ class OrderApproveBulkActionView(BaseOrderBulkActionView):
 
     def execute_single(self, instance, form: forms.Form):
         approve_order(instance, user=self.request.user)
+        return True
 
 
 class OrderDenyBulkActionView(BaseOrderBulkActionView):
@@ -301,6 +309,7 @@ class OrderDenyBulkActionView(BaseOrderBulkActionView):
         deny_order(instance, user=self.request.user,
                    comment=form.cleaned_data.get('comment') or None,
                    send_mail=form.cleaned_data['send_email'])
+        return True
 
 
 class OrderExpireBulkActionView(BaseOrderBulkActionView):
@@ -315,6 +324,33 @@ class OrderExpireBulkActionView(BaseOrderBulkActionView):
 
     def execute_single(self, instance, form: forms.Form):
         mark_order_expired(instance, user=self.request.user)
+        return True
+
+
+class OrderOverpaidRefundBulkActionView(BaseOrderBulkActionView):
+    label = _("Refund overpaid amount")
+
+    def allowed_for(self, queryset):
+        return Order.annotate_overpayments(queryset).filter(is_overpaid=True)
+
+    def execute_single(self, instance: Order, form: forms.Form):
+        if instance.pending_sum < 0:
+            try:
+                proposals = instance.propose_auto_refunds(instance.pending_sum * -1)
+                for payment, amount in proposals.items():
+                    refund = OrderRefund.objects.create(
+                        order=instance,
+                        payment=payment,
+                        source=OrderRefund.REFUND_SOURCE_ADMIN,
+                        state=OrderRefund.REFUND_STATE_CREATED,
+                        amount=amount,
+                        comment=_("Refund for overpayment"),
+                        provider=payment.provider
+                    )
+                    payment.payment_provider.execute_refund(refund)
+                    return True
+            except (ValueError, PaymentException):
+                return False
 
 
 class OrderDeleteBulkActionView(BaseOrderBulkActionView):
