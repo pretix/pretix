ca66138840c3e2ee9d87574d628fb09914721dfd
Mira Weller <weller@rami.io>
2023-11-24 14:51:14 +0100

    rebase migration

--- src/pretix/base/migrations/0253_item_unavail_modes.py
+++ src/pretix/base/migrations/0253_item_unavail_modes.py
@@ -5,7 +5,7 @@ from django.db import migrations, models
 
 class Migration(migrations.Migration):
     dependencies = [
-        ("pretixbase", "0250_eventmetaproperty_filter_public"),
+        ("pretixbase", "0252_logentry_organizer"),
     ]
 
     operations = [
