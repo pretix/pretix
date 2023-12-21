092b51f8f3b8f1d8ffc19654540cd794a66d90f9
Raphael Michel <michel@rami.io>
2023-12-21 13:42:35 +0100

    Waiting list: Fix crash with invalid subevent id

--- src/pretix/presale/views/waiting.py
+++ src/pretix/presale/views/waiting.py
@@ -24,6 +24,7 @@ from datetime import timedelta
 from django.conf import settings
 from django.contrib import messages
 from django.db import transaction
+from django.http import Http404
 from django.shortcuts import get_object_or_404, redirect, render
 from django.utils.decorators import method_decorator
 from django.utils.timezone import now
@@ -107,8 +108,11 @@ class WaitingView(EventViewMixin, FormView):
         self.subevent = None
         if request.event.has_subevents:
             if 'subevent' in request.GET:
-                self.subevent = get_object_or_404(SubEvent, event=request.event, pk=request.GET['subevent'],
-                                                  active=True)
+                try:
+                    self.subevent = get_object_or_404(SubEvent, event=request.event, pk=request.GET['subevent'],
+                                                      active=True)
+                except ValueError:
+                    raise Http404()
             else:
                 messages.error(request, pgettext_lazy('subevent', "You need to select a date."))
                 return redirect(self.get_index_url())
