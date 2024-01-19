5bfdb0894ed051f4b5c3e03fd2a02ee169ec11de
Raphael Michel <michel@rami.io>
2024-01-19 16:52:22 +0100

    Fix form validation

--- src/pretix/base/settings.py
+++ src/pretix/base/settings.py
@@ -310,6 +310,7 @@ DEFAULTS = {
         ),
         'form_kwargs': dict(
             min_value=1,
+            max_value=settings.PRETIX_MAX_ORDER_SIZE,
             required=True,
             label=_("Maximum number of items per order"),
             help_text=_("Add-on products will not be counted.")
