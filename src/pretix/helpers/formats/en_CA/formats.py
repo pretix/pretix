0cd2cdbf30e3ff329c1222c543b1b9dc9a69af37
Candide U <55474996+candideu@users.noreply.github.com>
2024-01-29 15:33:51 -0500

    Create formats.py for Canadian English
    Referencing: https://github.com/pretix/pretix/discussions/3842
    
    
    Based on:
    
    - https://github.com/pretix/pretix/blob/master/src/pretix/helpers/formats/en_US/formats.py (for time formatting and structure)
    
    - https://github.com/django/django/blob/main/django/conf/locale/fr_CA/formats.py (For dates)

---
+++ src/pretix/helpers/formats/en_CA/formats.py
@@ -0,0 +1,54 @@
+#
+# This file is part of pretix (Community Edition).
+#
+# Copyright (C) 2014-2020 Raphael Michel and contributors
+# Copyright (C) 2020-2021 rami.io GmbH and contributors
+#
+# This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General
+# Public License as published by the Free Software Foundation in version 3 of the License.
+#
+# ADDITIONAL TERMS APPLY: Pursuant to Section 7 of the GNU Affero General Public License, additional terms are
+# applicable granting you additional permissions and placing additional restrictions on your usage of this software.
+# Please refer to the pretix LICENSE file to obtain the full terms applicable to this work. If you did not receive
+# this file, see <https://pretix.eu/about/en/license>.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+# warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Affero General Public License along with this program.  If not, see
+# <https://www.gnu.org/licenses/>.
+#
+
+# This file is based on an earlier version of pretix which was released under the Apache License 2.0. The full text of
+# the Apache License 2.0 can be obtained at <http://www.apache.org/licenses/LICENSE-2.0>.
+#
+# This file may have since been changed and any changes are released under the terms of AGPLv3 as described above. A
+# full history of changes and contributors is available at <https://github.com/pretix/pretix>.
+#
+# This file contains Apache-licensed contributions copyrighted by: Tobias Kunze
+#
+# Unless required by applicable law or agreed to in writing, software distributed under the Apache License 2.0 is
+# distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations under the License.
+
+
+# Date according to https://docs.djangoproject.com/en/dev/ref/templates/builtins/#date
+SHORT_DATE_FORMAT = "Y-m-d"
+SHORT_DATETIME_FORMAT = 'Y-m-d P'
+TIME_FORMAT = 'P'
+WEEK_FORMAT = '\\W W, o'
+WEEK_DAY_FORMAT = 'D, M jS'
+SHORT_MONTH_DAY_FORMAT = 'm/d'
+
+DATE_INPUT_FORMATS = [
+    "%d/%m/%Y",  # '15-05-2006'
+    "%Y-%m-%d",  # '2006-05-15'
+    "%y-%m-%d",  # '06-05-15'
+]
+TIME_INPUT_FORMATS = [
+    '%I:%M %p',
+    '%H:%M:%S',  # '14:30:59'
+    '%H:%M:%S.%f',  # '14:30:59.000200'
+    '%H:%M',  # '14:30'
+]
