166b5e4f3b6cf4e9528d511699a85093b9cc334f
Raphael Michel <michel@rami.io>
2024-05-02 18:22:02 +0200

    Change default password hash to argon2id (#4121)
    * Change default password hash to argon2id
    
    * Install argon2

--- src/pretix/settings.py
+++ src/pretix/settings.py
@@ -704,6 +704,13 @@ BOOTSTRAP3 = {
     },
 }
 
+PASSWORD_HASHERS = [
+    "django.contrib.auth.hashers.Argon2PasswordHasher",
+    "django.contrib.auth.hashers.PBKDF2PasswordHasher",
+    "django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher",
+    "django.contrib.auth.hashers.BCryptSHA256PasswordHasher",
+    "django.contrib.auth.hashers.ScryptPasswordHasher",
+]
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
