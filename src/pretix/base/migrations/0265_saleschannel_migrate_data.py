c5c782853ccabb02d7ca0e26b948ca4542f8ad85
Raphael Michel <michel@rami.io>
2024-05-21 11:37:18 +0200

    Fix migration query

--- src/pretix/base/migrations/0265_saleschannel_migrate_data.py
+++ src/pretix/base/migrations/0265_saleschannel_migrate_data.py
@@ -19,8 +19,8 @@ def create_sales_channels(apps, schema_editor):
                 continue
             type_to_channel[t.identifier, o.pk] = o.sales_channels.get_or_create(
                 type=t.identifier,
-                position=i,
                 defaults=dict(
+                    position=i,
                     identifier=t.identifier,
                     label=LazyI18nString.from_gettext(t.verbose_name),
                 ),
