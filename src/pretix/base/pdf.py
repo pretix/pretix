618fa456cc0a76f76f1dbb4306e760ac61028648
Richard Schreiber <schreiber@rami.io>
2024-05-22 13:07:18 +0200

    fix rotated PDF-backgrounds

--- src/pretix/base/pdf.py
+++ src/pretix/base/pdf.py
@@ -1096,27 +1096,28 @@ class Renderer:
                 bg_page = self.bg_pdf.pages[i]
                 bg_rotation = bg_page.get('/Rotate')
                 if bg_rotation:
+                    # page.merge_page loses /Rotate on page2 so we need to manually add it as a transformation
                     # /Rotate is clockwise, transformation.rotate is counter-clockwise
-                    t = Transformation().rotate(bg_rotation)
-                    w = float(page.mediabox.width)
-                    h = float(page.mediabox.height)
+                    t = Transformation()
+                    w = bg_page.mediabox.width
+                    h = bg_page.mediabox.height
                     if bg_rotation in (90, 270):
                         # offset due to rotation base
                         if bg_rotation == 90:
-                            t = t.translate(h, 0)
+                            t = t.rotate(270).translate(0, w)
                         else:
-                            t = t.translate(0, w)
+                            t = t.rotate(90).translate(h, 0)
                         # rotate mediabox as well
-                        page.mediabox = RectangleObject((
-                            page.mediabox.left.as_numeric(),
-                            page.mediabox.bottom.as_numeric(),
-                            page.mediabox.top.as_numeric(),
-                            page.mediabox.right.as_numeric(),
+                        bg_page.mediabox = RectangleObject((
+                            bg_page.mediabox.left.as_numeric(),
+                            bg_page.mediabox.bottom.as_numeric(),
+                            bg_page.mediabox.top.as_numeric(),
+                            bg_page.mediabox.right.as_numeric(),
                         ))
-                        page.trimbox = page.mediabox
+                        bg_page.trimbox = bg_page.mediabox
                     elif bg_rotation == 180:
-                        t = t.translate(w, h)
-                    page.add_transformation(t)
+                        t = t.rotate(180).translate(w, h)
+                    bg_page.add_transformation(t)
                 page.merge_page(bg_page, over=False)
                 output.add_page(page)
 
@@ -1154,27 +1155,28 @@ def merge_background(fg_pdf, bg_pdf, out_file, compress):
             bg_page = bg_pdf.pages[i]
             bg_rotation = bg_page.get('/Rotate')
             if bg_rotation:
+                # page.merge_page loses /Rotate on page2 so we need to manually add it as a transformation
                 # /Rotate is clockwise, transformation.rotate is counter-clockwise
-                t = Transformation().rotate(bg_rotation)
-                w = float(page.mediabox.width)
-                h = float(page.mediabox.height)
+                t = Transformation()
+                w = bg_page.mediabox.width
+                h = bg_page.mediabox.height
                 if bg_rotation in (90, 270):
                     # offset due to rotation base
                     if bg_rotation == 90:
-                        t = t.translate(h, 0)
+                        t = t.rotate(270).translate(0, w)
                     else:
-                        t = t.translate(0, w)
+                        t = t.rotate(90).translate(h, 0)
                     # rotate mediabox as well
-                    page.mediabox = RectangleObject((
-                        page.mediabox.left.as_numeric(),
-                        page.mediabox.bottom.as_numeric(),
-                        page.mediabox.top.as_numeric(),
-                        page.mediabox.right.as_numeric(),
+                    bg_page.mediabox = RectangleObject((
+                        bg_page.mediabox.left.as_numeric(),
+                        bg_page.mediabox.bottom.as_numeric(),
+                        bg_page.mediabox.top.as_numeric(),
+                        bg_page.mediabox.right.as_numeric(),
                     ))
-                    page.trimbox = page.mediabox
+                    bg_page.trimbox = bg_page.mediabox
                 elif bg_rotation == 180:
-                    t = t.translate(w, h)
-                page.add_transformation(t)
+                    t = t.rotate(180).translate(w, h)
+                bg_page.add_transformation(t)
             page.merge_page(bg_page, over=False)
             output.add_page(page)
         output.write(out_file)
