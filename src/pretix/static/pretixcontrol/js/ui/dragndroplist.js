93a9528d6687fc31ba2a0275f02b4f68856f178d
Mira Weller <weller@rami.io>
2024-06-13 12:57:18 +0200

    show sort buttons if user fails at drag and drop, add labels for accessibility

--- src/pretix/static/pretixcontrol/js/ui/dragndroplist.js
+++ src/pretix/static/pretixcontrol/js/ui/dragndroplist.js
@@ -1,7 +1,7 @@
 /*global $, Sortable*/
 $(function () {
     $("[data-dnd-url]").each(function(){
-        var container = $(this),
+        const container = $(this),
             url = container.data("dnd-url"),
             handle = $('<span class="btn btn-default btn-sm dnd-sort-handle"><i class="fa fa-arrows"></i></span>');
 
@@ -11,7 +11,14 @@ $(function () {
             handle.addClass("disabled");
             return;
         }
-
+        let didSort = false, lastClick = 0;
+        function handleMouseUp() {
+            if (Date.now() - lastClick < 6000) {
+                container.find(".sortable-up, .sortable-down").removeClass("sr-only");
+            }
+            lastClick = Date.now();
+        }
+        container.find(".dnd-sort-handle").on("mouseup", handleMouseUp);
         Sortable.create(container.get(0), {
             filter: ".sortable-disabled",
             handle: ".dnd-sort-handle",
@@ -22,16 +29,18 @@ $(function () {
             onStart: function (evt) {
                 container.addClass("sortable-dragarea");
                 container.parent().addClass("sortable-sorting");
+                didSort = false;
             },
             onEnd: function (evt) {
                 container.removeClass("sortable-dragarea");
                 container.parent().removeClass("sortable-sorting");
+                if (!didSort) handleMouseUp();
             },
             onSort: function (evt){
                 if (evt.target !== evt.to) return;
+                didSort = true;
 
-                var ids = container.find("[data-dnd-id]").toArray().map(function (e) { return e.dataset.dndId; });
-
+                const ids = container.find("[data-dnd-id]").toArray().map(function (e) { return e.dataset.dndId; });
                 $.ajax(
                     {
                         'type': 'POST',
