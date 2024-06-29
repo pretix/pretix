fa74862b5bfb45fe3d14e96f736324fd955237e0
Raphael Michel <michel@rami.io>
2024-06-29 14:32:16 +0200

    Make PCI-relevant pages detectable

--- src/pretix/presale/checkoutflow.py
+++ src/pretix/presale/checkoutflow.py
@@ -1263,6 +1263,7 @@ class PaymentStep(CartMixin, TemplateFlowStep):
 
     def post(self, request):
         self.request = request
+        self.request.pci_dss_payment_page = True
 
         if "remove_payment" in request.POST:
             self._remove_payment(request.POST["remove_payment"])
@@ -1426,6 +1427,10 @@ class PaymentStep(CartMixin, TemplateFlowStep):
                     self.cart_session['payments'] = [p for p in self.cart_session['payments'] if p['provider'] != provider.identifier]
 
         return True
+
+    def get(self, request):
+        self.request.pci_dss_payment_page = True
+        return super().get(request)
 
 
 class ConfirmStep(CartMixin, AsyncAction, TemplateFlowStep):
