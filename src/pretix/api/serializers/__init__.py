94d13e4cdd932f704c05e92d3f0714b70a84a0d1
Raphael Michel <michel@rami.io>
2024-07-02 17:49:09 +0200

    API: Raise the right validation error (PRETIXEU-ABV)

--- src/pretix/api/serializers/__init__.py
+++ src/pretix/api/serializers/__init__.py
@@ -21,8 +21,8 @@
 #
 import json
 
-from django.core.exceptions import ValidationError
 from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
 
 
 class AsymmetricField(serializers.Field):
