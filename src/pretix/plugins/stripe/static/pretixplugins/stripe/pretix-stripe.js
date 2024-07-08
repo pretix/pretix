5997d3922cf4d76fb6c6eb4c761580197dadec09
Richard Schreiber <schreiber@rami.io>
2024-07-08 11:53:04 +0200

    Fix stripe SCA redirect/sandboxing

--- src/pretix/plugins/stripe/static/pretixplugins/stripe/pretix-stripe.js
+++ src/pretix/plugins/stripe/static/pretixplugins/stripe/pretix-stripe.js
@@ -295,6 +295,7 @@ var pretixstripe = {
             let iframe = document.createElement('iframe');
             iframe.src = payment_intent_next_action_redirect_url;
             iframe.className = 'embed-responsive-item';
+            iframe.setAttribute('sandbox', 'allow-top-navigation');
             $('#scacontainer').append(iframe);
             $('#scacontainer iframe').on("load", function () {
                 waitingDialog.hide();
