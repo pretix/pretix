89727152529c4ac974073c3542a95f2bcc32ecc0
Richard Schreiber <schreiber@rami.io>
2024-08-26 12:01:25 +0200

    Fix initial sidebar display for mobile

--- src/pretix/static/pretixcontrol/js/sb-admin-2.js
+++ src/pretix/static/pretixcontrol/js/sb-admin-2.js
@@ -8,26 +8,26 @@
 //Loads the correct sidebar on window load,
 //collapses the sidebar on window resize.
 // Sets the min-height of #page-wrapper to window size
+$(window).bind("load resize", function () {
+    'use strict';
+    var topOffset = 50,
+        width = (this.window.innerWidth > 0) ? this.window.innerWidth : this.screen.width;
+    if (width < 768) {
+        $('div.navbar-collapse').addClass('collapse');
+        topOffset = 100; // 2-row-menu
+    } else {
+        $('div.navbar-collapse').removeClass('collapse');
+    }
+
+    var height = ((this.window.innerHeight > 0) ? this.window.innerHeight : this.screen.height) - 1;
+    height = height - topOffset;
+    if (height < 1) height = 1;
+    if (height > topOffset) {
+        $("#page-wrapper").css("min-height", (height) + "px");
+    }
+});
 $(function () {
     'use strict';
-    $(window).bind("load resize", function () {
-        var topOffset = 50,
-            width = (this.window.innerWidth > 0) ? this.window.innerWidth : this.screen.width;
-        if (width < 768) {
-            $('div.navbar-collapse').addClass('collapse');
-            topOffset = 100; // 2-row-menu
-        } else {
-            $('div.navbar-collapse').removeClass('collapse');
-        }
-
-        var height = ((this.window.innerHeight > 0) ? this.window.innerHeight : this.screen.height) - 1;
-        height = height - topOffset;
-        if (height < 1) height = 1;
-        if (height > topOffset) {
-            $("#page-wrapper").css("min-height", (height) + "px");
-        }
-    });
-
     $('ul.nav ul.nav-second-level a.active').parent().parent().addClass('in').parent().addClass('active');
     $('#side-menu').metisMenu({
         'toggle': false,
