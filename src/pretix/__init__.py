47af20d417be292d896e5030926702f89ca1a4a0
Raphael Michel <michel@rami.io>
2024-08-28 09:18:35 +0200

    Bump version to 2024.8.0

--- src/pretix/__init__.py
+++ src/pretix/__init__.py
@@ -19,4 +19,4 @@
 # You should have received a copy of the GNU Affero General Public License along with this program.  If not, see
 # <https://www.gnu.org/licenses/>.
 #
-__version__ = "2024.8.0.dev0"
+__version__ = "2024.8.0"
