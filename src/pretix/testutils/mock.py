e3f6b3f70a29693b3579b89e91941bd214c4b146
Raphael Michel <michel@rami.io>
2024-10-25 14:32:28 +0200

    pytest: Fix failure with fakeserver and xdist

--- src/pretix/testutils/mock.py
+++ src/pretix/testutils/mock.py
@@ -19,6 +19,7 @@
 # You should have received a copy of the GNU Affero General Public License along with this program.  If not, see
 # <https://www.gnu.org/licenses/>.
 #
+import os
 from contextlib import contextmanager
 
 import fakeredis
