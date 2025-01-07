d545b15d0dbda249bb87e8a829dde3ebd669f35c
Raphael Michel <michel@rami.io>
2025-01-07 15:12:08 +0100

    Fix incorrect language code for Japanese

--- src/pretix/_base_settings.py
+++ src/pretix/_base_settings.py
@@ -94,7 +94,7 @@ ALL_LANGUAGES = [
     ('el', _('Greek')),
     ('id', _('Indonesian')),
     ('it', _('Italian')),
-    ('jp', _('Japanese')),
+    ('ja', _('Japanese')),
     ('lv', _('Latvian')),
     ('nb-no', _('Norwegian Bokmål')),
     ('pl', _('Polish')),
