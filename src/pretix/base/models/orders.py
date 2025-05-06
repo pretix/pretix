0bef14d09230487d735499caeeabbba3de446232
Raphael Michel <michel@rami.io>
2025-05-06 14:08:49 +0200

    Profile integration

--- src/pretix/base/models/orders.py
+++ src/pretix/base/models/orders.py
@@ -3347,6 +3347,7 @@ class InvoiceAddress(models.Model):
             self.internal_reference,
             (_('Beneficiary') + ': ' + self.beneficiary) if self.beneficiary else '',
         ]
+        parts += [f'{k}: {v}' for k, v in self.describe_transmission()]
         return '\n'.join([str(p).strip() for p in parts if p and str(p).strip()])
 
     @property
@@ -3401,6 +3402,8 @@ class InvoiceAddress(models.Model):
             'custom_field': self.custom_field,
             'internal_reference': self.internal_reference,
             'beneficiary': self.beneficiary,
+            'transmission_type': self.transmission_type,
+            **self.transmission_info,
         })
         return d
 
