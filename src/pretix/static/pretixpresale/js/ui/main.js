9acbf6ee0da0e2a738f423cd9b16702953b909a7
Raphael Michel <michel@rami.io>
2025-05-27 16:00:14 +0200

    Refactor stubs for gettext JS function

--- src/pretix/static/pretixpresale/js/ui/main.js
+++ src/pretix/static/pretixpresale/js/ui/main.js
@@ -1,26 +1,4 @@
-/*global $ */
-
-function gettext(msgid) {
-    if (typeof django !== 'undefined' && typeof django.gettext !== 'undefined') {
-        return django.gettext(msgid);
-    }
-    return msgid;
-}
-
-function ngettext(singular, plural, count) {
-    if (typeof django !== 'undefined' && typeof django.ngettext !== 'undefined') {
-        return django.ngettext(singular, plural, count);
-    }
-    return plural;
-}
-
-function interpolate(fmt, object, named) {
-    if (named) {
-        return fmt.replace(/%\(\w+\)s/g, function(match){return String(obj[match.slice(2,-2)])});
-    } else {
-        return fmt.replace(/%s/g, function(match){return String(obj.shift())});
-    }
-}
+/*global $, gettext, ngettext, interpolate */
 
 var form_handlers = function (el) {
     el.find('input, select, textarea').on('invalid', function (e) {
