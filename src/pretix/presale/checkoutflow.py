0940a7b21bf737e161c0e72e64ab4b58a4ab4fdd
Phin Wolkwitz <wolkwitz@rami.io>
2025-07-30 12:13:22 +0200

    Fix payment-method preselection

--- src/pretix/presale/checkoutflow.py
+++ src/pretix/presale/checkoutflow.py
@@ -1410,8 +1410,10 @@ class PaymentStep(CartMixin, TemplateFlowStep):
             ctx['selected'] = self.request.POST['payment']
         elif self.single_use_payment:
             ctx['selected'] = self.single_use_payment['provider']
-        elif "giftcard" in self.request.event.get_payment_providers():
-            ctx['selected'] = "giftcard"
+        elif ("giftcard" in self.request.event.get_payment_providers()
+              and self.cart_customer
+              and self.cart_customer.usable_gift_cards()):
+            ctx['selected'] = 'giftcard'
         else:
             ctx['selected'] = ''
         ctx['cart'] = self.get_cart()
