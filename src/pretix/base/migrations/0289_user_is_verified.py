83f891ce2498a825f5b439d8ccdae513f5ee99be
Mira Weller <weller@rami.io>
2025-09-04 22:25:16 +0200

    fix default

--- src/pretix/base/migrations/0289_user_is_verified.py
+++ src/pretix/base/migrations/0289_user_is_verified.py
@@ -13,6 +13,6 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name="user",
             name="is_verified",
-            field=models.BooleanField(default=True),
+            field=models.BooleanField(default=False),
         ),
     ]
