2de0eef2dd6e7e1038d55d305d0462c430320906
Raphael Michel <michel@rami.io>
2025-10-30 11:08:44 +0100

    Rebase migration

--- src/pretix/base/migrations/0293_cartposition_price_includes_rounding_correction_and_more.py
+++ src/pretix/base/migrations/0293_cartposition_price_includes_rounding_correction_and_more.py
@@ -8,7 +8,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("pretixbase", "0284_ordersyncresult_ordersyncqueue"),
+        ("pretixbase", "0292_giftcard_customer"),
     ]
 
     operations = [
