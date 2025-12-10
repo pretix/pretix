c8cb7a503d3451ebc6c30e5a21be648bc1ac7379
Phin Wolkwitz <wolkwitz@rami.io>
2025-12-10 15:13:11 +0100

    Add templatetag

---
+++ src/pretix/base/templatetags/html_datetime.py
@@ -0,0 +1,43 @@
+#
+# This file is part of pretix (Community Edition).
+#
+# Copyright (C) 2014-2020  Raphael Michel and contributors
+# Copyright (C) 2020-today pretix GmbH and contributors
+#
+# This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General
+# Public License as published by the Free Software Foundation in version 3 of the License.
+#
+# ADDITIONAL TERMS APPLY: Pursuant to Section 7 of the GNU Affero General Public License, additional terms are
+# applicable granting you additional permissions and placing additional restrictions on your usage of this software.
+# Please refer to the pretix LICENSE file to obtain the full terms applicable to this work. If you did not receive
+# this file, see <https://pretix.eu/about/en/license>.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+# warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Affero General Public License along with this program.  If not, see
+# <https://www.gnu.org/licenses/>.
+#
+from django import template
+from django.utils import dateformat
+from django.utils.formats import get_format
+from django.utils.translation import get_language
+
+register = template.Library()
+
+
+@register.filter(expects_localtime=True, is_safe=False)
+def html_datetime(value, arg=None):
+    if value in (None, ''):
+        return ''
+
+    lang = get_language()
+    datetime_format = get_format(arg, lang)
+
+    try:
+        date_iso = value.isoformat()
+        date_human = dateformat.format(value, datetime_format)
+        return f"<time datetime='{date_iso}'>{date_human}</time>"
+    except AttributeError:
+        return ''
