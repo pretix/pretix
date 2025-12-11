4f36996a251c24885bb580d421f84374bdbe2dcf
Raphael Michel <michel@rami.io>
2025-12-11 20:10:18 +0100

    Do not double-call extend_expired_positions in tests, make it private

--- src/pretix/base/services/cart.py
+++ src/pretix/base/services/cart.py
@@ -541,7 +541,7 @@ class CartManager:
             else:
                 raise e
 
-    def extend_expired_positions(self):
+    def _extend_expired_positions(self):
         requires_seat = Exists(
             SeatCategoryMapping.objects.filter(
                 Q(product=OuterRef('item'))
@@ -1449,7 +1449,7 @@ class CartManager:
         self._check_max_cart_size()
 
         err = self._delete_out_of_timeframe()
-        err = self.extend_expired_positions() or err
+        err = self._extend_expired_positions() or err
         err = err or self._check_min_per_voucher()
 
         self._extend_expiry_of_valid_existing_positions()
