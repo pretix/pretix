d964ace2038644e6d93b62887bccfe0283e09788
Ryan <rdn0613@hotmail.com>
2026-01-15 19:09:55 -0500

    Removed comments

--- src/pretix/control/views/waitinglist.py
+++ src/pretix/control/views/waitinglist.py
@@ -352,10 +352,8 @@ class WaitingListView(EventPermissionRequiredMixin, WaitingListQuerySetMixin, Pa
         qs = sorted(qs, key=lambda o: o.priority, reverse=True)
 
         if not revert:
-            # Set the event-specific setting for the lottery date
             self.request.event.settings.set('main_lottery_date', now().isoformat())
         else:
-            # Delete the event setting when reverting
             self.request.event.settings.delete('main_lottery_date')
 
         headers = [
