046b694285a7c5e36339b33b0d93fcd9e3dbbd94
Ryan <rdn0613@hotmail.com>
2026-01-22 18:09:53 -0500

    Fixing reversion

--- src/pretix/static/pretixcontrol/js/ui/waitinglist_lottery.js
+++ src/pretix/static/pretixcontrol/js/ui/waitinglist_lottery.js
@@ -2,11 +2,54 @@
 
 $(function () {
     var runLotteryBtn = $('#run-lottery-btn');
+    var revertListBtn = $('#revert-list-btn');
     var itemSelect = $('select[name="item"]');
     var confirmModal = $('#lottery-confirm-modal');
+    var modalTitle = $('#lottery-modal-title');
+    var modalDescription = $('#lottery-modal-description');
+    var productContainer = $('#lottery-product-container');
     var productNameSpan = $('#lottery-product-name');
+    var modalWarning = $('#lottery-modal-warning');
     var confirmBtn = $('#lottery-confirm-btn');
-    var lotteryUrl = null;
+    var actionUrl = null;
+    
+    function buildUrlParams(action) {
+        var urlParams = new URLSearchParams(window.location.search);
+        urlParams.set('lottery', action);
+        
+        // Preserve other filter parameters from the current URL
+        var currentParams = new URLSearchParams(window.location.search);
+        if (currentParams.has('status')) {
+            urlParams.set('status', currentParams.get('status'));
+        }
+        if (currentParams.has('subevent')) {
+            urlParams.set('subevent', currentParams.get('subevent'));
+        }
+        if (currentParams.has('email')) {
+            urlParams.set('email', currentParams.get('email'));
+        }
+        if (currentParams.has('name')) {
+            urlParams.set('name', currentParams.get('name'));
+        }
+        
+        return urlParams;
+    }
+    
+    function showConfirmationModal(config) {
+        modalTitle.text(config.title);
+        modalDescription.text(config.description);
+        modalWarning.text(config.warning);
+        confirmBtn.text(config.confirmButtonText);
+        
+        if (config.showProduct) {
+            productContainer.show();
+            productNameSpan.text(config.productName);
+        } else {
+            productContainer.hide();
+        }
+        
+        confirmModal.modal('show');
+    }
     
     if (runLotteryBtn.length && itemSelect.length) {
         runLotteryBtn.on('click', function(e) {
@@ -24,30 +67,55 @@ $(function () {
             }
             
             // Build the lottery URL with the item parameter preserved
-            var urlParams = new URLSearchParams(window.location.search);
-            urlParams.set('lottery', 'run');
+            var urlParams = buildUrlParams('run');
             urlParams.set('item', selectedItemId);
             
-            // Preserve other filter parameters from the current URL
-            var currentParams = new URLSearchParams(window.location.search);
-            if (currentParams.has('status')) {
-                urlParams.set('status', currentParams.get('status'));
-            }
-            if (currentParams.has('subevent')) {
-                urlParams.set('subevent', currentParams.get('subevent'));
-            }
-            if (currentParams.has('email')) {
-                urlParams.set('email', currentParams.get('email'));
-            }
-            if (currentParams.has('name')) {
-                urlParams.set('name', currentParams.get('name'));
-            }
-            
-            lotteryUrl = window.location.pathname + '?' + urlParams.toString();
+            actionUrl = window.location.pathname + '?' + urlParams.toString();
             
             // Show confirmation modal
-            productNameSpan.text(selectedItemText);
-            confirmModal.modal('show');
+            showConfirmationModal({
+                title: gettext('Confirm Lottery Run'),
+                description: gettext('You are about to run the lottery for the following product:'),
+                productName: selectedItemText,
+                showProduct: true,
+                warning: gettext('This action will shuffle the waiting list priorities for this product. This cannot be easily undone.'),
+                confirmButtonText: gettext('Yes, run the lottery')
+            });
+            
+            return false;
+        });
+    }
+    
+    if (revertListBtn.length && itemSelect.length) {
+        revertListBtn.on('click', function(e) {
+            e.preventDefault();
+            e.stopPropagation();
+            e.stopImmediatePropagation();
+            
+            var selectedItemId = itemSelect.val();
+            var selectedItemText = itemSelect.find('option:selected').text();
+            
+            // Validate that a product is selected (not "All products")
+            if (!selectedItemId || selectedItemId === '' || selectedItemId === null || selectedItemId === undefined) {
+                alert(gettext('You must select a specific product to revert the list. Please select a product from the dropdown and try again.'));
+                return false;
+            }
+            
+            // Build the revert URL with the item parameter preserved
+            var urlParams = buildUrlParams('revert');
+            urlParams.set('item', selectedItemId);
+            
+            actionUrl = window.location.pathname + '?' + urlParams.toString();
+            
+            // Show confirmation modal
+            showConfirmationModal({
+                title: gettext('Confirm List Reversion'),
+                description: gettext('You are about to revert the waiting list priorities for the following product:'),
+                productName: selectedItemText,
+                showProduct: true,
+                warning: gettext('This action will restore the waiting list priorities to their original order. This cannot be easily undone.'),
+                confirmButtonText: gettext('Yes, revert the list')
+            });
             
             return false;
         });
@@ -58,7 +126,7 @@ $(function () {
             e.preventDefault();
             e.stopPropagation();
             
-            if (lotteryUrl) {
+            if (actionUrl) {
                 // Close modal immediately
                 confirmModal.modal('hide');
                 
@@ -67,8 +135,8 @@ $(function () {
                 $('body').removeClass('modal-open');
                 $('body').css('padding-right', '');
                 
-                // Navigate to trigger download
-                window.location.href = lotteryUrl;
+                // Navigate to trigger action
+                window.location.href = actionUrl;
             }
         });
     }
