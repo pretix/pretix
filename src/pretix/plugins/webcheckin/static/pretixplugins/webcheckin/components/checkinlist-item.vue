9de15c185a9c5178b2e5940a66616a493a5498b1
rash <rash@rash.codes>
2026-02-05 15:29:00 +0100

    migrate webcheckin plugin to vite+vue3
    - migrate vue sfcs to script setup and pug
    - move fetch calls into a api.ts module
    - move common formatting and i18n strings into module

--- src/pretix/plugins/webcheckin/static/pretixplugins/webcheckin/components/checkinlist-item.vue
+++ src/pretix/plugins/webcheckin/static/pretixplugins/webcheckin/components/checkinlist-item.vue
@@ -1,28 +1,21 @@
-<template>
-  <a class="list-group-item" href="#" @click.prevent="$emit('selected', list)">
-    <div class="row">
-      <div class="col-md-6">
-        {{ list.name }}
-      </div>
-      <div class="col-md-6 text-muted">
-        {{ subevent }}
-      </div>
-    </div>
-  </a>
-</template>
-<script>
-export default {
-  components: {},
-  props: {
-    list: Object
-  },
-  computed: {
-    subevent () {
-      if (!this.list.subevent) return '';
-      const name = i18nstring_localize(this.list.subevent.name)
-      const date = moment.utc(this.list.subevent.date_from).tz(this.$root.timezone).format(this.$root.datetime_format)
-      return `${name} · ${date}`
-    }
-  },
-}
+<script setup lang="ts">
+import { computed } from 'vue'
+import type { CheckinList } from '../api'
+import { formatSubevent } from '../i18n'
+
+const props = defineProps<{
+	list: CheckinList
+}>()
+
+defineEmits<{
+	selected: [list: CheckinList]
+}>()
+
+const subevent = computed(() => formatSubevent(props.list.subevent))
 </script>
+<template lang="pug">
+a.list-group-item(href="#", @click.prevent="$emit('selected', list)")
+	.row
+		.col-md-6 {{ list.name }}
+		.col-md-6.text-muted {{ subevent }}
+</template>
