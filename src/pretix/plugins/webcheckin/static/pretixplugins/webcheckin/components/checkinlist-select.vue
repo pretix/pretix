0d522c0eedff92a029a9135a3c32b6721904655b
rash <rash@rash.codes>
2026-02-05 15:29:00 +0100

    migrate webcheckin plugin to vite+vue3
    - migrate vue sfcs to script setup and pug
    - move fetch calls into a api.ts module
    - move common formatting and i18n strings into module

--- src/pretix/plugins/webcheckin/static/pretixplugins/webcheckin/components/checkinlist-select.vue
+++ src/pretix/plugins/webcheckin/static/pretixplugins/webcheckin/components/checkinlist-select.vue
@@ -1,101 +1,99 @@
-<template>
-  <div class="panel panel-primary checkinlist-select">
-    <div class="panel-heading">
-      <h3 class="panel-title">
-        {{ $root.strings['checkinlist.select'] }}
-      </h3>
-    </div>
-    <ul class="list-group">
-      <checkinlist-item v-if="lists" v-for="l in lists" :list="l" :key="l.id" @selected="$emit('selected', l)"></checkinlist-item>
-      <li v-if="loading" class="list-group-item text-center">
-        <span class="fa fa-4x fa-cog fa-spin loading-icon"></span>
-      </li>
-      <li v-else-if="error" class="list-group-item text-center">
-        {{ error }}
-      </li>
-      <a v-else-if="next_url" class="list-group-item text-center" href="#" @click.prevent="loadNext">
-        {{ $root.strings['pagination.next'] }}
-      </a>
-    </ul>
-  </div>
-</template>
-<script>
-export default {
-  components: {
-    CheckinlistItem: CheckinlistItem.default,
-  },
-  data() {
-    return {
-      loading: false,
-      error: null,
-      lists: null,
-      next_url: null,
-    }
-  },
-  // TODO: pagination
-  mounted() {
-    this.load()
-  },
-  methods: {
-    load() {
-      this.loading = true
-      const cutoff = moment().subtract(8, 'hours').toISOString()
-      if (location.hash) {
-        fetch(this.$root.api.lists + location.hash.substr(1) + '/' + '?expand=subevent')
-            .then(response => response.json())
-            .then(data => {
-              this.loading = false
-              if (data.id) {
-                this.$emit('selected', data)
-              } else {
-                location.hash = ''
-                this.load()
-              }
-            })
-            .catch(reason => {
-              location.hash = ''
-              this.load()
-            })
-        return
-      }
-      fetch(this.$root.api.lists + '?exclude=checkin_count&exclude=position_count&expand=subevent&ends_after=' + cutoff)
-          .then(response => response.json())
-          .then(data => {
-            this.loading = false
-            if (data.results) {
-              this.lists = data.results
-              this.next_url = data.next
-            } else if (data.results === 0) {
-              this.error = this.$root.strings['checkinlist.none']
-            } else {
-              this.error = data
-            }
-          })
-          .catch(reason => {
-            this.loading = false
-            this.error = reason
-          })
-    },
-    loadNext() {
-      this.loading = true
-      fetch(this.next_url)
-          .then(response => response.json())
-          .then(data => {
-            this.loading = false
-            if (data.results) {
-              this.lists.push(...data.results)
-              this.next_url = data.next
-            } else if (data.results === 0) {
-              this.error = this.$root.strings['checkinlist.none']
-            } else {
-              this.error = data
-            }
-          })
-          .catch(reason => {
-            this.loading = false
-            this.error = reason
-          })
-    },
-  },
+<script setup lang="ts">
+import { ref, onMounted } from 'vue'
+import { api } from '../api'
+import type { CheckinList } from '../api'
+import { STRINGS } from '../i18n'
+import CheckinlistItem from './checkinlist-item.vue'
+
+const emit = defineEmits<{
+	selected: [list: CheckinList]
+}>()
+
+const loading = ref(false)
+const error = ref<unknown>(null)
+const lists = ref<CheckinList[] | null>(null)
+const nextUrl = ref<string | null>(null)
+
+async function load () {
+	loading.value = true
+	error.value = null
+
+	try {
+		if (location.hash) {
+			const listId = location.hash.substring(1)
+			try {
+				const data = await api.fetchCheckinList(listId)
+				loading.value = false
+				if (data.id) {
+					emit('selected', data)
+					load()
+				}
+			} catch {
+				location.hash = ''
+				load()
+			}
+			return
+		}
+
+		const data = await api.fetchCheckinLists()
+		loading.value = false
+
+		if (data.results) {
+			lists.value = data.results
+			nextUrl.value = data.next
+		} else if (data.results === 0) {
+			error.value = STRINGS['checkinlist.none']
+		} else {
+			error.value = data
+		}
+	} catch (e) {
+		loading.value = false
+		error.value = e
+	}
 }
+
+async function loadNext () {
+	if (!nextUrl.value) return
+
+	loading.value = true
+	error.value = null
+
+	try {
+		const data = await api.fetchNextPage<CheckinList>(nextUrl.value)
+		loading.value = false
+
+		if (data.results) {
+			lists.value.push(...data.results)
+			nextUrl.value = data.next
+		} else if (data.results === 0) {
+			error.value = STRINGS['checkinlist.none']
+		} else {
+			error.value = data
+		}
+	} catch (e) {
+		loading.value = false
+		error.value = e
+	}
+}
+
+onMounted(() => {
+	load()
+})
 </script>
+<template lang="pug">
+.panel.panel-primary.checkinlist-select
+	.panel-heading
+		h3.panel-title {{ STRINGS['checkinlist.select'] }}
+	ul.list-group
+		CheckinlistItem(
+			v-for="l in lists",
+			:key="l.id",
+			:list="l",
+			@selected="emit('selected', $event)"
+		)
+		li.list-group-item.text-center(v-if="loading")
+			span.fa.fa-4x.fa-cog.fa-spin.loading-icon
+		li.list-group-item.text-center(v-else-if="error") {{ error }}
+		a.list-group-item.text-center(v-else-if="nextUrl", href="#", @click.prevent="loadNext")
+			| {{ STRINGS['pagination.next'] }}
+</template>
