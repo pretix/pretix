afe469cf3a33f7a1b5c7c17d060e77ae214fc9aa
Raphael Michel <michel@rami.io>
2026-02-13 12:35:49 +0100

    SafeFormatter: Ignore conversion spec

--- src/tests/helpers/test_format.py
+++ src/tests/helpers/test_format.py
@@ -29,6 +29,8 @@ def test_format_map():
     assert format_map("Foo {baz}", {"bar": 3}) == "Foo {baz}"
     assert format_map("Foo {bar.__module__}", {"bar": 3}) == "Foo {bar.__module__}"
     assert format_map("Foo {bar!s}", {"bar": 3}) == "Foo 3"
+    assert format_map("Foo {bar!r}", {"bar": '3'}) == "Foo 3"
+    assert format_map("Foo {bar!a}", {"bar": '3'}) == "Foo 3"
     assert format_map("Foo {bar:<20}", {"bar": 3}) == "Foo 3"
 
 
