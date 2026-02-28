b6914ed5c826ea0e94ce2b30231cfa9bf1201f05
adityaanilraut <adityaanilraut@gmail.com>
2026-02-28 00:59:41 -0800

    Fix SQLite segfaults: remove crash_recovery hack and migrate CI to PostgreSQL-only
    - Remove CRASHED_ITEMS set, DSession import, and pytest_configure monkeypatch
      that worked around random SQLite segfaults in CI by retrying crashed tests
    - Remove the sqlite entry from the GitHub Actions test matrix
    - Hardcode ci_postgres.cfg in the test run commands
    - Remove the 'if: matrix.database == postgres' guards that are now always true
    - Strip redundant coverage upload condition down to python-version check
    
    Closes #7

--- src/tests/conftest.py
+++ src/tests/conftest.py
@@ -28,40 +28,8 @@ from django.test import override_settings
 from django.utils import translation
 from django_scopes import scopes_disabled
 from fakeredis import FakeConnection
-from xdist.dsession import DSession
 
 from pretix.testutils.mock import get_redis_connection
-
-CRASHED_ITEMS = set()
-
-
-@pytest.hookimpl(trylast=True)
-def pytest_configure(config):
-    """
-    Somehow, somewhere, our test suite causes a segfault in SQLite in the past, but only when run
-    on CI in full. Therefore, we monkeypatch pytest-xdist to retry segfaulted
-    tests and keep fingers crossed that this doesn't turn into an infinite loop.
-    """
-
-    def _handle_crashitem(self, nodeid, worker):
-        first = nodeid not in CRASHED_ITEMS
-        runner = self.config.pluginmanager.getplugin("runner")
-        fspath = nodeid.split("::")[0]
-        msg = "Worker %r crashed while running %r" % (worker.gateway.id, nodeid)
-        CRASHED_ITEMS.add(nodeid)
-        rep = runner.TestReport(
-            nodeid, (fspath, None, fspath), (), "restarted" if first else "failed", msg, "???"
-        )
-        rep.node = worker
-        self.config.hook.pytest_runtest_logreport(report=rep)
-
-        # Schedule retry
-        if first:
-            self.sched.pending.append(self.sched.collection.index(nodeid))
-            for node in self.sched.node2pending:
-                self.sched.check_schedule(node)
-
-    DSession.handle_crashitem = _handle_crashitem
 
 
 @pytest.hookimpl(hookwrapper=True)
