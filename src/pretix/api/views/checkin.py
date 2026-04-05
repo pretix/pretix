eb10d8350bdfcfa223196a6429acbb1dfff8101f
Raphael Michel <michel@rami.io>
2026-04-05 14:42:53 +0200

    [SECURITY] API: Add missing event filter for check-ins

--- src/pretix/api/views/checkin.py
+++ src/pretix/api/views/checkin.py
@@ -1117,7 +1117,7 @@ class CheckinViewSet(viewsets.ReadOnlyModelViewSet):
     permission = 'can_view_orders'
 
     def get_queryset(self):
-        qs = Checkin.all.filter().select_related(
+        qs = Checkin.all.filter(list__event=self.request.event).select_related(
             "position",
             "device",
         )
