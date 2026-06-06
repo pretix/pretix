ed210593866c1ea614c83f87659efb3a79e2ea82
KarlKeu00 <47644344+KarlKeu00@users.noreply.github.com>
2026-06-07 00:11:17 +0200

    Add Docker secrets support in config

--- src/pretix/helpers/config.py
+++ src/pretix/helpers/config.py
@@ -32,33 +32,66 @@ class EnvOrParserConfig:
         section = re.sub('[^a-zA-Z0-9]', '_', section.upper())
         option = re.sub('[^a-zA-Z0-9]', '_', option.upper())
         return f'PRETIX_{section}_{option}'
+    
+    def _file_envkey(self, section, option):
+        section = re.sub('[^a-zA-Z0-9]', '_', section.upper())
+        option = re.sub('[^a-zA-Z0-9]', '_', option.upper())
+        return f'FILE__PRETIX_{section}_{option}'
 
     def get(self, section, option, *, raw=False, vars=None, fallback=_UNSET):
+        if self._file_envkey(section, option) in os.environ:
+            try:
+                with open(os.environ[self._file_envkey(section, option)], 'r') as f:
+                    return f.read().strip()
+            except Exception:
+                pass  # If the file cannot be read, we fall back to the normal config value
         if self._envkey(section, option) in os.environ:
             return os.environ[self._envkey(section, option)]
         return self.cp.get(section, option, raw=raw, vars=vars, fallback=fallback)
 
     def getint(self, section, option, *, raw=False, vars=None, fallback=_UNSET):
+        if self._file_envkey(section, option) in os.environ:
+            try:
+                with open(os.environ[self._file_envkey(section, option)], 'r') as f:
+                    return int(f.read().strip())
+            except Exception:
+                pass  # If the file cannot be read, we fall back to the normal config value
         if self._envkey(section, option) in os.environ:
             return int(os.environ[self._envkey(section, option)])
         return self.cp.getint(section, option, raw=raw, vars=vars, fallback=fallback)
 
     def getfloat(self, section, option, *, raw=False, vars=None, fallback=_UNSET):
+        if self._file_envkey(section, option) in os.environ:
+            try:
+                with open(os.environ[self._file_envkey(section, option)], 'r') as f:
+                    return float(f.read().strip())
+            except Exception:
+                pass  # If the file cannot be read, we fall back to the normal config value
         if self._envkey(section, option) in os.environ:
             return float(os.environ[self._envkey(section, option)])
         return self.cp.getfloat(section, option, raw=raw, vars=vars, fallback=fallback)
 
     def getboolean(self, section, option, *, raw=False, vars=None, fallback=_UNSET):
+        if self._file_envkey(section, option) in os.environ:
+            try:
+                with open(os.environ[self._file_envkey(section, option)], 'r') as f:
+                    return self.cp._convert_to_boolean(f.read().strip())
+            except Exception:
+                pass  # If the file cannot be read, we fall back to the normal config value
         if self._envkey(section, option) in os.environ:
             return self.cp._convert_to_boolean(os.environ[self._envkey(section, option)])
         return self.cp.getboolean(section, option, raw=raw, vars=vars, fallback=fallback)
 
     def has_section(self, section):
+        if any(k.startswith(self._file_envkey(section, '')) for k in os.environ):
+            return True
         if any(k.startswith(self._envkey(section, '')) for k in os.environ):
             return True
         return self.cp.has_section(section)
 
     def has_option(self, section, option):
+        if self._file_envkey(section, option) in os.environ:
+            return True
         if self._envkey(section, option) in os.environ:
             return True
         return self.cp.has_option(section, option)
