b33259e84e131853942f59ded3c8a62efcf1e2cb
KarlKeu00 <47644344+KarlKeu00@users.noreply.github.com>
2026-06-07 00:32:14 +0200

    ruff format

--- src/pretix/helpers/config.py
+++ src/pretix/helpers/config.py
@@ -29,19 +29,19 @@ class EnvOrParserConfig:
         self.cp = configparser
 
     def _envkey(self, section, option):
-        section = re.sub('[^a-zA-Z0-9]', '_', section.upper())
-        option = re.sub('[^a-zA-Z0-9]', '_', option.upper())
-        return f'PRETIX_{section}_{option}'
-    
+        section = re.sub("[^a-zA-Z0-9]", "_", section.upper())
+        option = re.sub("[^a-zA-Z0-9]", "_", option.upper())
+        return f"PRETIX_{section}_{option}"
+
     def _file_envkey(self, section, option):
-        section = re.sub('[^a-zA-Z0-9]', '_', section.upper())
-        option = re.sub('[^a-zA-Z0-9]', '_', option.upper())
-        return f'FILE__PRETIX_{section}_{option}'
+        section = re.sub("[^a-zA-Z0-9]", "_", section.upper())
+        option = re.sub("[^a-zA-Z0-9]", "_", option.upper())
+        return f"FILE__PRETIX_{section}_{option}"
 
     def get(self, section, option, *, raw=False, vars=None, fallback=_UNSET):
         if self._file_envkey(section, option) in os.environ:
             try:
-                with open(os.environ[self._file_envkey(section, option)], 'r') as f:
+                with open(os.environ[self._file_envkey(section, option)], "r") as f:
                     return f.read().strip()
             except Exception:
                 pass  # If the file cannot be read, we fall back to the normal config value
@@ -52,7 +52,7 @@ class EnvOrParserConfig:
     def getint(self, section, option, *, raw=False, vars=None, fallback=_UNSET):
         if self._file_envkey(section, option) in os.environ:
             try:
-                with open(os.environ[self._file_envkey(section, option)], 'r') as f:
+                with open(os.environ[self._file_envkey(section, option)], "r") as f:
                     return int(f.read().strip())
             except Exception:
                 pass  # If the file cannot be read, we fall back to the normal config value
@@ -63,7 +63,7 @@ class EnvOrParserConfig:
     def getfloat(self, section, option, *, raw=False, vars=None, fallback=_UNSET):
         if self._file_envkey(section, option) in os.environ:
             try:
-                with open(os.environ[self._file_envkey(section, option)], 'r') as f:
+                with open(os.environ[self._file_envkey(section, option)], "r") as f:
                     return float(f.read().strip())
             except Exception:
                 pass  # If the file cannot be read, we fall back to the normal config value
@@ -74,18 +74,22 @@ class EnvOrParserConfig:
     def getboolean(self, section, option, *, raw=False, vars=None, fallback=_UNSET):
         if self._file_envkey(section, option) in os.environ:
             try:
-                with open(os.environ[self._file_envkey(section, option)], 'r') as f:
+                with open(os.environ[self._file_envkey(section, option)], "r") as f:
                     return self.cp._convert_to_boolean(f.read().strip())
             except Exception:
                 pass  # If the file cannot be read, we fall back to the normal config value
         if self._envkey(section, option) in os.environ:
-            return self.cp._convert_to_boolean(os.environ[self._envkey(section, option)])
-        return self.cp.getboolean(section, option, raw=raw, vars=vars, fallback=fallback)
+            return self.cp._convert_to_boolean(
+                os.environ[self._envkey(section, option)]
+            )
+        return self.cp.getboolean(
+            section, option, raw=raw, vars=vars, fallback=fallback
+        )
 
     def has_section(self, section):
-        if any(k.startswith(self._file_envkey(section, '')) for k in os.environ):
+        if any(k.startswith(self._file_envkey(section, "")) for k in os.environ):
             return True
-        if any(k.startswith(self._envkey(section, '')) for k in os.environ):
+        if any(k.startswith(self._envkey(section, "")) for k in os.environ):
             return True
         return self.cp.has_section(section)
 
