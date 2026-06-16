ce7c1870eab0cd361e7ce74e3eb91122a37d722d
Lukas Bockstaller <bockstaller@pretix.eu>
2026-06-16 13:27:54 +0200

    put errors.js in an IIFE call

--- src/pretix/static/pretixbase/js/errors.js
+++ src/pretix/static/pretixbase/js/errors.js
@@ -1,17 +1,11 @@
-const registerErrorLinkHandlers = (reloadAll = false) => {
-  const backwards = document.getElementById('goback');
-  if (backwards) {
-    backwards.onclick = reloadAll
-      ? () => window.location.reload(true)
-      : () => window.history.back();
-  }
-
-  const reload = document.getElementById('reload');
-  if (reload) {
-    reload.onclick = () => window.location.reload(true);
-  }
-};
-
-document.addEventListener("DOMContentLoaded", () => registerErrorLinkHandlers());
-
-document.addEventListener("pretix:async_task_replace_page:on_error", () => registerErrorLinkHandlers(true));
+['DOMContentLoaded', 'pretix:async-task-error'].forEach(function (ev) {
+  document.addEventListener(ev, function () {
+    document.querySelectorAll('#goback, #reload').forEach(function (element) {
+      const regularLoad = ev === 'DOMContentLoaded' && element.id === 'goback';
+      element.addEventListener('click', regularLoad
+        ? () => window.history.back()
+        : () => window.location.reload()
+      );
+    });
+  });
+});
