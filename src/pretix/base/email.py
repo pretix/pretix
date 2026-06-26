1f5fe1b237a69a92adcb68544f5b51045e7e3d9b
Raphael Michel <michel@pretix.eu>
2026-06-26 16:45:20 +0200

    SSRF protection: Edge case handling for CGNAT and v4/v6 mapping (Z#23236468) (#6260)
    * SSRF protection: Edge case handling for CGNAT and v4/v6 mapping (Z#23236468)
    
    * SMTP SSRF protection: Edge case handling for CGNAT and v4/v6 mapping (#6264)
    
    ---------
    
    Co-authored-by: pajowu <engelhardt@pretix.eu>

--- src/pretix/base/email.py
+++ src/pretix/base/email.py
@@ -57,6 +57,8 @@ logger = logging.getLogger('pretix.base.email')
 
 T = TypeVar("T", bound=EmailBackend)
 
+_cgnat_net = ipaddress.ip_network('100.64.0.0/10')
+
 
 def test_custom_smtp_backend(backend: T, from_addr: str) -> None:
     try:
@@ -253,12 +255,15 @@ def create_connection(address, timeout=socket.getdefaulttimeout(),
 
         if not getattr(settings, "MAIL_CUSTOM_SMTP_ALLOW_PRIVATE_NETWORKS", False):
             ip_addr = ipaddress.ip_address(sa[0])
+            check_ip4 = ip_addr.ipv4_mapped if getattr(ip_addr, "ipv4_mapped", None) else ip_addr
             if ip_addr.is_multicast:
                 raise socket.error(f"Request to multicast address {sa[0]} blocked")
             if ip_addr.is_loopback or ip_addr.is_link_local:
                 raise socket.error(f"Request to local address {sa[0]} blocked")
             if ip_addr.is_private:
                 raise socket.error(f"Request to private address {sa[0]} blocked")
+            if check_ip4 in _cgnat_net:
+                raise socket.error(f"Request to RFC 6598 address {sa[0]} blocked")
 
         sock = None
         try:
