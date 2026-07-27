442145bde481c457e0d68c41ad42b1a22f0d0f16
Raphael Michel <michel@rami.io>
2026-07-27 22:12:13 +0200

    Max height for timeline

--- src/pretix/static/pretixcontrol/js/ui/dashboard.js
+++ src/pretix/static/pretixcontrol/js/ui/dashboard.js
@@ -1,18 +1,20 @@
-/*global $,gettext*/
+/* global $,gettext */
 
 $(function () {
-    if ($("div[data-lazy-id]").length == 0) {
-        return;
-    }
-    $.getJSON("widgets.json" + ($("select[name='subevent']").val() ? "?subevent=" + $("select[name='subevent']").val() : ""), function (data) {
-        $.each(data.widgets, function (k, v) {
-            $("[data-lazy-id=" + v.lazy + "]").removeClass("widget-lazy-loading");
-            $("[data-lazy-id=" + v.lazy + "] .widget").html(v.content);
-        });
-    });
-});
+	if ($('div[data-lazy-id]').length == 0) {
+		return
+	}
+	$.getJSON('widgets.json' + ($('select[name=\'subevent\']').val() ? '?subevent=' + $('select[name=\'subevent\']').val() : ''), function (data) {
+		$.each(data.widgets, function (k, v) {
+			$('[data-lazy-id=' + v.lazy + ']').removeClass('widget-lazy-loading')
+			$('[data-lazy-id=' + v.lazy + '] .widget').html(v.content)
+		})
+	})
+})
 $(function () {
-    if ($("#logs_target").length == 0) {
-        return;
-    }
-});
+	$('.timeline').each(function () {
+		let $tl = $(this)
+		let $first = $(this).find('.row:not(.text-muted)').first()
+		$tl.scrollTop($tl.scrollTop() + Math.max($first.position().top - 50, 0))
+	})
+})
