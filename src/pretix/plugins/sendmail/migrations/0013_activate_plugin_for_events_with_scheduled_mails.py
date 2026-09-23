473290b88d86cf6626f6328cec5264cc305311a2
Phin Wolkwitz <wolkwitz@rami.io>
2026-09-23 12:48:54 +0200

    [wip] Add rule disabling to migration, add uninstall hook

--- src/pretix/plugins/sendmail/migrations/0013_activate_plugin_for_events_with_scheduled_mails.py
+++ src/pretix/plugins/sendmail/migrations/0013_activate_plugin_for_events_with_scheduled_mails.py
@@ -5,15 +5,24 @@ from django.db.models import Exists, OuterRef
 def activate_plugin(apps, schema_editor):
     Event = apps.get_model("pretixbase", "Event")
     ScheduledMail = apps.get_model("sendmail", "ScheduledMail")
+    Rule = apps.get_model("sendmail", "Rule")
+
     events = (
         Event.objects
         .exclude(plugins__icontains="pretix.plugins.sendmail")
         .filter(
             Exists(
-                ScheduledMail.objects.filter(event=OuterRef('pk')).exclude(state=ScheduledMail.STATE_COMPLETED)
+                ScheduledMail.objects.filter(event=OuterRef('pk'), rule__enabled=True).exclude(state='completed')
             )
         )
     )
+
+    only_completed_rules = Rule.objects.exclude(event__plugins__icontains="pretix.plugins.sendmail",
+                                                enabled=False).filter(
+        ~Exists(ScheduledMail.objects.filter(rule=OuterRef('pk')).exclude(state='completed'))
+    )
+    only_completed_rules.update(enabled=False)
+
     for event in events:
         event.enable_plugin('pretix.plugins.sendmail')
         event.save(update_fields=['plugins'])
