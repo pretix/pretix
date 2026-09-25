866863c4681e54b16f185f82fb269731e3d6d71d
Richard Schreiber <schreiber@rami.io>
2026-09-25 09:04:24 +0200

    Do not create so many savepoints

--- src/pretix/static/pretixcontrol/js/ui/editor.js
+++ src/pretix/static/pretixcontrol/js/ui/editor.js
@@ -556,8 +556,6 @@ var editor = {
 		editor.$fcv.get(0).height = editor.pdf_viewport.height
 		editor.fabric = new fabric.Canvas('fabric-canvas')
 
-		editor.fabric.on('object:modified', editor._create_savepoint)
-		editor.fabric.on('object:added', editor._create_savepoint)
 		editor.fabric.on('selection:cleared', editor._update_toolbox)
 		editor.fabric.on('selection:created', editor._update_toolbox)
 		editor.fabric.on('selection:updated', editor._update_toolbox)
@@ -591,6 +589,8 @@ var editor = {
 			editor._ready()
 		}
 
+		editor.fabric.on('object:modified', editor._create_savepoint)
+		editor.fabric.on('object:added', editor._create_savepoint)
 		editor.fabric.on('object:added', function(e) {
 			editor.fabric.setActiveObject(e.target);
 		})
@@ -960,7 +960,6 @@ var editor = {
 			mtr: true
 		})
 		editor.fabric.add(text)
-		editor._create_savepoint()
 		return text
 	},
 
@@ -976,7 +975,6 @@ var editor = {
 		rect.scaleToHeight(126)
 		rect.setControlsVisibility({ mtr: false, mb: false, mt: false, mr: false, ml: false })
 		editor.fabric.add(rect)
-		editor._create_savepoint()
 		return rect
 	},
 
@@ -1013,7 +1011,6 @@ var editor = {
 			mtr: true
 		})
 		editor.fabric.add(rect)
-		editor._create_savepoint()
 		$('#version-notice').show()
 		return rect
 	},
@@ -1030,7 +1027,6 @@ var editor = {
 		})
 		rect.setControlsVisibility({ mtr: false })
 		editor.fabric.add(rect)
-		editor._create_savepoint()
 		return rect
 	},
 
@@ -1048,7 +1044,6 @@ var editor = {
 		})
 		rect.setControlsVisibility({ mtr: false, mb: false, mt: false, mr: false, ml: false })
 		editor.fabric.add(rect)
-		editor._create_savepoint()
 		return rect
 	},
 
